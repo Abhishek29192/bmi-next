48901ed0622bb9e8d95b7cee4ef6f0e3d84e62a7
Dan Edwards <igneosaur@gmail.com>
2020-06-19 15:10:34 +0100

    feat: add contentful-migrate library

---
+++ libraries/contentful-migrate/cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const { runMigration } = require("contentful-migration");
+require("dotenv").config({ path: `${__dirname}/.env.${process.env.NODE_ENV}` });
+
+const main = async (argv) => {
+  const { SPACE_ID, ACCESS_TOKEN } = process.env;
+  const environmentId = argv.find((arg) => arg !== "-d");
+
+  try {
+    if (!SPACE_ID || !ACCESS_TOKEN)
+      throw Error("Missing env config `SPACE_ID` or `ACCESS_TOKEN`");
+
+    await runMigration({
+      filePath: `${__dirname}/src/index.js`,
+      spaceId: SPACE_ID,
+      accessToken: ACCESS_TOKEN,
+      environmentId
+    });
+    process.exit(0);
+  } catch (error) {
+    console.error("Error:", error.message);
+    process.exit(1);
+  }
+};
+
+main(process.argv.slice(2));
