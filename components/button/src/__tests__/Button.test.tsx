c9922ccd0a5432209091d68b4cf7105e7572da13
Alex Canessa <canessa.alex@gmail.com>
2020-06-30 13:57:59 +0200

    feat(button): use BMI styleguide

--- components/button/src/__tests__/Button.test.tsx
+++ components/button/src/__tests__/Button.test.tsx
@@ -4,7 +4,23 @@ import { render } from "@testing-library/react";
 
 describe("Button component", () => {
   it("renders correctly", () => {
-    const { container } = render(<Button>Hello World</Button>);
+    const { container } = render(<Button>Caption</Button>);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it("renders an outlined button on dark background", () => {
+    const { container } = render(
+      <Button hasDarkBackground variant="outlined">
+        Caption
+      </Button>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it("renders a flat button on dark background", () => {
+    const { container } = render(
+      <Button hasDarkBackground variant="text">
+        Caption
+      </Button>
+    );
     expect(container.firstChild).toMatchSnapshot();
   });
 });
