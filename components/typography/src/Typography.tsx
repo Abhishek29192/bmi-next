0c4c5ae7c536fb4a87966960f203f1aece27e8f0
Dan Edwards <igneosaur@gmail.com>
2020-07-01 10:19:46 +0100

    feat(typography): style component

--- components/typography/src/Typography.tsx
+++ components/typography/src/Typography.tsx
@@ -1,10 +1,29 @@
-import React from "react";
 import MaterialTypography, {
   TypographyProps
 } from "@material-ui/core/Typography";
+import classnames from "classnames";
+import React from "react";
+import styles from "./Typography.module.scss";
 
-const Typography = ({ children, variant, ...props }: TypographyProps) => (
-  <MaterialTypography variant={variant} {...props}>
+type Props = TypographyProps & {
+  hasUnderline?: boolean;
+};
+
+const Typography = ({
+  children,
+  className,
+  hasUnderline,
+  variant,
+  ...props
+}: Props) => (
+  <MaterialTypography
+    variant={variant}
+    className={classnames(className, styles["Typography"], {
+      [styles["Typography--underline"]]:
+        hasUnderline && ["h1", "h2", "h3", "h4"].includes(variant)
+    })}
+    {...props}
+  >
     {children}
   </MaterialTypography>
 );
