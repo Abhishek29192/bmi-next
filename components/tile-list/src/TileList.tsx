b5697c5bbc5528dc098cfcb830e178e651060e39
Nina Kahr <nina@digital-detox.co.uk>
2020-07-13 18:01:01 +0100

    feat(tile-list): create component

--- components/tile-list/src/TileList.tsx
+++ components/tile-list/src/TileList.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "@bmi/button";
+import styles from "./TileList.module.scss";
+import Card from "@bmi/card";
+import Icon from "@bmi/icon";
+import classnames from "classnames";
 
-const TileList = () => {
-  return null;
+type TileProps = {
+  children: React.ReactNode;
+  icon: React.ReactNode;
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 };
 
+const Tile = ({ children, icon, onClick }: TileProps) => {
+  return (
+    <Card
+      className={classnames(styles["Tile"], {
+        [styles["Tile--clickable"]]: !!onClick
+      })}
+      onClick={onClick}
+    >
+      <Icon className={styles["Tile__icon"]}>{icon}</Icon>
+      <div className={styles["Tile__description"]}>{children}</div>
+    </Card>
+  );
+};
+
+type Props = {
+  children: React.ReactNode[];
+  isPaginated?: boolean;
+  itemsPerPage?: number;
+};
+
+const TileList = ({ children, isPaginated, itemsPerPage = 8 }: Props) => {
+  const numberOfTiles = children.length;
+  const [numberOfVisibleTiles, setNumberOfVisibleTiles] = useState(
+    isPaginated ? itemsPerPage : numberOfTiles
+  );
+  return (
+    <>
+      <div className={styles["TileList__wrapper"]}>
+        {children.slice(0, numberOfVisibleTiles)}
+      </div>
+      {numberOfVisibleTiles >= numberOfTiles ? null : (
+        <div className={styles["TileList__button"]}>
+          <Button
+            variant="outlined"
+            fullWidth
+            onClick={() => setNumberOfVisibleTiles(numberOfVisibleTiles + 8)}
+          >
+            Show More
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
+
+TileList.Item = Tile;
 export default TileList;
--- components/tile-list/src/__tests__/TileList.test.tsx
+++ components/tile-list/src/__tests__/TileList.test.tsx
@@ -1,10 +1,54 @@
 import React from "react";
 import TileList from "../";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import Typography from "@bmi/typography";
+import UserIcon from "@material-ui/icons/Person";
 
 describe("TileList component", () => {
   it("renders correctly", () => {
-    const { container } = render(<TileList />);
+    const { container } = render(
+      <TileList>
+        <TileList.Item icon={<UserIcon />}>
+          <Typography>Text 1</Typography>
+        </TileList.Item>
+        <TileList.Item icon={<UserIcon />}>
+          <Typography>Text 2</Typography>
+        </TileList.Item>
+      </TileList>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it("renders correctly with pagination", () => {
+    const { container } = render(
+      <TileList isPaginated itemsPerPage={2}>
+        <TileList.Item icon={<UserIcon />}>
+          <Typography>Text 1</Typography>
+        </TileList.Item>
+        <TileList.Item icon={<UserIcon />}>
+          <Typography>Text 2</Typography>
+        </TileList.Item>
+        <TileList.Item icon={<UserIcon />}>
+          <Typography>Text 2</Typography>
+        </TileList.Item>
+      </TileList>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it("shows more tiles on 'Show more' click", () => {
+    const { container, getByRole } = render(
+      <TileList isPaginated itemsPerPage={2}>
+        <TileList.Item icon={<UserIcon />}>
+          <Typography>Text 1</Typography>
+        </TileList.Item>
+        <TileList.Item icon={<UserIcon />}>
+          <Typography>Text 2</Typography>
+        </TileList.Item>
+        <TileList.Item icon={<UserIcon />}>
+          <Typography>Text 3</Typography>
+        </TileList.Item>
+      </TileList>
+    );
+    fireEvent.click(getByRole("button"));
     expect(container.firstChild).toMatchSnapshot();
   });
 });
