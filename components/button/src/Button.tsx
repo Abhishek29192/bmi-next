8840330659246cbb1680f30793937129880962fc
Nina Kahr <nina@digital-detox.co.uk>
2020-07-15 11:43:27 +0100

    feat(button): add clickable area ripple and hover color

--- components/button/src/Button.tsx
+++ components/button/src/Button.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import MaterialButton, {
   ButtonProps as MuiButtonProps
 } from "@material-ui/core/Button";
@@ -32,50 +32,22 @@ const Button = ({
   isIconButton,
   accessibilityLabel,
   size,
-  onClick,
   disabled,
   ...rest
 }: ButtonProps | IconButtonProps) => {
-  const [iconRef, setIconRef] = useState<any>();
-
   return isIconButton ? (
-    <div
-      className={classnames(styles["IconButtonWrapper"], {
-        [styles[`IconButtonWrapper--${size || "medium"}`]]: isIconButton
-      })}
-      onClick={(event) => {
-        const target = event.target as HTMLElement;
-        if (
-          typeof target.className === "string" &&
-          target.className.includes("IconButtonWrapper")
-        ) {
-          iconRef.click();
-        }
-      }}
+    <MaterialIconButton
+      className={classnames(
+        styles["IconButton"],
+        styles[`IconButton--${size || "medium"}`],
+        styles[`IconButton--${disabled ? "disabled" : "enabled"}`],
+        className
+      )}
       aria-label={accessibilityLabel}
-      data-testid="icon-button-wrapper"
+      {...rest}
     >
-      <MaterialIconButton
-        className={classnames(
-          styles["IconButton"],
-          styles[`IconButton--${size || "medium"}`],
-          styles[`IconButton--${disabled ? "disabled" : "enabled"}`],
-          className
-        )}
-        ref={(ref) => {
-          setIconRef(ref);
-        }}
-        onClick={(event) => {
-          if (onClick) {
-            onClick(event);
-          }
-        }}
-        aria-label={accessibilityLabel}
-        {...rest}
-      >
-        {children}
-      </MaterialIconButton>
-    </div>
+      {children}
+    </MaterialIconButton>
   ) : (
     <MaterialButton
       className={classnames(styles["Button"], className, {
