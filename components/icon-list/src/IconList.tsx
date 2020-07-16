a3ad477b65bc46b24928bc250673df4c4ba0ea5d
Nina Kahr <nina@digital-detox.co.uk>
2020-07-16 16:15:36 +0100

    feat(icon-list): create component

---
+++ components/icon-list/src/IconList.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./IconList.module.scss";
+import Typography from "@bmi/typography";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+
+type ListItemProps = {
+  icon?: React.ReactNode;
+  title: string;
+  children: React.ReactNode;
+};
+const ListItem = ({ icon, title, children }: ListItemProps) => (
+  <div className={styles["ListItem"]}>
+    <div className={styles["ListItem__icon"]}>
+      {icon || <ChevronRightIcon />}
+    </div>
+    <div className={styles["ListItem__content"]}>
+      <Typography className={styles["ListItem__title"]} variant="h6">
+        {title}
+      </Typography>
+      {children}
+    </div>
+  </div>
+);
+
+type Props = {
+  children:
+    | React.ReactElement<ListItemProps>
+    | React.ReactElement<ListItemProps>[];
+};
+
+const IconList = ({ children }: Props) => {
+  const items: React.ReactElement<any>[] = [].concat(children);
+  return (
+    <div>
+      {items.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+    </div>
+  );
+};
+
+IconList.Item = ListItem;
+
+export default IconList;
