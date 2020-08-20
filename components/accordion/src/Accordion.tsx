8cf2784b942332196051ed425e57635346ba2b82
Nina Kahr <nina@digital-detox.co.uk>
2020-08-20 23:37:17 +0200

    feat(accordion): make accordion behave like a radio

--- components/accordion/src/Accordion.tsx
+++ components/accordion/src/Accordion.tsx
@@ -6,14 +6,81 @@ import MaterialAccordionSummary, {
   ExpansionPanelSummaryProps
 } from "@material-ui/core/ExpansionPanelSummary";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Accordion.module.scss";
 
-const Accordion = ({ children, ...props }: ExpansionPanelProps) => (
-  <ExpansionPanel className={styles.Accordion} {...props}>
-    {children}
-  </ExpansionPanel>
-);
+type AccordionItemProps = ExpansionPanelProps & {
+  isExpanded?: boolean;
+  setExpanded?: React.Dispatch<React.SetStateAction<number>>;
+  isRadio?: boolean;
+  itemKey?: number;
+  isDefaultExpanded?: boolean;
+};
+
+type AccordionProps = {
+  children:
+    | React.ReactElement<AccordionItemProps>
+    | React.ReactElement<AccordionItemProps>[];
+  isRadio?: boolean;
+};
+
+const Accordion = ({ children, isRadio }: AccordionProps) => {
+  const firstDefaultExpanded = React.Children.toArray(children).findIndex(
+    (child) => React.isValidElement(child) && child.props.defaultExpanded
+  );
+
+  const [expanded, setExpanded] = useState<number>(firstDefaultExpanded + 1);
+
+  return (
+    <>
+      {React.Children.map(children, (child, index) => {
+        const itemKey = index + 1;
+        const radioProps = isRadio
+          ? {
+              isRadio,
+              isExpanded: expanded === itemKey,
+              setExpanded
+            }
+          : {};
+
+        return React.cloneElement(child, {
+          itemKey,
+          ...radioProps
+        });
+      })}
+    </>
+  );
+};
+
+const AccordionItem = ({
+  children,
+  itemKey,
+  onChange,
+  isRadio,
+  isExpanded,
+  setExpanded,
+  ...props
+}: AccordionItemProps) => {
+  const handleChange = (event, expandedState) => {
+    if (setExpanded) {
+      setExpanded(expandedState ? itemKey : 0);
+    }
+    if (onChange) {
+      onChange(event, expandedState);
+    }
+  };
+
+  return (
+    <ExpansionPanel
+      expanded={isExpanded}
+      onChange={handleChange}
+      className={styles.Accordion}
+      {...props}
+    >
+      {children}
+    </ExpansionPanel>
+  );
+};
 
 type AccordionSummaryProps = ExpansionPanelSummaryProps & {
   expandIcon?: React.ReactElement;
@@ -32,5 +99,6 @@ const AccordionSummary = ({
 
 Accordion.Summary = AccordionSummary;
 Accordion.Details = ExpansionPanelDetails;
+Accordion.Item = AccordionItem;
 
 export default Accordion;
--- components/accordion/src/__tests__/Accordion.test.tsx
+++ components/accordion/src/__tests__/Accordion.test.tsx
@@ -1,15 +1,124 @@
 import React from "react";
 import Accordion from "../";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 describe("Accordion component", () => {
   it("renders correctly", () => {
     const { container } = render(
       <Accordion>
-        <Accordion.Summary>Heading</Accordion.Summary>
-        <Accordion.Details>Content</Accordion.Details>
+        <Accordion.Item>
+          <Accordion.Summary>Heading</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+        <Accordion.Item>
+          <Accordion.Summary>Heading</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
       </Accordion>
     );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it("renders correctly with default expanded item", () => {
+    const { container } = render(
+      <Accordion>
+        <Accordion.Item>
+          <Accordion.Summary>Heading</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+        <Accordion.Item defaultExpanded>
+          <Accordion.Summary>Heading</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+      </Accordion>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it("renders correctly as radio", () => {
+    const { container } = render(
+      <Accordion isRadio>
+        <Accordion.Item>
+          <Accordion.Summary>Heading</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+        <Accordion.Item>
+          <Accordion.Summary>Heading</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+      </Accordion>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it("renders correctly with onChange", () => {
+    const onChange = jest.fn();
+    const { getByText } = render(
+      <Accordion>
+        <Accordion.Item onChange={onChange}>
+          <Accordion.Summary>Heading One</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+        <Accordion.Item>
+          <Accordion.Summary>Heading Two</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+      </Accordion>
+    );
+    const firstPanel = getByText("Heading One");
+    fireEvent.click(firstPanel);
+
+    expect(onChange.mock.calls).toMatchSnapshot();
+  });
+  it("renders correctly after expanded item clicked", () => {
+    const { getByText, container } = render(
+      <Accordion>
+        <Accordion.Item defaultExpanded>
+          <Accordion.Summary>Heading One</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+        <Accordion.Item>
+          <Accordion.Summary>Heading Two</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+      </Accordion>
+    );
+    const firstPanel = getByText("Heading One");
+    fireEvent.click(firstPanel);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it("renders correctly as radio when item is expanded", () => {
+    const { getByText, container } = render(
+      <Accordion isRadio>
+        <Accordion.Item>
+          <Accordion.Summary>Heading One</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+        <Accordion.Item>
+          <Accordion.Summary>Heading Two</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+      </Accordion>
+    );
+    const firstPanel = getByText("Heading One");
+    fireEvent.click(firstPanel);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it("renders correctly as radio when item is collapsed", () => {
+    const { getByText, container } = render(
+      <Accordion isRadio>
+        <Accordion.Item defaultExpanded>
+          <Accordion.Summary>Heading One</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+        <Accordion.Item>
+          <Accordion.Summary>Heading Two</Accordion.Summary>
+          <Accordion.Details>Content</Accordion.Details>
+        </Accordion.Item>
+      </Accordion>
+    );
+    const firstPanel = getByText("Heading One");
+    fireEvent.click(firstPanel);
+
     expect(container.firstChild).toMatchSnapshot();
   });
 });
