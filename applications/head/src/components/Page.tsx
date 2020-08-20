5f93267c13c44f840d08c015549307eab9e5b984
Nina Kahr <nina@digital-detox.co.uk>
2020-08-20 13:39:26 +0200

    feat(head): include header and footer components in Page

--- applications/head/src/components/Page.tsx
+++ applications/head/src/components/Page.tsx
@@ -1,15 +1,51 @@
-// TODO: Maybe move this in the component folder.
-
 import React from "react";
-// TODO: Remove `src` once !9 gets merged.
 import BmiThemeProvider from "@bmi/theme-provider/src";
+// import Menu from "../components/Menu";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import NewsletterSignUp from "../components/NewsLetterSignUp";
+import { Helmet } from "react-helmet";
+import { PageData, SiteData } from "../templates/types";
 
 type Props = {
   children: React.ReactNode;
+  pageData: PageData;
+  siteData: SiteData;
 };
 
-const Page = ({ children }: Props) => {
-  return <BmiThemeProvider>{children}</BmiThemeProvider>;
+const Page = ({ children, pageData, siteData }: Props) => {
+  const { hero: heroData } = pageData;
+  const {
+    footerMainNavigation,
+    footerSecondaryNavigation,
+    signUpTitle,
+    signUpDescription,
+    signUpInputLabel,
+    signUpCallToAction
+  } = siteData;
+
+  const signUpData = {
+    title: signUpTitle,
+    description: signUpDescription.signUpDescription,
+    inputLabel: signUpInputLabel,
+    inputCallToAction: signUpCallToAction
+  };
+
+  return (
+    <BmiThemeProvider>
+      <Helmet title={pageData.title} />
+      {/* <Menu /> add this when merged */}
+      <Hero data={heroData} />
+      {children}
+      {pageData.showSignUpBanner ? (
+        <NewsletterSignUp data={signUpData} />
+      ) : null}
+      <Footer
+        mainNavigation={footerMainNavigation}
+        secondaryNavigation={footerSecondaryNavigation}
+      />
+    </BmiThemeProvider>
+  );
 };
 
 export default Page;
