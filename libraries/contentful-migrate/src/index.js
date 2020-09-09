c7f269b5d71bded1bea9700f945362ce88b237b6
Dan Edwards <igneosaur@gmail.com>
2020-09-09 10:16:33 +0100

    refactor(contentful-migrate): refactor json files to include more type     properties

--- libraries/contentful-migrate/src/index.js
+++ libraries/contentful-migrate/src/index.js
@@ -5,14 +5,23 @@ const contentTypes = require("require-dir")(`${__dirname}/content-types`);
 module.exports = function (migration) {
   const argv = process.argv.slice(2);
 
-  Object.entries(contentTypes).forEach(([name, fields]) => {
-    if (argv.includes("-d")) migration.deleteContentType(name.toLowerCase());
-    const contentType = migration
-      .createContentType(name.toLowerCase())
-      .name(name);
+  Object.entries(contentTypes).forEach(
+    ([, { name, description = null, displayField = null, fields }]) => {
+      const id = name.charAt(0).toLowerCase() + name.slice(1).replace(/ /g, "");
+      if (argv.includes("-d")) {
+        migration.deleteContentType(id);
+      }
+      const contentType = migration.createContentType(id).name(name);
+      if (description) {
+        contentType.description(description);
+      }
+      if (displayField) {
+        contentType.displayField(displayField);
+      }
 
-    Object.entries(fields).forEach(([fieldName, field]) =>
-      contentType.createField(field.name && fieldName, field)
-    );
-  });
+      fields.forEach(({ id, ...options }) =>
+        contentType.createField(id, options)
+      );
+    }
+  );
 };
