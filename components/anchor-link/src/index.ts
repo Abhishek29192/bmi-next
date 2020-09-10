855838bd69c32efc6384a91c52407269070b1d67
RenownedOyster <anthonywbutler1@gmail.com>
2020-09-10 13:02:54 +0100

    feat(location-card): add location card component

--- components/anchor-link/src/index.ts
+++ components/anchor-link/src/index.ts
@@ -1,3 +1,4 @@
 import AnchorLink from "./AnchorLink";
 
+export type { ClickableAction } from "@bmi/clickable";
 export default AnchorLink;
---
+++ components/location-card/src/LocationCard.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./LocationCard.module.scss";
+import Typography from "@bmi/typography";
+import AnchorLink, { ClickableAction } from "@bmi/anchor-link";
+import Icon from "@bmi/icon";
+import { LocationOn, Phone, Mail } from "@material-ui/icons";
+
+type DetailType = "address" | "phone" | "email";
+
+type DetailProps = {
+  text: React.ReactNode;
+  action?: ClickableAction;
+  label: React.ReactNode;
+  type: DetailType;
+};
+
+type Props = {
+  title: React.ReactNode;
+  details: readonly [DetailProps, ...DetailProps[]];
+  footNote: React.ReactNode;
+};
+
+const LocationItem = ({ text, action, label, type }: DetailProps) => {
+  const typeToIconMap: Record<DetailType, SVGImport> = {
+    address: LocationOn,
+    phone: Phone,
+    email: Mail
+  };
+
+  return (
+    <>
+      <dt className={styles["term"]}>
+        <Icon source={typeToIconMap[type]} className={styles["icon"]} />
+        <Typography className={styles["label"]}>{label}</Typography>
+      </dt>
+      <dd className={styles["description"]}>
+        {action ? (
+          <AnchorLink action={action} className={styles["link"]}>
+            {text}
+          </AnchorLink>
+        ) : (
+          text
+        )}
+      </dd>
+    </>
+  );
+};
+
+const LocationCard = ({ title, details, footNote }: Props) => {
+  return (
+    <address className={styles["LocationCard"]}>
+      <Typography variant="h5">{title}</Typography>
+      <dl className={styles["list"]}>
+        {details.map((detail, index) => (
+          <LocationItem key={index} {...detail} />
+        ))}
+      </dl>
+      <Typography className={styles["foot-note"]}>{footNote}</Typography>
+    </address>
+  );
+};
+
+export default LocationCard;
