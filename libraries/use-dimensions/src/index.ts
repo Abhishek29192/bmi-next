1b3e1a0e54bc53b92a6f29c47727f7fe69b00f38
Henry Liu <thenrikie@hotmail.com>
2020-09-16 15:18:32 +0100

    feat(use-dimensions): add useDimensions hook

---
+++ libraries/use-dimensions/src/index.ts
@@ -0,0 +1,66 @@
+// NOTE: source code copied from https://github.com/Swizec/useDimensions with unmerged SSR support. Removed scroll event listening and x, y values in the return object.
+
+import { useState, useCallback, useLayoutEffect, useEffect } from "react";
+
+export interface DimensionObject {
+  width: number;
+  height: number;
+  right: number;
+  bottom: number;
+}
+
+export type UseDimensionsHook = [
+  (node: HTMLElement) => void,
+  {} | DimensionObject,
+  HTMLElement
+];
+
+export interface UseDimensionsArgs {
+  liveMeasure?: boolean;
+}
+
+function getDimensionObject(node: HTMLElement): DimensionObject {
+  const rect = node.getBoundingClientRect();
+
+  return {
+    width: rect.width,
+    height: rect.height,
+    right: rect.right,
+    bottom: rect.bottom
+  };
+}
+
+function useDimensions({
+  liveMeasure = true
+}: UseDimensionsArgs = {}): UseDimensionsHook {
+  const [dimensions, setDimensions] = useState({});
+  const [node, setNode] = useState(null);
+  const useLayoutHookBasedOnEnvironment =
+    typeof window === "undefined" ? useEffect : useLayoutEffect;
+
+  const ref = useCallback((node) => {
+    setNode(node);
+  }, []);
+
+  useLayoutHookBasedOnEnvironment(() => {
+    if (node) {
+      const measure = () =>
+        window.requestAnimationFrame(() =>
+          setDimensions(getDimensionObject(node))
+        );
+      measure();
+
+      if (liveMeasure) {
+        window.addEventListener("resize", measure);
+
+        return () => {
+          window.removeEventListener("resize", measure);
+        };
+      }
+    }
+  }, [node]);
+
+  return [ref, dimensions, node];
+}
+
+export default useDimensions;
