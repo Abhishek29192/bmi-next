f320b00f29c411958f7a8ae2e1a9c7b114467038
Henry Liu <thenrikie@hotmail.com>
2020-10-05 18:45:50 +0100

    feat(head): add lead block to PDP

---
+++ applications/head/src/components/ProductLeadBlock.tsx
@@ -0,0 +1,160 @@
+import React from "react";
+import Button from "@bmi/button";
+import LeadBlock from "@bmi/lead-block";
+import Icon from "@bmi/icon";
+import IconList from "@bmi/icon-list";
+import Tabs from "@bmi/tabs";
+import Table from "@bmi/table";
+import Typography from "@bmi/typography";
+import CheckIcon from "@material-ui/icons/Check";
+
+const BlueCheckIcon = <Icon source={CheckIcon} style={{ color: "#009fe3" }} />;
+
+const ProductLeadBlock = () => {
+  return (
+    <Tabs initialValue="one">
+      <Tabs.TabPanel heading="About" index="one">
+        <LeadBlock>
+          <LeadBlock.Content>
+            <LeadBlock.Content.Section>
+              <Typography>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren. Lorem
+                ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                nonumy eirmod tempor invidunt ut labore et dolore.
+              </Typography>
+            </LeadBlock.Content.Section>
+            <LeadBlock.Content.Section>
+              <LeadBlock.Content.Heading>
+                Guarantees and warranties
+              </LeadBlock.Content.Heading>
+              <Typography>Ipsum</Typography>
+            </LeadBlock.Content.Section>
+            <LeadBlock.Content.Section>
+              <LeadBlock.Content.Heading>
+                Awards and certificates
+              </LeadBlock.Content.Heading>
+              <Typography>Ipsum</Typography>
+            </LeadBlock.Content.Section>
+          </LeadBlock.Content>
+          <LeadBlock.Card theme="blue-900">
+            <LeadBlock.Card.Section>
+              <LeadBlock.Card.Heading>Heading 1</LeadBlock.Card.Heading>
+              <LeadBlock.Card.Content>
+                <IconList>
+                  <IconList.Item
+                    icon={BlueCheckIcon}
+                    title="Lorem ipsum dolor sit amet"
+                  >
+                    Lorem ipsum
+                  </IconList.Item>
+                  <IconList.Item
+                    icon={BlueCheckIcon}
+                    title="Lorem ipsum dolor sit amet"
+                  >
+                    Lorem ipsum
+                  </IconList.Item>
+                </IconList>
+              </LeadBlock.Card.Content>
+            </LeadBlock.Card.Section>
+            <LeadBlock.Card.Section>
+              <LeadBlock.Card.Heading variant="h5">
+                Heading 2
+              </LeadBlock.Card.Heading>
+              <LeadBlock.Card.Content>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </Typography>
+              </LeadBlock.Card.Content>
+              <LeadBlock.Card.Action>
+                <Button hasDarkBackground variant="outlined">
+                  Caption
+                </Button>
+              </LeadBlock.Card.Action>
+            </LeadBlock.Card.Section>
+          </LeadBlock.Card>
+        </LeadBlock>
+      </Tabs.TabPanel>
+      <Tabs.TabPanel heading="Technical specifications" index="two">
+        <LeadBlock>
+          <LeadBlock.Content>
+            <Table hasNoBorder>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell>Lorem</Table.Cell>
+                  <Table.Cell>ipsum</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Lorem</Table.Cell>
+                  <Table.Cell>ipsum</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Lorem</Table.Cell>
+                  <Table.Cell>ipsum</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Lorem</Table.Cell>
+                  <Table.Cell>ipsum</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Lorem</Table.Cell>
+                  <Table.Cell>ipsum</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Lorem</Table.Cell>
+                  <Table.Cell>ipsum</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Lorem</Table.Cell>
+                  <Table.Cell>ipsum</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
+          </LeadBlock.Content>
+          <LeadBlock.Card theme="blue-900">
+            <LeadBlock.Card.Section>
+              <LeadBlock.Card.Heading hasUnderline>
+                Heading 1
+              </LeadBlock.Card.Heading>
+              <LeadBlock.Card.Content>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus ut egestas risus. Fusce elementum nisi elementum dui
+                  vehicula, vitae fermentum velit egestas.
+                </Typography>
+              </LeadBlock.Card.Content>
+              <LeadBlock.Card.Action>
+                <Button hasDarkBackground variant="outlined">
+                  Caption
+                </Button>
+              </LeadBlock.Card.Action>
+            </LeadBlock.Card.Section>
+            <LeadBlock.Card.Section>
+              <LeadBlock.Card.Heading hasUnderline>
+                Heading 2
+              </LeadBlock.Card.Heading>
+              <LeadBlock.Card.Content>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus ut egestas risus. Fusce elementum nisi elementum dui
+                  vehicula, vitae fermentum velit egestas.
+                </Typography>
+              </LeadBlock.Card.Content>
+              <LeadBlock.Card.Action>
+                <Typography>
+                  <Button hasDarkBackground variant="outlined">
+                    Caption
+                  </Button>
+                </Typography>
+              </LeadBlock.Card.Action>
+            </LeadBlock.Card.Section>
+          </LeadBlock.Card>
+        </LeadBlock>
+      </Tabs.TabPanel>
+    </Tabs>
+  );
+};
+
+export default ProductLeadBlock;
--- applications/head/src/templates/product-details-page.tsx
+++ applications/head/src/templates/product-details-page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 // import { graphql } from "gatsby";
 import Container from "@bmi/container";
+import Section from "@bmi/section";
 import Page, { Data as PageData } from "../components/Page";
 import { Data as SiteData } from "../components/Site";
 import ProductOverview, {
   Data as ProductOverviewData
 } from "../components/ProductOverview";
+import ProductLeadBlock from "../components/ProductLeadBlock";
 
 type Data = PageData & {
   productData: ProductOverviewData;
@@ -26,7 +28,9 @@ const ProductDetailsPage = ({ data }: Props) => {
       <Container>
         <ProductOverview data={productData} />
       </Container>
-      {/* LEAD BLOCK HERE */}
+      <Section>
+        <ProductLeadBlock />
+      </Section>
     </Page>
   );
 };
