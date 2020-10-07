e8078e410171db6d1dec097afcb4c377b77b906d
Henry Liu <thenrikie@hotmail.com>
2020-10-07 13:02:13 +0100

    feat(nba-card): add NBA Card component

---
+++ components/nba-card/src/__tests__/NBACard.test.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import NBACard from "../";
+import { render } from "@testing-library/react";
+
+describe("NbaCard component", () => {
+  it("renders correctly", () => {
+    const { container } = render(
+      <NBACard theme="blue-900" title="H4 Heading">
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+          condimentum nisi at turpis fringilla, non malesuada mi porta. Aliquam
+          ut eros in libero rutrum ullamcorper.
+        </div>
+      </NBACard>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("renders with footer", () => {
+    const { container } = render(
+      <NBACard theme="blue-900" title="H4 Heading" footer={<div>footer</div>}>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+          condimentum nisi at turpis fringilla, non malesuada mi porta. Aliquam
+          ut eros in libero rutrum ullamcorper.
+        </div>
+      </NBACard>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
