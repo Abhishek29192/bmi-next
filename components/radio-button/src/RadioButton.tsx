a0ae0e406ea1800e277fbe633a777f057fd21ed7
Yazan Alkheder <yazan@digital-detox.co.uk>
2020-10-11 01:34:17 +0100

    feat(radio-button): create RadioButton component

---
+++ components/radio-button/src/RadioButton.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import classnames from "classnames";
+import styles from "./RadioButton.module.scss";
+
+export type Props = {
+  name?: string;
+  value: string;
+  id?: string;
+  children: React.ReactNode;
+  before?: React.ReactNode;
+  className?: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+const RadioButton = ({
+  name,
+  value,
+  id = `radio-${name}-${value}`,
+  children,
+  before,
+  className,
+  style,
+  ...rest
+}: Props) => {
+  return (
+    <div className={classnames(styles["RadioButton"], className)} style={style}>
+      <input
+        id={id}
+        className={styles["input"]}
+        type="radio"
+        name={name}
+        value={value}
+        {...rest}
+      />
+      <label className={styles["label"]} htmlFor={id}>
+        {before ? before : null}
+        <ButtonBase tabIndex={-1} component="div" className={styles["text"]}>
+          {children}
+        </ButtonBase>
+      </label>
+    </div>
+  );
+};
+
+export default RadioButton;
---
+++ components/radio-button/src/index.ts
@@ -0,0 +1,5 @@
+import RadioButton from "./RadioButton";
+
+export { Props } from "./RadioButton";
+
+export default RadioButton;
