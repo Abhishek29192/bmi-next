0e8ec42a8b09ed8bd99bc672364563cd8687f6fc
Henry Liu <thenrikie@hotmail.com>
2020-10-12 13:58:09 +0100

    feat(head): update sections test

--- applications/head/src/components/__tests__/Sections.test.tsx
+++ applications/head/src/components/__tests__/Sections.test.tsx
@@ -53,10 +53,123 @@ describe("Sections component", () => {
           cta: null
         },
         isReversed: null
+      },
+      {
+        __typename: "ContentfulLeadBlockSection",
+        text: {
+          json: {
+            data: {},
+            content: [
+              {
+                data: {},
+                content: [
+                  {
+                    data: {},
+                    marks: [],
+                    value: "lead block heading",
+                    nodeType: "text"
+                  }
+                ],
+                nodeType: BLOCKS.HEADING_2
+              },
+              {
+                data: {},
+                content: [
+                  {
+                    data: {},
+                    marks: [],
+                    value: "lead block content",
+                    nodeType: "text"
+                  }
+                ],
+                nodeType: BLOCKS.PARAGRAPH
+              }
+            ],
+            nodeType: BLOCKS.DOCUMENT
+          }
+        },
+        link: {
+          __typename: "ContentfulLink",
+          id: "00000000-0000-0000-0000-000000000000",
+          label: "lead block cta label",
+          icon: null,
+          isLabelHidden: null,
+          url: null,
+          linkedPage: {
+            slug: "contact-us"
+          }
+        },
+        cardTheme: "pearl",
+        cardSections: [
+          {
+            id: "00000000-0000-0000-0000-000000000000",
+            hasUnderline: true,
+            title: "card section 1 title",
+            description: {
+              description: "card section 1 description"
+            },
+            link: {
+              __typename: "ContentfulLink",
+              id: "00000000-0000-0000-0000-000000000000",
+              label: "card section 1 link label",
+              icon: null,
+              isLabelHidden: null,
+              url: "/",
+              linkedPage: null
+            },
+            linkType: null
+          }
+        ]
+      },
+      {
+        __typename: "ContentfulCardCollectionSection",
+        title: "card collection section title",
+        cardLabel: "Card Label",
+        cardType: "Next Best Action Card",
+        cards: [
+          {
+            __typename: "ContentfulPromo",
+            title: "promo title",
+            subtitle: "promo subtitle",
+            featuredImage: null,
+            cta: {
+              __typename: "ContentfulLink",
+              id: "00000000-0000-0000-0000-000000000000",
+              label: "Contact us",
+              icon: null,
+              isLabelHidden: null,
+              url: null,
+              linkedPage: {
+                slug: "contact-us"
+              }
+            },
+            id: "00000000-0000-0000-0000-000000000000"
+          }
+        ]
+      },
+      {
+        __typename: "ContentfulCardCollectionSection",
+        title: "card section title 2",
+        cardLabel: "Go to {{title}}",
+        cardType: "Product Overview Card",
+        cards: [
+          {
+            __typename: "ContentfulSimplePage",
+            id: "00000000-0000-0000-0000-000000000000",
+            slug: "slug",
+            title: "page title",
+            subtitle: "page subtitle",
+            featuredImage: {
+              resize: {
+                src: "//image-url"
+              }
+            }
+          }
+        ]
       }
     ];
 
     const { container } = render(<Sections data={data} />);
-    expect(container.firstChild).toMatchSnapshot();
+    expect(container.children).toMatchSnapshot();
   });
 });
