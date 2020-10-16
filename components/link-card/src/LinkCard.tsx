ea6f54c38e5a7522e1964f4978fd1f4d26270f81
Dominik Sipowicz <dominik.sipowicz@gmail.com>
2020-10-16 09:20:02 +0100

    feat(link-card): create LinkCard component
    feat(link-card): click handle and rotate animation
    fix(link-card): only close button clicable as per design
    docs(link-card): package deps
    feat(link-card): address MR feedback

---
+++ components/link-card/src/LinkCard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import classnames from "classnames";
+import ChevronRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import Card from "@bmi/card";
+import Typography from "@bmi/typography";
+import styles from "./LinkCard.module.scss";
+
+type Props = {
+  title: React.ReactNode;
+  subtitle?: React.ReactNode;
+  children: React.ReactNode;
+  isOpen?: boolean;
+  onClick?: () => void;
+  onCloseClick?: () => void;
+};
+
+const LinkCard = ({
+  title,
+  subtitle,
+  isOpen = false,
+  children,
+  onClick,
+  onCloseClick
+}: Props) => {
+  return (
+    <Card
+      className={classnames(styles["LinkCard"], {
+        [styles[`LinkCard--selected`]]: isOpen
+      })}
+      onClick={!isOpen ? onClick : null}
+    >
+      <div className={styles["item"]}>
+        <Typography gutterBottom variant="h6">
+          {title}
+        </Typography>
+        <Typography className={styles["subtitle"]}>{subtitle}</Typography>
+      </div>
+      <div className={styles["icon"]}>
+        <ChevronRightIcon onClick={isOpen ? onCloseClick : null} />
+      </div>
+      {isOpen && <div className={styles["details"]}>{children}</div>}
+    </Card>
+  );
+};
+
+export default LinkCard;
