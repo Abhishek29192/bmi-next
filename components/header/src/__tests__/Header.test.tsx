8ac16dbd3647a4345aee4515a19a8e36ab0477e1
Dan Edwards <igneosaur@gmail.com>
2020-10-19 12:26:22 +0100

    test(header): update tests

--- components/header/src/__tests__/Header.test.tsx
+++ components/header/src/__tests__/Header.test.tsx
@@ -1,3 +1,4 @@
+import Arrow from "@bmi/icon/src/svgs/Arrow.svg";
 import { languages } from "@bmi/language-selection";
 import { render } from "@testing-library/react";
 import React from "react";
@@ -36,6 +37,11 @@ const navigation = [
           },
           { label: "Turf Roofs" }
         ]
+      },
+      {
+        label: "View all our products",
+        icon: Arrow,
+        hasSeparator: true
       },
       { label: "Wall" }
     ]
