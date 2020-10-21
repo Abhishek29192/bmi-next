a7702e5e57756e1bd91503752048249f7f749bca
Dominik Widomski <dominik@digital-detox.co.uk>
2020-10-21 11:33:24 +0100

    feat(company-details): create CompanyDetails component

---
+++ components/company-details/src/CompanyDetails.tsx
@@ -0,0 +1,162 @@
+import React from "react";
+import Button from "@bmi/button";
+import Typography from "@bmi/typography";
+import AnchorLink, { ClickableAction } from "@bmi/anchor-link";
+import Icon from "@bmi/icon";
+import Logo from "@bmi/logo";
+import RoofProExpertLogo from "@bmi/logo/svgs/RoofProExpert.svg";
+import RoofProPartnerLogo from "@bmi/logo/svgs/RoofProPartnerSmall.svg";
+import RoofProEliteLogo from "@bmi/logo/svgs/RoofProElite.svg";
+import styles from "./CompanyDetails.module.scss";
+
+import {
+  LocationOn,
+  Phone,
+  Mail,
+  Directions,
+  Public
+} from "@material-ui/icons";
+
+type RoofProLevel = "expert" | "partner" | "elite";
+
+type DetailTypeWithIcons =
+  | "address"
+  | "phone"
+  | "email"
+  | "distance"
+  | "website";
+
+type DetailType = DetailTypeWithIcons | "cta" | "content";
+
+type DetailProps =
+  | {
+      type: "roofProLevel";
+      label: React.ReactNode;
+      level: RoofProLevel;
+    }
+  | {
+      type: DetailType;
+      text: React.ReactNode;
+      label: React.ReactNode;
+      action?: ClickableAction;
+    };
+
+type Props = {
+  name: React.ReactNode;
+  details: readonly DetailProps[];
+  children: React.ReactNode;
+};
+
+const typeToIconMap: Record<DetailTypeWithIcons, SVGImport> = {
+  address: LocationOn,
+  phone: Phone,
+  email: Mail,
+  distance: Directions,
+  website: Public
+};
+
+const DetailsItem = (props: DetailProps) => {
+  const icon = typeToIconMap[props.type];
+
+  if (props.type === "cta") {
+    const { action, label } = props;
+
+    return (
+      <div className={[styles["row"], styles["row--cta"]].join(", ")}>
+        <Button
+          className={styles["cta-button"]}
+          variant="contained"
+          action={action}
+        >
+          {label}
+        </Button>
+      </div>
+    );
+  }
+
+  if (props.type === "content") {
+    const { label, text } = props;
+
+    return (
+      <div className={styles["row"]}>
+        <dt className={styles["term"]}>
+          <span className={styles["label"]}>{label}</span>
+        </dt>
+        <dd className={styles["description"]}>{text}</dd>
+      </div>
+    );
+  }
+
+  if (props.type === "roofProLevel") {
+    const { label, level } = props;
+    const iconSourceMap: Record<RoofProLevel, SVGImport> = {
+      expert: RoofProExpertLogo,
+      partner: RoofProPartnerLogo,
+      elite: RoofProEliteLogo
+    };
+
+    return (
+      <div className={styles["row"]}>
+        <dt className={styles["term"]}>
+          <span className={styles["label"]}>{label}</span>
+        </dt>
+        <dd className={styles["description"]}>
+          <Logo
+            source={iconSourceMap[level]}
+            className={styles[`roofProLevelIcon--${level}`]}
+          />
+          <span className={styles["accessibility-label"]}>{level}</span>
+        </dd>
+      </div>
+    );
+  }
+
+  const { type, label, text, action } = props;
+  const WrapperElement = type === "address" ? "address" : "div";
+
+  return (
+    <WrapperElement className={styles["row"]}>
+      <dt className={styles["term"]}>
+        {icon ? <Icon source={icon} className={styles["icon"]} /> : null}
+        <Typography
+          className={[styles["label"], styles["accessibility-label"]].join(
+            ", "
+          )}
+        >
+          {label}
+        </Typography>
+      </dt>
+      <dd className={styles["description"]}>
+        {action ? (
+          <AnchorLink action={action} className={styles["link"]}>
+            {text}
+          </AnchorLink>
+        ) : (
+          text
+        )}
+      </dd>
+    </WrapperElement>
+  );
+};
+
+const CompanyDetails = ({ name, details, children }: Props) => {
+  return (
+    <div className={styles["CompanyDetails"]}>
+      <Typography variant="h5" className={styles["title"]}>
+        {name}
+      </Typography>
+      <div className={styles["scrollableContent"]}>
+        {details.length ? (
+          <dl className={styles["list"]}>
+            {details.map((detail, index) => (
+              <DetailsItem key={index} {...detail} />
+            ))}
+          </dl>
+        ) : null}
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default CompanyDetails;
---
+++ components/company-details/src/__tests__/CompanyDetails.test.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import CompanyDetails from "../";
+import { render } from "@testing-library/react";
+
+describe("CompanyDetails component", () => {
+  it("renders with no details", () => {
+    const { container } = render(
+      <CompanyDetails name="Name of the company" details={[]}>
+        <p>Summary</p>
+      </CompanyDetails>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("renders all types of details", () => {
+    const { container } = render(
+      <CompanyDetails
+        name="Name of the company"
+        details={[
+          {
+            type: "address",
+            text: "full address",
+            label: "Address"
+          },
+          {
+            type: "distance",
+            text: "12.56km",
+            label: "Distance"
+          },
+          {
+            type: "cta",
+            text: "Get directions",
+            action: { model: "htmlLink", href: "https://google.com" },
+            label: "Get directions"
+          },
+          {
+            type: "phone",
+            text: "67 97 90 99",
+            action: { model: "htmlLink", href: "tel:000000000000" },
+            label: "Telephone"
+          },
+          {
+            type: "email",
+            text: "hello@roofingcompany.com",
+            action: {
+              model: "htmlLink",
+              href: "mailto:hello@roofingcompany.com"
+            },
+            label: "Email"
+          },
+          {
+            type: "website",
+            text: "Visit website",
+            action: { model: "htmlLink", href: "https://roofingcompany.com" },
+            label: "Website"
+          },
+          {
+            type: "content",
+            label: "Type of roof",
+            text: <b>Flat</b>
+          },
+          {
+            type: "roofProLevel",
+            label: "BMI RoofPro Level",
+            level: "expert"
+          },
+          {
+            type: "roofProLevel",
+            label: "BMI RoofPro Level",
+            level: "elite"
+          },
+          {
+            type: "roofProLevel",
+            label: "BMI RoofPro Level",
+            level: "partner"
+          }
+        ]}
+      >
+        <p>Summary</p>
+      </CompanyDetails>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
