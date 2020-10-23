18537f9582efdd474888bf23a63b04b165e9f676
Alex Canessa <canessa.alex@gmail.com>
2020-10-23 21:16:51 +0200

    feat(migrate): add Product Lister Page references

---
+++ libraries/migrate/migrations/link/20201023190321-add-product-lister-page-validation.js
@@ -0,0 +1,38 @@
+module.exports.description = "Add Product Lister Page reference to Link";
+
+module.exports.up = (migration) => {
+  const link = migration.editContentType("link");
+
+  link.editField("linkedPage", {
+    items: {
+      type: "Link",
+      validations: [
+        {
+          linkContentType: [
+            "contactUsPage",
+            "page",
+            "teamPage",
+            "productListerPage"
+          ]
+        }
+      ],
+      linkType: "Entry"
+    }
+  });
+};
+
+module.exports.down = (migration) => {
+  const link = migration.editContentType("link");
+
+  link.editField("linkedPage", {
+    items: {
+      type: "Link",
+      validations: [
+        {
+          linkContentType: ["contactUsPage", "page", "teamPage"]
+        }
+      ],
+      linkType: "Entry"
+    }
+  });
+};
