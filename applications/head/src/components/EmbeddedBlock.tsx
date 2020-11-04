7743fb90dbd518300a190a8dda2e1d040458f526
Alex Canessa <canessa.alex@gmail.com>
2020-11-04 15:42:36 +0000

    feat(head): handle links as RichText block

--- applications/head/src/components/EmbeddedBlock.tsx
+++ applications/head/src/components/EmbeddedBlock.tsx
@@ -1,21 +1,33 @@
 import React from "react";
 import EmbeddedTable from "./EmbeddedTable";
+import EmbeddedLink from "./EmbeddedLink";
 import { Block } from "@contentful/rich-text-types";
 
-const EmbeddedBlock = ({ node }: { node: Block }) => {
+export type Settings = {
+  theme?: "primary" | "secondary";
+  backgroundTheme?: "light" | "dark";
+};
+
+const EmbeddedBlock = ({
+  node,
+  ...settings
+}: {
+  node: Block;
+} & Settings) => {
   // NOTE: No type for this from Contentful, protecting in case it's missing in JSON
   const contentType = node.data?.target?.sys?.contentType?.sys?.contentful_id;
   const fields = node.data?.target?.fields;
 
   const Component = {
-    table: EmbeddedTable
+    table: EmbeddedTable,
+    link: EmbeddedLink
   }[contentType];
 
   if (!Component) {
     return null;
   }
 
-  return <Component fields={fields} />;
+  return <Component fields={fields} {...settings} />;
 };
 
 export default EmbeddedBlock;
