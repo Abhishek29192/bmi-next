4ba09518b59d72b73b655bffa780856966b0c3c3
Alex Canessa <canessa.alex@gmail.com>
2020-11-05 12:32:25 +0000

    fix(container): apply overflow hidden on small devices

--- components/container/src/Container.tsx
+++ components/container/src/Container.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import MUIContainer, { ContainerProps } from "@material-ui/core/Container";
+import classnames from "classnames";
+import styles from "./Container.module.scss";
 
-const Container = (props: ContainerProps) => {
-  return <MUIContainer maxWidth="xl" {...props} />;
+const Container = ({
+  className,
+  maxWidth = "xl",
+  ...props
+}: ContainerProps) => {
+  return (
+    <MUIContainer
+      maxWidth={maxWidth}
+      className={classnames(className, styles["Container"], {
+        [styles[`Container--${maxWidth}`]]: maxWidth !== "xl"
+      })}
+      {...props}
+    />
+  );
 };
 
 export default Container;
