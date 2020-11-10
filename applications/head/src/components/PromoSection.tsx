b26e22408d83973a857d3749375e546bbd3265d4
Alex Canessa <canessa.alex@gmail.com>
2020-11-10 17:43:53 +0000

    feat(head): integrate promo section

---
+++ applications/head/src/components/PromoSection.tsx
@@ -0,0 +1,46 @@
+import React, { useContext } from "react";
+import { graphql } from "gatsby";
+import PromoSection from "@bmi/promo-section";
+import Button from "@bmi/button";
+import { Data as PromoData } from "./Promo";
+import { getClickableActionFromUrl } from "./Link";
+import { SiteContext } from "./Site";
+import styles from "./styles/PromoSection.module.scss";
+
+export type Data = PromoData;
+
+const IntegratedPromoSection = ({ data }: { data: Data }) => {
+  const { countryCode } = useContext(SiteContext);
+  const { title, subtitle, featuredImage, cta } = data;
+
+  return (
+    <PromoSection
+      title={title}
+      imageSource={featuredImage?.file.url}
+      className={styles["PromoSection"]}
+    >
+      {subtitle}
+      {cta && (
+        <div className={styles["link"]}>
+          <Button
+            action={getClickableActionFromUrl(
+              cta.linkedPage,
+              cta.url,
+              countryCode
+            )}
+          >
+            {cta.label}
+          </Button>
+        </div>
+      )}
+    </PromoSection>
+  );
+};
+
+export default IntegratedPromoSection;
+
+export const query = graphql`
+  fragment PromoSectionFragment on ContentfulSection {
+    ...PromoFragment
+  }
+`;
