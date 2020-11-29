a2da43302c368c8e315b2fc1a95340205943c5e8
Alex Canessa <canessa.alex@gmail.com>
2020-11-29 17:55:41 +0100

    feat(head): handle both PIM and CMS documents

--- applications/head/src/components/DocumentSimpleTableResults.tsx
+++ applications/head/src/components/DocumentSimpleTableResults.tsx
@@ -5,44 +5,66 @@ import Table from "@bmi/table";
 import Button from "@bmi/button";
 import Checkbox from "@bmi/checkbox";
 import Icon, { iconMap } from "@bmi/icon";
+import { Data as PIMDocumentData } from "./PIMDocument";
 import { Data as DocumentData } from "./Document";
 import { SiteContext } from "./Site";
 import { getClickableActionFromUrl } from "./Link";
 import styles from "./styles/DocumentSimpleTableResults.module.scss";
 
 type Props = {
-  documents: DocumentData[];
+  documents: (DocumentData | PIMDocumentData)[];
 };
 
-const fileIconsMap = {
+type Format = "application/pdf" | "image/jpg" | "image/jpeg" | "image/png";
+
+const fileIconsMap: Record<Format, React.ComponentType> = {
   "application/pdf": iconMap.FilePDF,
+  "image/jpg": iconMap.FileJPG,
   "image/jpeg": iconMap.FileJPEG,
   "image/png": iconMap.FilePNG
 };
 
-// TODO: consider global approach to BMI icons
-const iconStyle = {
-  width: "1em",
-  height: "1em"
+const mapAssetToFileDownload = (
+  data: DocumentData | PIMDocumentData
+): FileDownloadButtonProps => {
+  if (data.__typename === "PIMDocument") {
+    const { url, format, fileSize: size } = data;
+
+    return {
+      url,
+      // @ts-ignore: Format to string
+      format,
+      size
+    };
+  }
+
+  const {
+    asset: { file }
+  } = data;
+
+  return {
+    url: file.url,
+    // @ts-ignore: Format to string
+    format: file.contentType,
+    size: file.details.size
+  };
 };
 
-const FileDownloadButton = ({
-  url,
-  contentType,
-  details
-}: DocumentData["asset"]["file"]) => (
+type FileDownloadButtonProps = {
+  url: string;
+  format: Format;
+  size: number;
+};
+
+const FileDownloadButton = ({ url, format, size }: FileDownloadButtonProps) => (
   <Button
     action={getClickableActionFromUrl(null, null, null, `https:${url}`)}
     variant="text"
     startIcon={
-      <Icon
-        source={fileIconsMap[contentType]}
-        // TODO: consider responsibility of icon styles
-        style={iconStyle}
-      />
+      <Icon source={fileIconsMap[format]} className={styles["download-icon"]} />
     }
   >
-    {filesize(details.size)}
+    {filesize(size)}
   </Button>
 );
 
@@ -66,19 +88,20 @@ const DocumentSimpleTableResults = ({ documents }: Props) => {
         <Table.Head>
           <Table.Row>
             <Table.Cell>
-              {getMicroCopy("documentDownloadLibrary.product")}
+              {getMicroCopy("documentLibrary.headers.title")}
             </Table.Cell>
             <Table.Cell>
-              {getMicroCopy("documentDownloadLibrary.download")}
+              {getMicroCopy("documentLibrary.headers.download")}
             </Table.Cell>
             <Table.Cell>
-              {getMicroCopy("documentDownloadLibrary.add")}
+              {getMicroCopy("documentLibrary.headers.add")}
             </Table.Cell>
           </Table.Row>
         </Table.Head>
         <Table.Body>
           {documents.map((document, index) => {
-            const { title, asset } = document;
+            const { id, title } = document;
+            const assetData = mapAssetToFileDownload(document);
 
             return (
               <Table.Row
@@ -94,14 +117,14 @@ const DocumentSimpleTableResults = ({ documents }: Props) => {
                   {title}
                 </Table.Cell>
                 <Table.Cell className={styles["table-cell"]} align="center">
-                  <FileDownloadButton {...asset.file} />
+                  <FileDownloadButton {...assetData} />
                 </Table.Cell>
                 <Table.Cell className={styles["table-cell"]} align="center">
                   <Checkbox
-                    name={asset.file.fileName}
+                    name={id}
                     inputProps={{
                       "aria-label": `${getMicroCopy(
-                        "documentDownloadLibrary.download"
+                        "documentLibrary.download"
                       )} ${title}`
                     }}
                     onChange={(checked: boolean) =>
