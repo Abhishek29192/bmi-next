c8a5cd48efdd9c6a3f75f0ef4c8eecda681c9a58
Dan Edwards <igneosaur@gmail.com>
2020-11-30 13:06:43 +0000

    feat(head): implement document zip download functionality

--- applications/head/src/components/DocumentResults.tsx
+++ applications/head/src/components/DocumentResults.tsx
@@ -1,8 +1,10 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { graphql } from "gatsby";
 import { Data as DocumentData } from "./Document";
 import { Data as PIMDocumentData } from "./PIMDocument";
 import DocumentResultsFooter from "../components/DocumentResultsFooter";
+import { downloadAs } from "../utils/client-download";
 import DocumentSimpleTableResults from "./DocumentSimpleTableResults";
 import DocumentTechnicalTableResults from "./DocumentTechnicalTableResults";
 import DocumentCardsResults from "./DocumentCardsResults";
@@ -33,6 +35,43 @@ const DocumentResults = ({ data, format }: Props) => {
     return null;
   }
 
+  const handleDownloadClick = async (list: Record<string, any>) => {
+    const listValues = Object.values(list).filter(Boolean);
+    const currentTime = new Date().getTime();
+
+    if (listValues.length === 0 || format === "cards") {
+      return () => {};
+    }
+
+    try {
+      if (!process.env.DOWNLOAD_ZIP_FUNCTION_URL) {
+        throw Error(
+          "`DOWNLOAD_ZIP_FUNCTION_URL` missing in environment config"
+        );
+      }
+
+      const requestBody = listValues.map(
+        ({ __typename, asset, title: name, url }) => ({
+          href:
+            __typename === "ContentfulDocument"
+              ? `https:${asset.file.url}`
+              : `https:${url}`,
+          name
+        })
+      );
+
+      const response = await axios.post(
+        process.env.DOWNLOAD_ZIP_FUNCTION_URL,
+        requestBody,
+        { responseType: "blob" }
+      );
+
+      return downloadAs(response.data, `BMI_${currentTime}.zip`);
+    } catch (error) {
+      console.error("DocumentResults", error); // eslint-disable-line
+    }
+  };
+
   return (
     <>
       <ResultsComponent
@@ -43,7 +82,7 @@ const DocumentResults = ({ data, format }: Props) => {
       <DocumentResultsFooter
         page={page}
         count={count}
-        onDownloadClick={format !== "cards" ? () => {} : undefined}
+        onDownloadClick={handleDownloadClick}
         onPageChange={(_, page) => setPage(page)}
       />
     </>
