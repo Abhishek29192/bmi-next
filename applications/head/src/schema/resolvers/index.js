c211e8979402df6f8f49c4049befe961521fdc0a
Alex Canessa <canessa.alex@gmail.com>
2020-11-30 13:55:40 +0100

    feat(head): add documents to product schema

--- applications/head/src/schema/resolvers/index.js
+++ applications/head/src/schema/resolvers/index.js
@@ -1,9 +1,13 @@
 "use strict";
 
 const documents = require("./documents");
+const pimDocuments = require("./pim-documents");
 
 module.exports = {
   ContentfulDocumentLibraryPage: {
     documents
+  },
+  Products: {
+    documents: pimDocuments
   }
 };
---
+++ applications/head/src/schema/resolvers/pim-documents.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const _ = require("lodash");
+const { getFormatFromFileName } = require("./utils/documents");
+const {
+  generateIdFromString,
+  generateDigestFromData
+} = require("./utils/encryption");
+
+module.exports = {
+  type: ["PIMDocument"],
+  async resolve(source, args, context) {
+    const assetTypes = await context.nodeModel.getAllNodes(
+      { type: "ContentfulAssetType" },
+      { connectionType: "ContentfulAssetType" }
+    );
+
+    if (!source.assets || !source.assets.length) {
+      return [];
+    }
+
+    return source.assets
+      .map((asset) => {
+        const id = generateIdFromString(source.name + asset.name);
+        const assetType = _.find(assetTypes, { pimCode: asset.assetType });
+
+        if (!assetType) {
+          return;
+        }
+
+        const fieldData = {
+          title: `${source.name} ${assetType.name}`,
+          url: asset.url,
+          assetType___NODE: assetType && assetType.id,
+          fileSize: asset.fileSize,
+          product___NODE: source.id,
+          format: getFormatFromFileName(asset.realFileName)
+        };
+
+        return {
+          id,
+          ...fieldData,
+          parent: source.id,
+          children: [],
+          internal: {
+            type: "PIMDocument",
+            owner: "@bmi/resolvers",
+            contentDigest: generateDigestFromData(fieldData)
+          }
+        };
+      })
+      .filter(Boolean);
+  }
+};
