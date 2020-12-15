2d0a179bedaca8fa8bac74a79d4d2ce7b1a21571
Alex Canessa <canessa.alex@gmail.com>
2020-12-15 20:33:32 +0100

    fix(head): use polyfill for match all function

--- applications/head/src/components/FormInputs.tsx
+++ applications/head/src/components/FormInputs.tsx
@@ -7,6 +7,7 @@ import TextField from "@bmi/text-field";
 import Upload from "@bmi/upload";
 import { SiteContext } from "./Site";
 import AnchorLink from "@bmi/anchor-link";
+import matchAll from "string.prototype.matchall";
 
 const InputTypes = [
   "text",
@@ -34,7 +35,7 @@ export const convertMarkdownLinksToAnchorLinks = (
     return;
   }
 
-  const matches = [...source.matchAll(/\[([^\]]+)\]\(([^)]+)\)/g)];
+  const matches = [...matchAll(source, /\[([^\]]+)\]\(([^)]+)\)/g)];
 
   if (!matches || !matches.length) {
     return source;
