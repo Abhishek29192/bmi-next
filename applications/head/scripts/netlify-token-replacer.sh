b69079bf75eb8e913cb67e1920f00e996eb34418
Mahesh Warnasooriya <mahesh.warnasooriya@bmigroup.com>
2021-01-18 10:48:23 +0000

    ci(head): add env variable values to netlify.toml

---
+++ applications/head/scripts/netlify-token-replacer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Replace tokens in netlify.toml file with corresponding environment variable values.
+
+sed -i 's^CONTENT_SECURITY_POLICY^'"${CONTENT_SECURITY_POLICY}"'^' netlify.toml
