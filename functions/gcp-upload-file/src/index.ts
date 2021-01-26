3a7d3090794232c67b82b3f9ae587c4ea30fa340
Mahesh Warnasooriya <mahesh.warnasooriya@bmigroup.com>
2021-01-26 09:27:53 +0000

    feat(gcp-upload-file): use secrets manger

--- functions/gcp-upload-file/src/index.ts
+++ functions/gcp-upload-file/src/index.ts
@@ -2,18 +2,40 @@ import type { HttpFunction } from "@google-cloud/functions-framework/build/src/f
 import { Buffer } from "buffer";
 import { createClient } from "contentful-management";
 import { config } from "dotenv";
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
 
 config({
   path: `${__dirname}/../.env.${process.env.NODE_ENV || "development"}`
 });
 
-export const upload: HttpFunction = async (request, response) => {
-  const {
-    CONTENTFUL_ENVIRONMENT,
-    CONTENTFUL_SPACE_ID,
-    CONTENTFUL_MANAGEMENT_ACCESS_TOKEN: accessToken
-  } = process.env;
+const {
+  CONTENTFUL_ENVIRONMENT,
+  CONTENTFUL_SPACE_ID,
+  SECRET_MAN_GCP_PROJECT_NAME,
+  CONTENTFUL_MANAGEMENT_TOKEN_SECRET
+} = process.env;
 
+let contentfulEnvironmentCache;
+const secretManagerClient = new SecretManagerServiceClient();
+
+const getContentfulEnvironment = async () => {
+  if (!contentfulEnvironmentCache) {
+    const managementTokenSecret = await secretManagerClient.accessSecretVersion(
+      {
+        name: `projects/${SECRET_MAN_GCP_PROJECT_NAME}/secrets/${CONTENTFUL_MANAGEMENT_TOKEN_SECRET}/versions/latest`
+      }
+    );
+    const managementToken = managementTokenSecret[0].payload.data.toString();
+    const client = createClient({ accessToken: managementToken });
+    const space = await client.getSpace(CONTENTFUL_SPACE_ID);
+    contentfulEnvironmentCache = await space.getEnvironment(
+      CONTENTFUL_ENVIRONMENT
+    );
+  }
+  return contentfulEnvironmentCache;
+};
+
+export const upload: HttpFunction = async (request, response) => {
   response.set("Access-Control-Allow-Origin", "*");
 
   if (request.method === "OPTIONS") {
@@ -24,9 +46,7 @@ export const upload: HttpFunction = async (request, response) => {
     return response.status(204).send("");
   } else {
     try {
-      const client = createClient({ accessToken });
-      const space = await client.getSpace(CONTENTFUL_SPACE_ID);
-      const environment = await space.getEnvironment(CONTENTFUL_ENVIRONMENT);
+      const environment = await getContentfulEnvironment();
 
       if (!(request.body instanceof Buffer)) {
         throw Error("Endpoint only accepts file buffers");
