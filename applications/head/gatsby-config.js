34ac88c21c6751c4dee400c8ef7b42e27d2bf971
Hitesh Khatri <Hitesh.khatri@bmigroup.com>
2021-02-01 12:48:37 +0000

    fix(head): add favicons

--- applications/head/gatsby-config.js
+++ applications/head/gatsby-config.js
@@ -32,7 +32,107 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`
+        display: `minimal-ui`,
+        include_favicon: true,
+        cache_busting_mode: `query`,
+        icon: `src/images/favicon.png`,
+        icons: [
+          {
+            src: `icons/icon-16x16.png`,
+            sizes: `16x16`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-24x24.png`,
+            sizes: `24x24`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-32x32.png`,
+            sizes: `32x32`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-48x48.png`,
+            sizes: `48x48`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-57x57.png`,
+            sizes: `57x57`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-60x60.png`,
+            sizes: `60x60`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-64x64.png`,
+            sizes: `64x64`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-72x72.png`,
+            sizes: `72x72`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-76x76.png`,
+            sizes: `76x76`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-96x96.png`,
+            sizes: `96x96`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-114x114.png`,
+            sizes: `114x114`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-120x120.png`,
+            sizes: `120x120`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-128x128.png`,
+            sizes: `128x128`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-144x144.png`,
+            sizes: `144x144`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-152x152.png`,
+            sizes: `152x152`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-180x180.png`,
+            sizes: `180x180`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-256x256.png`,
+            sizes: `256x256`,
+            type: `image/png`
+          },
+          {
+            src: `icons/icon-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`
+          }
+        ]
       }
     },
     {
