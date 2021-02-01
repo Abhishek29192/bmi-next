ba00eec853563eefb0c45c0c39c7490f31099fa1
Alex Canessa <canessa.alex@gmail.com>
2021-02-01 18:56:50 +0100

    fix(head): make netlify token replacer working on MacOS

--- applications/head/scripts/netlify-token-replacer.sh
+++ applications/head/scripts/netlify-token-replacer.sh
@@ -2,4 +2,4 @@
 
 # Replace tokens in netlify.toml file with corresponding environment variable values.
 
-sed -i 's^CONTENT_SECURITY_POLICY^'"${CONTENT_SECURITY_POLICY}"'^' netlify.toml
+sed -i '' 's^CONTENT_SECURITY_POLICY^'"${CONTENT_SECURITY_POLICY}"'^' netlify.toml
