c038536eac37c37e8222bd8e7f3f6695c458bdf6
Dan Edwards <igneosaur@gmail.com>
2021-02-08 09:53:00 +0000

    feat(head): implement new GoogleApi context into section

--- applications/head/src/components/ServiceLocatorSection.tsx
+++ applications/head/src/components/ServiceLocatorSection.tsx
@@ -1,20 +1,24 @@
+/* global google */
 import Autocomplete from "@bmi/autocomplete";
 import CompanyDetails, {
   DetailProps,
   RoofProLevel
 } from "@bmi/company-details";
 import GeolocationButton from "@bmi/geolocation-button";
-import GoogleAutocomplete, {
-  GeocoderResult as GoogleGeocoderResult
-} from "@bmi/google-autocomplete";
-import { GoogleLatLng } from "@bmi/google-map";
+import GoogleApi, {
+  GeocoderResult as GoogleGeocoderResult,
+  Google,
+  LatLngLiteral as GoogleLatLng,
+  loadGoogleApi
+} from "@bmi/google-api";
+import GoogleAutocomplete from "@bmi/google-autocomplete";
 import Grid from "@bmi/grid";
 import LinkCard from "@bmi/link-card";
 import Section from "@bmi/section";
 import Tabs from "@bmi/tabs";
 import Typography from "@bmi/typography";
 import { graphql } from "gatsby";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { getClickableActionFromUrl, LinkData } from "./Link";
 import RichText, { RichTextData } from "./RichText";
 import { Data as RooferData } from "./Roofer";
@@ -31,10 +35,19 @@ export type Data = {
 
 const ServiceLocatorSection = ({ data }: { data: Data }) => {
   const { getMicroCopy, countryCode } = useContext(SiteContext);
+  const [googleApi, setgoogleApi] = useState<Google>(null);
   const [selectedRoofer, setSelectedRoofer] = useState<string>("");
   const [activePosition, setActivePosition] = useState<GoogleLatLng>(null);
-  const googleApiKey = process.env.GATSBY_GOOGLE_API_KEY;
   const { label, body, roofers } = data;
+
+  const initialise = async () => {
+    await loadGoogleApi(process.env.GATSBY_GOOGLE_API_KEY, ["places"]);
+    setgoogleApi(typeof google !== "undefined" ? google : null);
+  };
+
+  useEffect(() => {
+    initialise();
+  }, []);
 
   const handleListCloseClick = () => setSelectedRoofer("");
 
@@ -122,97 +135,97 @@ const ServiceLocatorSection = ({ data }: { data: Data }) => {
       backgroundColor="white"
       className={styles["ServiceLocationSection"]}
     >
-      <Section.Title>{label}</Section.Title>
-      {body && <RichText document={body} />}
-      <Grid container spacing={3} className={styles["search"]}>
-        <Grid item xs={12} md={4}>
-          <Autocomplete
-            size="small"
-            id="company-autocomplete"
-            label={getMicroCopy("findARoofer.companyFieldLabel")}
-            className={styles["company-autocomplete"]}
-            onInputChange={(_, inputValue) => {
-              // @todo Company search
-            }}
-            options={
-              roofers && roofers.length ? roofers.map(({ name }) => name) : []
-            }
-          />
-          {googleApiKey && (
-            <>
-              <Typography className={styles["and-or-label"]}>
-                {getMicroCopy("findARoofer.andOr")}
-              </Typography>
-              <GoogleAutocomplete
-                size="small"
-                id="location-autocomplete"
-                apiKey={googleApiKey}
-                label={getMicroCopy("findARoofer.locationFieldLabel")}
-                className={styles["location-autocomplete"]}
-                onPlaceChange={handlePlaceChange}
-              />
-            </>
-          )}
-          <GeolocationButton
-            onPosition={({ coords }) =>
-              setActivePosition({ lat: coords.latitude, lng: coords.longitude })
-            }
-          >
-            {getMicroCopy("findARoofer.geolocationButton")}
-          </GeolocationButton>
+      <GoogleApi.Provider value={googleApi}>
+        <Section.Title>{label}</Section.Title>
+        {body && <RichText document={body} />}
+        <Grid container spacing={3} className={styles["search"]}>
+          <Grid item xs={12} md={4}>
+            <Autocomplete
+              size="small"
+              id="company-autocomplete"
+              label={getMicroCopy("findARoofer.companyFieldLabel")}
+              className={styles["company-autocomplete"]}
+              onInputChange={(_, inputValue) => {
+                // @todo Company search
+              }}
+              options={
+                roofers && roofers.length ? roofers.map(({ name }) => name) : []
+              }
+            />
+            <Typography className={styles["and-or-label"]}>
+              {getMicroCopy("findARoofer.andOr")}
+            </Typography>
+            <GoogleAutocomplete
+              size="small"
+              id="location-autocomplete"
+              label={getMicroCopy("findARoofer.locationFieldLabel")}
+              className={styles["location-autocomplete"]}
+              onPlaceChange={handlePlaceChange}
+            />
+            <GeolocationButton
+              onPosition={({ coords }) =>
+                setActivePosition({
+                  lat: coords.latitude,
+                  lng: coords.longitude
+                })
+              }
+            >
+              {getMicroCopy("findARoofer.geolocationButton")}
+            </GeolocationButton>
+          </Grid>
         </Grid>
-      </Grid>
-      <Tabs
-        initialValue="list"
-        className={styles["tab-bar"]}
-        theme="secondary"
-        isMobileOnly
-      >
-        <Tabs.TabPanel
-          md={4}
-          className={styles["tab-panel"]}
-          heading="List"
-          index="list"
+        <Tabs
+          initialValue="list"
+          className={styles["tab-bar"]}
+          theme="secondary"
+          isMobileOnly
         >
-          <div className={styles["list"]}>
-            {roofers && roofers.length ? (
-              roofers.map((roofer) => (
-                <LinkCard
-                  key={roofer.id}
-                  onClick={() => handleListClick(roofer)}
-                  onCloseClick={handleListCloseClick}
-                  isOpen={selectedRoofer === roofer.id}
-                  title={roofer.name}
-                  subtitle={roofer.address}
-                >
-                  <CompanyDetails details={getCompanyDetails(roofer)}>
-                    <Typography>{roofer.summary}</Typography>
-                  </CompanyDetails>
-                </LinkCard>
-              ))
-            ) : (
-              <div className={styles["no-results"]}>
-                <Typography variant="h4">
-                  {getMicroCopy("findARoofer.noResults.title")}
-                </Typography>
-                <Typography>
-                  {getMicroCopy("findARoofer.noResults.subtitle")}
-                </Typography>
-              </div>
-            )}
-          </div>
-        </Tabs.TabPanel>
-        <Tabs.TabPanel
-          md={8}
-          className={styles["tab-panel"]}
-          heading="Map"
-          index="map"
-        >
-          <div className={styles["map"]}>
-            {/* @todo: Use `activePosition` for map here */}
-          </div>
-        </Tabs.TabPanel>
-      </Tabs>
+          <Tabs.TabPanel
+            md={4}
+            className={styles["tab-panel"]}
+            heading={getMicroCopy("findARoofer.listLabel")}
+            index="list"
+          >
+            <div className={styles["list"]}>
+              {roofers && roofers.length ? (
+                roofers.map((roofer) => (
+                  <LinkCard
+                    key={roofer.id}
+                    onClick={() => handleListClick(roofer)}
+                    onCloseClick={handleListCloseClick}
+                    isOpen={selectedRoofer === roofer.id}
+                    title={roofer.name}
+                    subtitle={roofer.address}
+                  >
+                    <CompanyDetails details={getCompanyDetails(roofer)}>
+                      <Typography>{roofer.summary}</Typography>
+                    </CompanyDetails>
+                  </LinkCard>
+                ))
+              ) : (
+                <div className={styles["no-results"]}>
+                  <Typography variant="h4">
+                    {getMicroCopy("findARoofer.noResults.title")}
+                  </Typography>
+                  <Typography>
+                    {getMicroCopy("findARoofer.noResults.subtitle")}
+                  </Typography>
+                </div>
+              )}
+            </div>
+          </Tabs.TabPanel>
+          <Tabs.TabPanel
+            md={8}
+            className={styles["tab-panel"]}
+            heading={getMicroCopy("findARoofer.mapLabel")}
+            index="map"
+          >
+            <div className={styles["map"]}>
+              {/* @todo: Use `activePosition` for map here */}
+            </div>
+          </Tabs.TabPanel>
+        </Tabs>
+      </GoogleApi.Provider>
     </Section>
   );
 };
