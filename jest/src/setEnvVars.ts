f77f870fede3eab77ce01773dcb40d01593c4971
Hitesh Khatri <Hitesh.khatri@bmigroup.com>
2021-02-11 14:42:44 +0000

    feat(gcp-apsis-integration): apsis integration for Norway

--- jest/src/setEnvVars.ts
+++ jest/src/setEnvVars.ts
@@ -3,3 +3,17 @@ process.env.CONTENTFUL_MANAGEMENT_TOKEN_SECRET =
   "TEST_CONTENTFUL_MANAGEMENT_TOKEN_SECRET";
 process.env.CONTENTFUL_SPACE_ID = "TEST_CONTENTFUL_SPACE_ID";
 process.env.CONTENTFUL_ENVIRONMENT = "TEST_CONTENTFUL_ENVIRONMENT";
+
+process.env.APSIS_API_BASE_URL = "https://api.apsis.one";
+process.env.APSIS_CLIENT_ID = "";
+process.env.APSIS_CLIENT_SECTRET = "";
+
+process.env.APSIS_TARGET_SECTION = "usercreated.sections.fulq3a5aou";
+process.env.APSIS_TARGET_KEYSPACE = "com.keyspaces.email";
+process.env.APSIS_CONSENT_LIST_DESCRIMINATOR = "usercreated.targets.ylbz9hz52c";
+process.env.APSIS_TOPIC_DESCRIMINATOR =
+  "usercreated.topics.nyhetsbrev_-_nettside-rg4kf3kt24";
+process.env.APSIS_TARGET_EMAIL_ATTRIBUTE_ID = "9820534";
+process.env.APSIS_TARGET_GDPR_1_ATTRIBUTE_ID = "312460234";
+process.env.APSIS_TARGET_GDPR_2_ATTRIBUTE_ID = "312461234";
+process.env.APSIS_TARGET_CHANNEL = "com.channels.email";
