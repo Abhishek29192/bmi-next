0036d89595c94c3510f659db8a24e6a4bf51faa5
Benjamin Sproule <benjamin.sproule@bmigroup.com>
2021-02-16 19:18:31 +0000

    ci: fix builds to run for only relevant changes

--- applications/head/src/utils/__tests__/get-credentials-data.test.ts
+++ applications/head/src/utils/__tests__/get-credentials-data.test.ts
@@ -4,21 +4,35 @@ describe("getCredentialsData function", () => {
   it("returns a single space", () => {
     const mockEnv = {
       SPACE_ID: "YYYYYYYYYYYYY",
+      ACCESS_TOKEN: "XXXXXXXXXXXXXXXXXXXXXXX",
+      ENVIRONMENT: "AAAAAAAAAAAAAAAA"
+    };
+
+    expect(getCredentialsData(mockEnv)).toMatchSnapshot();
+  });
+
+  it("returns a single space with default ENVIRONMENT", () => {
+    const mockEnv = {
+      SPACE_ID: "YYYYYYYYYYYYY",
       ACCESS_TOKEN: "XXXXXXXXXXXXXXXXXXXXXXX"
     };
 
     expect(getCredentialsData(mockEnv)).toMatchSnapshot();
   });
+
   it("returns multiple spaces", () => {
     const mockEnv = {
       SPACE_ID: "YYYYYYYYYYYYY",
       ACCESS_TOKEN: "XXXXXXXXXXXXXXXXXXXXXXX",
+      ENVIRONMENT: "AAAAAAAAAAAAAAAA",
       SPACE_ID_1: "ZZZZZZZZZZZZ",
-      ACCESS_TOKENN: "NNNNNNNNNNNNNNNNNNNNNNN"
+      ACCESS_TOKENN: "NNNNNNNNNNNNNNNNNNNNNNN",
+      ENVIRONMENT_1: "BBBBBBBBBBBBBBBBB"
     };
 
     expect(getCredentialsData(mockEnv)).toMatchSnapshot();
   });
+
   it("throws an error if no credentials are set", () => {
     const mockEnv = {
       NODE_ENV: "testing"
