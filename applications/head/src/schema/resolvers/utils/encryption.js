6c14b0dd9d2639f33e5499c967221dcbbf8b68e7
Alex Canessa <canessa.alex@gmail.com>
2021-03-02 19:10:06 +0100

    fix(head): use correct id for variant paths

--- applications/head/src/schema/resolvers/utils/encryption.js
+++ applications/head/src/schema/resolvers/utils/encryption.js
@@ -3,10 +3,13 @@
 const crypto = require("crypto");
 const MurmurHash3 = require("imurmurhash");
 
-exports.generateIdFromString = (string, useDate = true) =>
-  MurmurHash3(string + useDate ? new Date().getTime().toString : "")
+exports.generateIdFromString = (string, useDate = true) => {
+  const dateString = useDate ? new Date().getTime().toString() : "";
+
+  return MurmurHash3(string + dateString)
     .result()
     .toString();
+};
 
 exports.generateDigestFromData = (data) =>
   crypto.createHash("md5").update(JSON.stringify(data)).digest("hex");
