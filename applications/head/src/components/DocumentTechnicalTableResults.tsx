6cd50dde52112e1720b5ad30d434e4f4c55572ac
Simon bone <simon.bone@bmigroup.com>
2021-03-03 14:56:30 +0000

    feat(head): style accordion document results for mobile

--- applications/head/src/components/DocumentTechnicalTableResults.tsx
+++ applications/head/src/components/DocumentTechnicalTableResults.tsx
@@ -3,6 +3,8 @@ import { groupBy, uniqBy } from "lodash";
 import classnames from "classnames";
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import InfoIcon from "@material-ui/icons/Info";
+import Accordion from "@bmi/accordion";
+import Typography from "@bmi/typography";
 import Table from "@bmi/table";
 import Icon, { iconMap } from "@bmi/icon";
 import Tooltip from "@bmi/tooltip";
@@ -135,127 +137,216 @@ const DocumentTechnicalTableResults = ({
 
   return (
     <div className={styles["DocumentTechnicalTableResults"]}>
-      <Table rowBgColorPattern="none">
-        <Table.Head>
-          <Table.Row className={styles["header-row"]}>
-            <Table.Cell>
-              {getMicroCopy("documentLibrary.headers.product")}
-            </Table.Cell>
-            {assetTypes.map((assetType, index) => {
-              return (
-                <Table.Cell
-                  key={`asset-type-${assetType.id}-${index}`}
-                  className={styles["asset-type-cell"]}
-                >
-                  {assetType.code}
-                  <AssetHeader assetType={assetType} />
-                </Table.Cell>
-              );
-            })}
-            <Table.Cell className={styles["all-files-header"]}>
-              <span className={styles["all-files-header-wrapper"]}>
-                <Icon
-                  source={iconMap.Download}
-                  className={styles["all-files-icon"]}
-                />
-                <span>{getMicroCopy("documentLibrary.headers.allFiles")}</span>
-              </span>
-            </Table.Cell>
-          </Table.Row>
-        </Table.Head>
-        <Table.Body>
+      <div className={styles["accordion-div"]}>
+        <Accordion>
           {documentsByProduct.map(([productName, assets], index) => {
             const key = `${
               assets.find(({ product }) => product.code)?.product.code
             }-${index}`;
             return (
-              <Table.Row
-                key={key}
-                className={classnames(styles["row"], {
-                  [styles["row--checked"]]: !!list[key]
-                })}
-              >
-                <Table.Cell>{productName}</Table.Cell>
+              <Accordion.Item key={key}>
+                <Accordion.Summary>{productName}</Accordion.Summary>
                 {assetTypes.map((assetType, index) => {
                   const asset = assets.find(
                     ({ assetType: { id } }) => id === assetType.id
                   );
 
-                  if (!asset) {
+                  if (asset) {
                     return (
-                      <Table.Cell
-                        key={`${productName}-missing-asset-${index}`}
-                        className={styles["align-center"]}
+                      <Accordion.Details
+                        key={`${productName}-asset-${asset.id}`}
+                        className={styles["accordion-details"]}
                       >
-                        <Icon
-                          source={iconMap.Cross}
-                          className={styles["no-document-icon"]}
-                        />
-                      </Table.Cell>
+                        <div className={styles["accordion-details-left-align"]}>
+                          {asset.__typename !== "PIMLinkDocument" ? (
+                            <Icon
+                              source={fileIconsMap[asset.format]}
+                              className={styles["format-icon"]}
+                            />
+                          ) : (
+                            <Icon
+                              source={iconMap.External}
+                              className={styles["external-link-icon"]}
+                            />
+                          )}
+                        </div>
+                        <div className={styles["accordion-details-left-align"]}>
+                          {assetType.code}
+                        </div>
+                        <div
+                          className={
+                            styles["accordion-details-left-align-smallmargin"]
+                          }
+                        >
+                          <AssetHeader assetType={assetType} />
+                        </div>
+                        <div
+                          className={styles["accordion-details-right-align"]}
+                        >
+                          {asset.__typename !== "PIMLinkDocument" ? (
+                            <Clickable
+                              model="download"
+                              href={asset.url}
+                              download={asset.title}
+                            >
+                              <Icon
+                                source={iconMap.Download}
+                                className={styles["all-files-icon"]}
+                              />
+                            </Clickable>
+                          ) : (
+                            <Button
+                              isIconButton
+                              variant="text"
+                              action={{
+                                model: "htmlLink",
+                                href: asset.url,
+                                target: "_blank",
+                                rel: "noopener noreferrer"
+                              }}
+                            >
+                              <Icon
+                                source={iconMap.Download}
+                                className={styles["all-files-icon"]}
+                              />
+                            </Button>
+                          )}
+                        </div>
+                      </Accordion.Details>
                     );
                   }
-
-                  return (
-                    <Table.Cell
-                      key={`${productName}-asset-${asset.id}`}
-                      className={styles["align-center"]}
-                    >
-                      {asset.__typename !== "PIMLinkDocument" ? (
-                        <Clickable
-                          model="download"
-                          href={asset.url}
-                          download={asset.title}
-                        >
-                          <Icon
-                            source={fileIconsMap[asset.format]}
-                            className={styles["format-icon"]}
-                          />
-                        </Clickable>
-                      ) : (
-                        <Button
-                          isIconButton
-                          variant="text"
-                          action={{
-                            model: "htmlLink",
-                            href: asset.url,
-                            target: "_blank",
-                            rel: "noopener noreferrer"
-                          }}
-                        >
-                          <Icon
-                            source={iconMap.External}
-                            className={styles["external-link-icon"]}
-                          />
-                        </Button>
-                      )}
-                    </Table.Cell>
-                  );
                 })}
-                <Table.Cell className={styles["align-center"]}>
-                  <DownloadList.Checkbox
-                    name={key}
-                    maxLimitReachedLabel={getMicroCopy(
-                      "documents.download.maxReached"
-                    )}
-                    ariaLabel={`${getMicroCopy(
-                      "documentLibrary.download"
-                    )} ${productName}`}
-                    value={assets.filter(
-                      ({ __typename }) => __typename !== "PIMLinkDocument"
-                    )}
-                    fileSize={assets.reduce((acc, curr) => {
-                      if (curr.__typename === "PIMLinkDocument") {
-                        return 0;
-                      }
-                      return acc + (curr.fileSize || 0);
-                    }, 0)}
-                  />
-                </Table.Cell>
-              </Table.Row>
+              </Accordion.Item>
             );
           })}
-        </Table.Body>
-      </Table>
+        </Accordion>
+      </div>
+
+      <div className={styles["table-div"]}>
+        <Table rowBgColorPattern="none">
+          <Table.Head>
+            <Table.Row className={styles["header-row"]}>
+              <Table.Cell>
+                {getMicroCopy("documentLibrary.headers.product")}
+              </Table.Cell>
+              {assetTypes.map((assetType, index) => {
+                return (
+                  <Table.Cell
+                    key={`asset-type-${assetType.id}-${index}`}
+                    className={styles["asset-type-cell"]}
+                  >
+                    {assetType.code}
+                    <AssetHeader assetType={assetType} />
+                  </Table.Cell>
+                );
+              })}
+              <Table.Cell className={styles["all-files-header"]}>
+                <span className={styles["all-files-header-wrapper"]}>
+                  <Icon
+                    source={iconMap.Download}
+                    className={styles["all-files-icon"]}
+                  />
+                  <span>
+                    {getMicroCopy("documentLibrary.headers.allFiles")}
+                  </span>
+                </span>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Head>
+          <Table.Body>
+            {documentsByProduct.map(([productName, assets], index) => {
+              const key = `${
+                assets.find(({ product }) => product.code)?.product.code
+              }-${index}`;
+              return (
+                <Table.Row
+                  key={key}
+                  className={classnames(styles["row"], {
+                    [styles["row--checked"]]: !!list[key]
+                  })}
+                >
+                  <Table.Cell>{productName}</Table.Cell>
+                  {assetTypes.map((assetType, index) => {
+                    const asset = assets.find(
+                      ({ assetType: { id } }) => id === assetType.id
+                    );
+
+                    if (!asset) {
+                      return (
+                        <Table.Cell
+                          key={`${productName}-missing-asset-${index}`}
+                          className={styles["align-center"]}
+                        >
+                          <Icon
+                            source={iconMap.Cross}
+                            className={styles["no-document-icon"]}
+                          />
+                        </Table.Cell>
+                      );
+                    }
+
+                    return (
+                      <Table.Cell
+                        key={`${productName}-asset-${asset.id}`}
+                        className={styles["align-center"]}
+                      >
+                        {asset.__typename !== "PIMLinkDocument" ? (
+                          <Clickable
+                            model="download"
+                            href={asset.url}
+                            download={asset.title}
+                          >
+                            <Icon
+                              source={fileIconsMap[asset.format]}
+                              className={styles["format-icon"]}
+                            />
+                          </Clickable>
+                        ) : (
+                          <Button
+                            isIconButton
+                            variant="text"
+                            action={{
+                              model: "htmlLink",
+                              href: asset.url,
+                              target: "_blank",
+                              rel: "noopener noreferrer"
+                            }}
+                          >
+                            <Icon
+                              source={iconMap.External}
+                              className={styles["external-link-icon"]}
+                            />
+                          </Button>
+                        )}
+                      </Table.Cell>
+                    );
+                  })}
+                  <Table.Cell className={styles["align-center"]}>
+                    <DownloadList.Checkbox
+                      name={key}
+                      maxLimitReachedLabel={getMicroCopy(
+                        "documents.download.maxReached"
+                      )}
+                      ariaLabel={`${getMicroCopy(
+                        "documentLibrary.download"
+                      )} ${productName}`}
+                      value={assets.filter(
+                        ({ __typename }) => __typename !== "PIMLinkDocument"
+                      )}
+                      fileSize={assets.reduce((acc, curr) => {
+                        if (curr.__typename === "PIMLinkDocument") {
+                          return 0;
+                        }
+                        return acc + (curr.fileSize || 0);
+                      }, 0)}
+                    />
+                  </Table.Cell>
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table>
+      </div>
     </div>
   );
 };
