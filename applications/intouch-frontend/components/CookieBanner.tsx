a2f805fd249f7dd9fda5ad71b0be833344aeb1bf
Steve Mitchell <steve@steviant.co.uk>
2021-03-08 15:59:38 +0000

    feat(intouch-frontend): integrate API data into Company page

--- applications/intouch-frontend/components/CookieBanner.tsx
+++ applications/intouch-frontend/components/CookieBanner.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import styles from "./styles/CookieBanner.module.scss";
 
-// interface Props {
-//   children: React.ReactNode | React.ReactNode[];
-// }
+// WIP
+interface Props {
+  children: React.ReactNode | React.ReactNode[];
+}
 
 const CookieBanner = ({ children }: Props) => (
-  <div className={styles.cookieBanner}>
-    <p>
-      The Roof Pro Portal of the BMI Group uses first-party and third-party
-      cookies to ensure the functionality of the website. To find out more or
-      change your cookie settings, please click here. By registering and using
-      the Portal, you consent to our use of cookies. Find out more.
-    </p>
-  </div>
+  <div className={styles.cookieBanner}>{children}</div>
 );
 
 export default CookieBanner;
