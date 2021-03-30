3c22a81f3c76b799d62a4e34c256897caf8ff04c
Rowland Corentin <rowland.corentin@icloud.com>
2021-03-30 13:33:26 +0100

    fix(head): remove need for separate template file

--- applications/head/scripts/netlify-toml-builder.js
+++ applications/head/scripts/netlify-toml-builder.js
@@ -10,17 +10,34 @@ require("dotenv").config({
 //console.log(process.env);
 
 const buildContent = () => {
-  const templateFile = path.resolve(__dirname, "../netlify_template.toml");
+  const templateFile = path.resolve(__dirname, "../netlify.toml");
+
   const redirectsFile = path.resolve(
     __dirname,
-    "../redirects_" + process.env.SPACE_MARKET_CODE + ".toml"
+    "../redirects_" + process.env.SPACE_MARKET_CODE.toLowerCase() + ".toml"
   );
-  const fileOut = path.resolve(__dirname, "../netlify.toml");
-  const fileContentIn = fs.readFileSync(templateFile, "utf-8");
-  const fileContentIn2 = fs.readFileSync(redirectsFile, "utf-8");
+  if (redirectsFile) {
+    const fileOut = path.resolve(__dirname, "../netlify.toml");
+    const fileContentIn = fs.readFileSync(templateFile, "utf-8");
+    const fileContentIn2 = fs.readFileSync(redirectsFile, "utf-8");
+    const redirectsMarker = "# *** REDIRECTS_START_HERE ***";
+    const settingsWithoutRedirects = fileContentIn.split(redirectsMarker)[0];
 
-  fs.writeFileSync(fileOut, fileContentIn + fileContentIn2);
+    fs.writeFileSync(
+      fileOut,
+      settingsWithoutRedirects + redirectsMarker + "\n" + fileContentIn2
+    );
+  } else {
+    console.warn("redirects file for selected market could not be found");
+  }
 };
+
+if (!process.env.SPACE_MARKET_CODE) {
+  console.error(
+    "Please provide a value for Environment Variable 'process.env.SPACE_MARKET_CODE'."
+  );
+  process.exit(1);
+}
 
 console.log(
   "** Generating netlify toml file from template + market redirects for code:" +
