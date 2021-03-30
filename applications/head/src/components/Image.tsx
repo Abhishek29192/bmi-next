3d4fe2e24acbd62a396253eee873f3297f20c86a
Alex Canessa <canessa.alex@gmail.com>
2021-03-30 21:24:46 +0200

    feat(head): add options to the Image component

--- applications/head/src/components/Image.tsx
+++ applications/head/src/components/Image.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Img, { GatsbyImageFluidProps } from "gatsby-image";
-
+// NOTE: The `withIEPolyfill` package exports objectFit/Position.
+import Img from "gatsby-image/withIEPolyfill";
 // TODO: This package gets deprecated in gatsby@3.x in favour of
 // gatsby-plugin-image. When we migrate to next gatsby version we have to
 // change this package too. For migration check https://www.gatsbyjs.com/docs/how-to/images-and-media/using-gatsby-plugin-image/#migrating
+import { GatsbyImageFluidProps } from "gatsby-image";
 
 // TODO: Move in its own file.
 type ContentfulAsset = GatsbyImageFluidProps & {
@@ -18,8 +19,8 @@ type ContentfulAsset = GatsbyImageFluidProps & {
 };
 
 export type Data = {
-  type: string | null;
   altText: string | null;
+  type: "Decorative" | "Descriptive" | null;
   image: ContentfulAsset;
   caption: {
     caption: string;
@@ -30,15 +31,33 @@ export type Data = {
   } | null;
 };
 
-const Image = ({ data }: { data?: Data }) => {
-  if (!data) {
-    return null;
-  }
-
-  return <Img fluid={data.image.fluid} alt={data.altText} draggable={false} />;
+type Options = {
+  size?: "cover" | "contain";
+  position?: string;
 };
 
-export const renderImage = (data?: Data) => {
+const typeToObjectFitMap: Record<Data["type"], Options["size"]> = {
+  Decorative: "cover",
+  Descriptive: "contain"
+};
+
+const getPosition = ({
+  size,
+  position,
+  focalPoint
+}: Options & Pick<Data, "focalPoint">): Options["position"] => {
+  if (position) {
+    return position;
+  }
+
+  if (size === "cover" && focalPoint) {
+    return `${focalPoint.x}% ${focalPoint.y}%`;
+  }
+
+  return "center";
+};
+
+const Image = ({ data, size, position }: { data?: Data } & Options) => {
   if (!data) {
     return null;
   }
@@ -48,8 +67,36 @@ export const renderImage = (data?: Data) => {
       fluid={data.image.fluid}
       alt={data.altText}
       draggable={false}
-      // NOTE: Remove default styling.
-      style={{ position: undefined, overflow: undefined }}
+      style={{ position: undefined }}
+      objectFit={size || typeToObjectFitMap[data.type || "Decorative"]}
+      objectPosition={getPosition({
+        size,
+        position,
+        focalPoint: data.focalPoint
+      })}
+    />
+  );
+};
+
+export const renderImage = (data?: Data, options: Options = {}) => {
+  if (!data) {
+    return null;
+  }
+
+  const { size, position } = options;
+
+  return (
+    <Img
+      fluid={data.image.fluid}
+      alt={data.altText}
+      draggable={false}
+      style={{ position: undefined }}
+      objectFit={size || typeToObjectFitMap[data.type || "Decorative"]}
+      objectPosition={getPosition({
+        size,
+        position,
+        focalPoint: data.focalPoint
+      })}
     />
   );
 };
@@ -60,6 +107,7 @@ export const query = graphql`
   fragment ImageFragment on ContentfulImage {
     type
     altText
+    type
     image {
       # srcSetBreakpoints
       # quality
