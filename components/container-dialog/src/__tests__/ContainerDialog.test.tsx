7686f591c7a99ca44277044089e824425c17e7cd
Hitesh Khatri <Hitesh.khatri@bmigroup.com>
2021-04-01 17:30:46 +0100

    feat(container-dialog): create new dialog per design

---
+++ components/container-dialog/src/__tests__/ContainerDialog.test.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ContainerDialog from "../";
+
+describe("ContainerDialog component", () => {
+  it("renders correctly", () => {
+    const { container } = render(
+      <ContainerDialog open onCloseClick={console.log}>
+        <div>Some content</div>
+      </ContainerDialog>
+    );
+    expect(container.parentElement).toMatchSnapshot();
+  });
+
+  it("calls all events", () => {
+    const closeLabel = "Close";
+    const backdropClassName = "test-backdrop";
+
+    const onCloseClick = jest.fn();
+    const onBackdropClick = jest.fn();
+
+    const { container, getByLabelText, getByText } = render(
+      <ContainerDialog
+        open
+        onCloseClick={onCloseClick}
+        onBackdropClick={onBackdropClick}
+        backdropProps={{
+          className: backdropClassName
+        }}
+      >
+        <div>Some content</div>
+      </ContainerDialog>
+    );
+
+    const closeButton = getByLabelText(closeLabel);
+    closeButton.click();
+    expect(onCloseClick).toHaveBeenCalled();
+
+    const backdrop = container.parentElement.querySelector(
+      `.${backdropClassName}`
+    );
+    // @ts-ignore
+    backdrop.click();
+    expect(onBackdropClick).toHaveBeenCalled();
+  });
+});
