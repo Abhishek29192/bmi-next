47d1f791cd972228255c680caed30bbd2201b91c
Alex Canessa <canessa.alex@gmail.com>
2021-04-06 20:45:37 +0200

    fix(head): use correct team profile picture

--- applications/head/src/components/__tests__/TeamList.test.tsx
+++ applications/head/src/components/__tests__/TeamList.test.tsx
@@ -8,9 +8,21 @@ describe("TeamList component", () => {
       {
         name: "Name",
         jobTitle: "Job Title",
-        profilePicture: {
-          resize: {
-            src: "link-to.jpg"
+        profileImage: {
+          type: null,
+          altText: "Lorem ipsum",
+          caption: null,
+          focalPoint: null,
+          image: {
+            fluid: {
+              aspectRatio: 1,
+              src: "",
+              srcSet: "",
+              sizes: ""
+            },
+            resize: {
+              src: "link-to-page.png"
+            }
           }
         },
         links: null
