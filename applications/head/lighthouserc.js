3912b731d37ea137bc32968211b7be59cefbe0e8
Benjamin Sproule <benjamin.sproule@bmigroup.com>
2021-04-19 16:59:03 +0100

    ci(head): trigger lighthouse and zap after deploy

--- applications/head/lighthouserc.js
+++ applications/head/lighthouserc.js
@@ -3,34 +3,25 @@
 module.exports = {
   ci: {
     collect: {
-      // NOTE: URL(s) to search.
-      // Can be overidden in command line with: --collect.url=
-      url: ["http://localhost:8000/no"],
-      // NOTE: Number of runs per page (with result aggregated)
-      // Can be overidden in command line with: --collect.numberOfRuns=
       numberOfRuns: 3,
       puppeteerScript: "./scripts/login-head-netlify.js",
       puppeteerLaunchOptions: {
         args: ["--no-sandbox"]
       },
       settings: {
-        // NOTE: set password for puppeteerScript.
-        // Defaulted to empty string (ignored), set to correct value in
-        // command line with --collect.settings.pwd=
         pwd: "",
         chromeFlags: "--no-sandbox"
       }
     },
     assert: {
+      preset: "lighthouse:recommended",
       assertions: {
-        // NOTE: Set additional categories with alerting here.
-        "categories:accessibility": ["error", { minScore: 0.9 }]
+        // "categories:accessibility": ["error", { minScore: 0.9 }]
       }
     },
-    // TODO: Future work to configure upload section to upload to Lighthouse
-    // server for performance analysis and build history.
     upload: {
-      target: "temporary-public-storage"
+      target: "filesystem",
+      outputDir: "lighthouseci/reports"
     }
   }
 };
