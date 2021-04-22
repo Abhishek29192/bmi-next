7aa4d071cce767b2ab072ce24bd776d95971e433
Dominik Widomski <dominik@digital-detox.co.uk>
2021-04-22 15:23:31 +0100

    refactor(intouch-frontend): move app initialProps into _app

--- applications/intouch/frontend/pages/_app.tsx
+++ applications/intouch/frontend/pages/_app.tsx
@@ -1,11 +1,11 @@
 import React from "react";
+import Router from "next/router";
 import { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import { ApolloProvider } from "@apollo/client";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import { useApollo } from "../lib/apolloClient";
-
-import initialProps from "../lib/initialProps/app";
+import auth0 from "../lib/auth0";
 import useApi from "../hooks/useApi";
 
 import "../styles/globals.css";
@@ -35,6 +35,37 @@ const AuthApp = ({ Component, pageProps, ...rest }: AppProps) => {
   );
 };
 
-AuthApp.getInitialProps = initialProps;
+AuthApp.getInitialProps = async ({ ctx, Component }) => {
+  const { req, res, pathname } = ctx;
+
+  // Lets api work normally
+  if (pathname.indexOf("/api") !== -1) {
+    return {};
+  }
+
+  // Check if the user is a company_admin and if he has a company
+  const session = await auth0.getSession(req, res);
+
+  if (session) {
+    ctx.session = session;
+    const { user } = session;
+    const regCompleted = user[`${process.env.AUTH0_NAMESPACE}/reg_to_complete`];
+    if (pathname !== "/company-registration" && regCompleted) {
+      if (res) {
+        res.writeHead(302, { Location: "/company-registration" });
+        res.end();
+      } else {
+        Router.replace("/company-registration");
+      }
+      return {};
+    }
+  }
+
+  if (Component.getServerSideProps) {
+    Object.assign({}, await Component.getServerSideProps(ctx));
+  }
+
+  return {};
+};
 
 export default appWithTranslation(AuthApp);
