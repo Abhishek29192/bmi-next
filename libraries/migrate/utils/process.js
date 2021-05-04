e8e12a078d31ffa8223d110421cff32db3798729
Dan Edwards <igneosaur@gmail.com>
2021-05-04 14:55:52 +0100

    fix(migrate): identify dry run option -d when valid

--- libraries/migrate/utils/process.js
+++ libraries/migrate/utils/process.js
@@ -1,5 +1,5 @@
 "use strict";
 
 module.exports.isDryRun = Boolean(
-  process.argv.find((arg) => arg.includes("--dry-run") || arg.includes("-d"))
+  process.argv.slice(2).find((arg) => arg.includes("--dry-run") || arg === "-d")
 );
