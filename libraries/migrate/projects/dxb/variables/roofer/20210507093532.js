857d2f56c7744253c6629144601df5f767a0c067
Hitesh Khatri <Hitesh.khatri@bmigroup.com>
2021-05-07 12:10:28 +0100

    feat(migrate): add new roofer types

---
+++ libraries/migrate/projects/dxb/variables/roofer/20210507093532.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const rooferTypes = ["Pitched roof", "Flat roof"];
+
+module.exports = rooferTypes;
---
+++ libraries/migrate/projects/dxb/variables/roofer/20210507094158.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const rooferTypes = [
+  "Pitched roof",
+  "Flat roof",
+  "Pitched roof bitumen",
+  "Pitched roof tiles",
+  "Roof coaters"
+];
+
+module.exports = rooferTypes;
