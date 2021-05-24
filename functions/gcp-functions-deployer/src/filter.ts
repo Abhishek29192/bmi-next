092b2b5e8ce013702b6254ef7ea48814f517b8d6
Mahesh Warnasooriya <mahesh.warnasooriya@bmigroup.com>
2021-05-24 16:51:20 +0100

    feat(gcp-functions-deployer): multi-functions

--- functions/gcp-functions-deployer/src/filter.ts
+++ functions/gcp-functions-deployer/src/filter.ts
@@ -3,21 +3,21 @@ import { DownloadResponse } from "@google-cloud/storage/build/src/file";
 export function filterFunctionMetadata(
   content: DownloadResponse,
   sourceName: string
-): string {
+): object[] {
   // eslint-disable-next-line no-console
   console.log(`sourceName:${sourceName}`);
   if (!content) {
     return null;
   }
   const allFunctionMetadata = JSON.parse(content[0].toString());
-  const curerntFunctionMetadata = allFunctionMetadata.find(function (el) {
+  const curerntFunctionsMetadata = allFunctionMetadata.filter(function (el) {
     return el.source_archive_object === sourceName;
   });
 
-  if (!curerntFunctionMetadata) {
+  if (!curerntFunctionsMetadata || !curerntFunctionsMetadata.length) {
     // eslint-disable-next-line no-console
     console.warn(`Metadata not found for source : ${sourceName}`);
     return null;
   }
-  return curerntFunctionMetadata;
+  return curerntFunctionsMetadata;
 }
