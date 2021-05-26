7c0e8f43f3f2826644910508f0e5f11a622c90a5
Francesco Venica <francesco.venica@gmail.com>
2021-05-26 09:47:35 +0100

    feat(intouch): improve docker-compose

--- applications/intouch/api/services/companies/src/data/init-db.sh
+++ applications/intouch/api/services/companies/src/data/init-db.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 set -e
 
-export DATABASE_NAME="${DATABASE_NAME}"
+export POSTGRES_DB="${POSTGRES_DB}"
 export POSTGRES_USER="${POSTGRES_USER}"
 
-echo "Importin schema and data"
+echo "Importing schema and data"
 dump="`cat /var/lib/postgresql/company.sql`"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATABASE_NAME" <<HERE
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<HERE
     $dump
 HERE
 
 echo "Creating roles"
 dump="`cat /var/lib/postgresql/roles.sql`"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATABASE_NAME" <<HERE
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<HERE
     $dump
 HERE
 
 echo "Creating procedures"
 dump="`cat /var/lib/postgresql/procedure.sql`"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATABASE_NAME" <<HERE
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<HERE
     $dump
 HERE
 
 echo "Creating RLS"
 dump="`cat /var/lib/postgresql/rls.sql`"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATABASE_NAME" <<HERE
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<HERE
     $dump
 HERE
