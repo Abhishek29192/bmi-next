5cbcd4b4d5ffcd5ba80b79fefedc948a31b2c4fe
Umit Karaogul <umit@digital-detox.co.uk>
2021-06-15 14:09:05 +0100

    feat(intouch-frontend): add images folder for custom images,svgs, etc

--- applications/intouch/frontend/components/SidePanel/TrainingSidePanel/index.tsx
+++ applications/intouch/frontend/components/SidePanel/TrainingSidePanel/index.tsx
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
 import Typography from "@bmi/typography";
 import { SvgIcon } from "@material-ui/core";
+import { Technology } from "@bmi/intouch-api-types";
 import { FilterResult } from "../../FilterResult";
 import { SidePanel } from "../";
 import { TrainingQuery } from "../../../graphql/generated/operations";
-import PitchIcon from "./images/roofPitch.svg";
-import FlatIcon from "./images/roofFlat.svg";
-import OtherIcon from "./images/roofOther.svg";
+import { PitchIcon, FlatIcon, OtherIcon } from "../../icons";
 
 const DEFAULT_FILTER_CRITERIA = "all";
 
-enum COURSE_TECHNOLOGIES {
-  FLAT = "FLAT",
-  PITCHED = "PITCHED",
-  OTHER = "OTHER"
-}
+const courseTech: { [K in Technology]: string } = {
+  FLAT: "FLAT",
+  PITCHED: "PITCHED",
+  OTHER: "OTHER"
+};
 
 const TECHNOLOGIES_ICONS: {
-  [K in COURSE_TECHNOLOGIES]: React.FC<React.SVGProps<SVGSVGElement>>;
+  [K in Technology]: React.FC<React.SVGProps<SVGSVGElement>>;
 } = {
   FLAT: FlatIcon,
   PITCHED: PitchIcon,
@@ -30,9 +29,9 @@ const trainingFilters = [
     attr: DEFAULT_FILTER_CRITERIA,
     isActive: false
   },
-  ...Object.keys(COURSE_TECHNOLOGIES).map((key) => ({
-    label: `${COURSE_TECHNOLOGIES[`${key}`]}`.toLowerCase(),
-    attr: COURSE_TECHNOLOGIES[`${key}`],
+  ...Object.keys(courseTech).map((key) => ({
+    label: `${courseTech[`${key}`]}`.toLowerCase(),
+    attr: courseTech[`${key}`],
     isActive: false
   }))
 ];
@@ -78,9 +77,7 @@ export const TrainingSidePanel = ({
               </Typography>
               <Typography style={{ display: "flex", padding: "2px" }}>
                 <SvgIcon
-                  component={
-                    TECHNOLOGIES_ICONS[technology as COURSE_TECHNOLOGIES]
-                  }
+                  component={TECHNOLOGIES_ICONS[technology as Technology]}
                 />
                 {courseEnrollments.nodes.map((node) => node.status)[0] ||
                   DEFAULT_ENROLLMENT_STATUS}
---
+++ applications/intouch/frontend/components/icons/index.ts
@@ -0,0 +1,5 @@
+import FlatIcon from "./svgs/roofFlat.svg";
+import PitchIcon from "./svgs/roofPitch.svg";
+import OtherIcon from "./svgs/roofOther.svg";
+
+export { FlatIcon, PitchIcon, OtherIcon };
