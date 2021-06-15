95a8ed64d1b37d4e8aa002cdf4eab8c1fd0f8d77
Benjamin Sproule <benjamin.sproule@bmigroup.com>
2021-06-15 22:49:05 +0100

    test(download-list): use correct act

--- components/download-list/src/__tests__/DownloadList.test.tsx
+++ components/download-list/src/__tests__/DownloadList.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { render, fireEvent, cleanup } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+import { render, fireEvent, cleanup, act } from "@testing-library/react";
 import DownloadList from "../";
 
 afterEach(cleanup);
