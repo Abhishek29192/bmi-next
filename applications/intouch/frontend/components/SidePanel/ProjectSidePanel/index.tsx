a47d8990f1f3214bcb05586b6342c707db71d3a6
Umit Karaogul <umit@digital-detox.co.uk>
2021-06-24 15:35:15 +0100

    feat(intouch-frontend): show projects on ProjectSidePanel

--- applications/intouch/frontend/components/SidePanel/ProjectSidePanel/index.tsx
+++ applications/intouch/frontend/components/SidePanel/ProjectSidePanel/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Typography from "@bmi/typography";
+import { useTranslation } from "next-i18next";
+import { SvgIcon } from "@material-ui/core";
+import { Technology } from "@bmi/intouch-api-types";
 import { FilterResult } from "../../FilterResult";
 import { SidePanel } from "../";
+import { GetProjectsQuery } from "../../../graphql/generated/operations";
+import { PitchIcon, FlatIcon, OtherIcon } from "../../icons";
 
 const projectFilters = [
   { label: "Not started", attr: "notStarted" },
@@ -11,27 +16,59 @@ const projectFilters = [
   { label: "Pitched", attr: "pitched" }
 ];
 
+const technologyIcon: {
+  [K in Technology]: React.FC<React.SVGProps<SVGSVGElement>>;
+} = {
+  FLAT: FlatIcon,
+  PITCHED: PitchIcon,
+  OTHER: OtherIcon
+};
+
+export enum ProjectStatus {
+  NOT_STARTED = "Not started",
+  IN_PROGRESS = "In progress",
+  COMPLETED = "Completed"
+}
+
 type ProjectSidePanelProps = {
-  projectDetails: { id: number; name: string; company: string }[];
+  projects: GetProjectsQuery["projects"];
   onProjectSelected?: (projectId: number) => void;
 };
 export const ProjectSidePanel = ({
-  projectDetails,
+  projects,
   onProjectSelected
 }: ProjectSidePanelProps) => {
+  const { t } = useTranslation("project-page");
+
+  const { nodes = [] } = projects || {};
+
+  const getProjectStatus = (startDate, endDate) => {
+    if (!startDate && !endDate) return ProjectStatus.NOT_STARTED;
+    else if (startDate && !endDate) return ProjectStatus.IN_PROGRESS;
+    else return ProjectStatus.COMPLETED;
+  };
+
   return (
     <SidePanel searchLabel="Search for a project" filters={projectFilters}>
-      {projectDetails.map((project) => (
-        <FilterResult
-          label={project.name}
-          key={project.name}
-          onClick={() => {
-            onProjectSelected && onProjectSelected(project.id);
-          }}
-        >
-          <Typography>{project.company}</Typography>
-        </FilterResult>
-      ))}
+      {nodes.map(
+        ({ id, name, siteAddress, technology, startDate, endDate }) => (
+          <FilterResult
+            label={name}
+            key={id}
+            onClick={() => {
+              onProjectSelected && onProjectSelected(id);
+            }}
+          >
+            <Typography>
+              {siteAddress?.town},{siteAddress?.postcode}
+            </Typography>
+            <Typography style={{ display: "flex" }}>
+              <SvgIcon component={technologyIcon[technology as Technology]} />
+              {t(getProjectStatus(startDate, endDate))}
+            </Typography>
+          </FilterResult>
+        )
+      )}
     </SidePanel>
   );
 };
