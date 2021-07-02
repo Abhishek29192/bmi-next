18b8b656a08b9423d3dfd6a4733377438dc21de3
Yazan Alkheder <yazan@digital-detox.co.uk>
2021-07-02 17:27:19 +0100

    feat(gcp-update-webtools-bucket): log fetch error body

--- applications/dxb/functions/gcp-update-webtools-bucket/src/index.ts
+++ applications/dxb/functions/gcp-update-webtools-bucket/src/index.ts
@@ -83,7 +83,12 @@ const fetchData = async (body: object, remainingRetries = 5): Promise<any> => {
 
   // Errors other than 429
   if (!response.ok) {
-    throw new Error(response.statusText);
+    console.error(response.statusText);
+
+    const errorData = await response.json();
+    console.error(errorData);
+
+    throw new Error("Failed to fetch data");
   }
 
   const data = await response.json();
