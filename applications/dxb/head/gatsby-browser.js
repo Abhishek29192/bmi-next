829222f8037fdb82909e448c6ba4af56c7aa4545
Hitesh Khatri <hitesh.khatri@bmigroup.com>
2021-07-06 08:49:02 +0100

    fix(head): remove body class fix

--- applications/dxb/head/gatsby-browser.js
+++ applications/dxb/head/gatsby-browser.js
@@ -2,12 +2,6 @@
 /* eslint-disable-next-line strict */
 "use strict";
 
-// exports.onServiceWorkerUpdateReady = () => {
-//   window.location.reload();
-// };
-
-exports.onClientEntry = () => {
-  window.addEventListener("load", () => {
-    document.body.className = document.body.className.replace(/\bno-js\b/, "");
-  });
+exports.onServiceWorkerUpdateReady = () => {
+  window.location.reload();
 };
--- applications/dxb/head/gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-/* eslint-disable-next-line strict */
-"use strict";
-
-exports.onRenderBody = ({ setBodyAttributes }) => {
-  setBodyAttributes({
-    className: "no-js"
-  });
-};
