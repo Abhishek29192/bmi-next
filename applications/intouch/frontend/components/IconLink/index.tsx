8e43c22a9db051624fed491f7717fdae877c4e37
Francesco Moro <franzmoro@users.noreply.github.com>
2021-07-14 17:32:19 +0200

    refactor(intouch-frontend): allow external links in IconLink

--- applications/intouch/frontend/components/IconLink/index.tsx
+++ applications/intouch/frontend/components/IconLink/index.tsx
@@ -1,33 +1,43 @@
 /* eslint-disable security/detect-unsafe-regex */
 import React from "react";
-import Link from "next/link";
 import PhoneIcon from "@material-ui/icons/Phone";
 import EmailIcon from "@material-ui/icons/Email";
 import PublicIcon from "@material-ui/icons/Public";
 import Icon from "@bmi/icon";
 import Typography from "@bmi/typography";
+import { Link } from "../Link";
 import styles from "./styles.module.scss";
 
-export type IconLinkProps = {
+export type IconLinkProps = React.AnchorHTMLAttributes<any> & {
   href: string;
   icon: SVGImport;
   label: string;
+  isExternal?: boolean;
 };
 
-export const IconLink = ({ href, icon, label }: IconLinkProps) => {
-  return (
-    // TODO: use a Link component that allows external urls
-    <Link href={href}>
-      <a className={styles.main}>
-        <Icon source={icon} color="action" style={{ fontSize: 24 }} />
-        <Typography variant="body1">{label}</Typography>
-      </a>
-    </Link>
-  );
-};
+export const IconLink = ({
+  href,
+  icon,
+  label,
+  isExternal = false,
+  ...rest
+}: IconLinkProps) => (
+  <Link href={href} className={styles.main} isExternal={isExternal} {...rest}>
+    <>
+      <Icon source={icon} color="action" style={{ fontSize: 24 }} />
+      <Typography variant="body1">{label}</Typography>
+    </>
+  </Link>
+);
 
 export const PhoneNumberLink = ({ phoneNumber }: { phoneNumber: string }) => (
-  <IconLink href={`tel:${phoneNumber}`} icon={PhoneIcon} label={phoneNumber} />
+  <IconLink
+    href={`tel:${phoneNumber}`}
+    icon={PhoneIcon}
+    label={phoneNumber}
+    isExternal
+    target="self"
+  />
 );
 
 export const EmailLink = ({ emailAddress }: { emailAddress: string }) => (
@@ -35,6 +45,8 @@ export const EmailLink = ({ emailAddress }: { emailAddress: string }) => (
     href={`mailto:${emailAddress}`}
     icon={EmailIcon}
     label={emailAddress}
+    isExternal
+    target="self"
   />
 );
 
@@ -49,5 +61,6 @@ export const WebsiteLink = ({
     href={url}
     icon={icon}
     label={url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "")}
+    isExternal
   />
 );
