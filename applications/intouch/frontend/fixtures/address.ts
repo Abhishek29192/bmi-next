6441ef3e146cc75e08fdd3e86dd42b701d3cc5b1
Francesco Moro <franzmoro@users.noreply.github.com>
2021-07-15 12:29:55 +0200

    test(intouch-frontend): add tests for Address component

--- applications/intouch/frontend/fixtures/address.ts
+++ applications/intouch/frontend/fixtures/address.ts
@@ -3,9 +3,9 @@ import { Address } from "@bmi/intouch-api-types";
 export const mockAddress: Partial<Address> = {
   firstLine: "Nursery Rd",
   secondLine: "Brixton",
-  town: "London",
-  region: null,
-  country: "UK",
   postcode: "SW9 8BP",
+  town: "London",
+  region: "Some Region",
+  country: "UK",
   coordinates: { x: 51.4632583, y: -0.1194107 }
 };
