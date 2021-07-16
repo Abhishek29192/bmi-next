e10b91250b78d2e163ab8a76b66cb449b01051af
Francesco Moro <franzmoro@users.noreply.github.com>
2021-07-16 15:05:08 +0200

    feat(intouch-frontend): add norwegian local with temporary translations

--- applications/intouch/frontend/lib/config.ts
+++ applications/intouch/frontend/lib/config.ts
@@ -1,11 +1,13 @@
 export const REDIRECT_MAP = {
   // Public domain & sub-domains
   "intouch.dddev.io": "en",
+  "no.intouch.dddev.io": "no",
   // Local domains
   localhost: "en",
   "en.local.intouch": "en",
   "it.local.intouch": "it",
   "de.local.intouch": "de",
   "es.local.intouch": "es",
-  "us.local.intouch": "us"
+  "us.local.intouch": "us",
+  "no.local.intouch": "no"
 };
