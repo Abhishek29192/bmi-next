7efc8ae8f1d59d72dd98a0f841bbf87cbe20e496
Rowland Corentin <rowland.corentin@icloud.com>
2021-07-19 11:20:56 +0100

    fix(head): test to ensure image only converts when it should

--- applications/dxb/head/src/utils/__tests__/images.test.ts
+++ applications/dxb/head/src/utils/__tests__/images.test.ts
@@ -11,6 +11,13 @@ describe("getJpgImage function", () => {
     expect(getJpgImage(img)).not.toContain(img + "fm=jpg");
   });
 
+  it("does nothing if fm is already specified", () => {
+    const img =
+      "https://images.ctfassets.net/18fop5x17y3g/7JfBbAmDeTWlHzSlc1auWm/dead1b50a4dcf1ee9f6fba135f9135b3/BMI-Zanda-Logo-Web.webp?h=250&fm=webp";
+
+    expect(getJpgImage(img)).not.toContain(img + "fm=jpg");
+  });
+
   it("returns a jpeg image from contentful images api", () => {
     const img =
       "https://images.ctfassets.net/18fop5x17y3g/7JfBbAmDeTWlHzSlc1auWm/dead1b50a4dcf1ee9f6fba135f9135b3/BMI-Zanda-Logo-Web.webp?h=250";
