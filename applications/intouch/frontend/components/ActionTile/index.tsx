0626d61b654bb68c63948372d868bf0bc0c18c59
Umit Karaogul <umit@digital-detox.co.uk>
2021-07-27 10:33:18 +0100

    feat(intouch-frontend): add disabled&&click prop to ActionTile component

--- applications/intouch/frontend/components/ActionTile/index.tsx
+++ applications/intouch/frontend/components/ActionTile/index.tsx
@@ -3,18 +3,33 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import Typography from "@bmi/typography";
 import Icon from "@bmi/icon";
 import { BMI } from "@bmi/logo";
+import classnames from "classnames";
 import styles from "./styles.module.scss";
 
 export type ActionTileProps = {
   title: string;
   description: string;
-  // action?: ClickableAction;
+  disabled?: boolean;
+  onClick?: () => void;
+  isSelected?: boolean;
 };
 
-export const ActionTile = ({ title, description }: ActionTileProps) => {
+export const ActionTile = ({
+  title,
+  description,
+  disabled = false,
+  onClick,
+  isSelected
+}: ActionTileProps) => {
   return (
     <div className={styles.main}>
-      <CardActionArea className={styles.clickable}>
+      <CardActionArea
+        className={classnames(styles["clickable"], {
+          [styles["clickable--selected"]]: isSelected
+        })}
+        disabled={disabled}
+        onClick={onClick}
+      >
         <Icon source={BMI} className={styles.logo} />
         <div className={styles.text}>
           <Typography variant="h5" className={styles.title}>
