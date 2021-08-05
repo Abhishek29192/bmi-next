9aad835fc14285d843c6f2b806e2c9142d79c824
Umit Karaogul <umit@digital-detox.co.uk>
2021-08-05 20:33:44 +0100

    feat(intouch-frontend): update guarantee wizard select evidence step

--- applications/intouch/frontend/components/WizardLayout/Steps/SelectEvidence.tsx
+++ applications/intouch/frontend/components/WizardLayout/Steps/SelectEvidence.tsx
@@ -1,24 +1,39 @@
 import React from "react";
 import Upload from "@bmi/upload";
+import { useTranslation } from "next-i18next";
+import { useWizardContext } from "../WizardContext";
 
 export const SelectEvidence = () => {
+  const { data, setData } = useWizardContext();
+  const { t } = useTranslation("project-page");
+
+  const onFilesChangeHandler = (evidences) => {
+    setData({
+      ...data,
+      evidences
+    });
+  };
+
   return (
-    <>
-      <Upload
-        id={"add-evidence"}
-        name={"add-evidence"}
-        buttonLabel={"Upload Files"}
-        accept=".pdf,.jpg,.jpeg,.png"
-        instructions={"You can add files"}
-        mapValue={() => {}}
-        mapBody={(file) => ({ file })}
-        microcopyProvider={{
-          "upload.instructions.drop": "Drop",
-          "upload.instructions.browse": "Browse"
-        }}
-        defaultExpanded={true}
-        onFilesChange={(files) => {}}
-      />
-    </>
+    <Upload
+      id={"add-evidence"}
+      name={"add-evidence"}
+      buttonLabel={t("upload_tab.add_evidence_modal.button_label")}
+      accept=".pdf,.jpg,.jpeg,.png"
+      instructions={t("upload_tab.add_evidence_modal.instruction")}
+      mapValue={(file) => file}
+      mapBody={(file) => file}
+      microcopyProvider={{
+        "upload.instructions.drop": t(
+          "upload_tab.add_evidence_modal.microcopy.drop"
+        ),
+        "upload.instructions.browse": t(
+          "upload_tab.add_evidence_modal.microcopy.browse"
+        )
+      }}
+      defaultExpanded={true}
+      onFilesChange={onFilesChangeHandler}
+      value={data.evidences}
+    />
   );
 };
