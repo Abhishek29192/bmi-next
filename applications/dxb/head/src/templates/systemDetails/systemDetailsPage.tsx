9753d841755ed87e82073853a799e7d9ef54be2f
Lucas Cheung <kwok.cheung@bmigroup.com>
2021-08-10 17:08:05 +0100

    Added aboutLeadBlock and its style scss Updated systemDetailsPage to add in aboutLeadBlock

--- applications/dxb/head/src/templates/systemDetails/systemDetailsPage.tsx
+++ applications/dxb/head/src/templates/systemDetails/systemDetailsPage.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { graphql } from "gatsby";
+import Tabs from "@bmi/tabs";
+import { Tab, TabProps } from "@material-ui/core";
+import Section from "@bmi/section";
 import Page from "../../components/Page";
-import { Data as SiteData } from "../../components/Site";
+import { Data as SiteData, SiteContext } from "../../components/Site";
 import ShareWidgetSection, {
   Data as ShareWidgetSectionData
 } from "../../components/ShareWidgetSection";
+import withGTM from "../../utils/google-tag-manager";
 import LeadBlockSection from "./leadBlockSection";
 import ImageGallerySection from "./imageGallerySection";
 import { SystemDetails } from "./types";
+import AboutLeadBlock from "./aboutLeadBlock";
 
 type Props = {
   pageContext: {
@@ -21,10 +26,16 @@ type Props = {
   };
 };
 
+const GTMTab = withGTM<TabProps>(Tab, {
+  label: "label"
+});
+
 const SystemDetailsPage = ({ data }: Props) => {
+  const { getMicroCopy } = useContext(SiteContext);
   const { contentfulSite, dataJson } = data;
   const { resources } = contentfulSite;
-  const { name, categories, classifications, images } = dataJson;
+  const { name, categories, classifications, images, assets, longDescription } =
+    dataJson;
 
   return (
     <Page
@@ -42,6 +53,34 @@ const SystemDetailsPage = ({ data }: Props) => {
         cta={resources?.sdpLeadBlockCta}
       />
       <ImageGallerySection images={images || []} />
+      <Section backgroundColor="white">
+        <Tabs
+          initialValue="one"
+          tabComponent={(props: TabProps) => (
+            <GTMTab
+              gtm={{ id: "selector-tabs1", action: "Selector – Tabs" }}
+              {...props}
+            />
+          )}
+        >
+          <Tabs.TabPanel
+            heading={getMicroCopy("sdp.leadBlock.about")}
+            index="one"
+          >
+            <AboutLeadBlock
+              longDescription={longDescription}
+              guaranteesAndWarranties={assets.filter(
+                ({ assetType }) =>
+                  assetType === "GUARANTIES" || assetType === "WARRANTIES"
+              )}
+              awardsAndCertificates={assets.filter(
+                ({ assetType }) =>
+                  assetType === "AWARDS" || assetType === "CERTIFICATES"
+              )}
+            />
+          </Tabs.TabPanel>
+        </Tabs>
+      </Section>
     </Page>
   );
 };
@@ -57,6 +96,15 @@ export const pageQuery = graphql`
       name
       shortDescription
       longDescription
+      assets {
+        allowedToDownload
+        assetType
+        fileSize
+        mime
+        name
+        realFileName
+        url
+      }
       categories {
         categoryType
         name
