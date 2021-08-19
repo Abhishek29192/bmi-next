08705bae55239acb1557144616cf9676dcb83a9f
Francesco Moro <franzmoro@users.noreply.github.com>
2021-08-19 11:14:51 +0100

    feat(intouch-frontend): add OneTrust cookie consent banner

--- applications/intouch/terraform/frontend.tf
+++ applications/intouch/terraform/frontend.tf
@@ -58,6 +58,10 @@ resource "google_cloud_run_service" "default" {
           name  = "NODE_ENV"
           value = "production"
         }
+        env {
+          name  = "ONE_TRUST_GUID"
+          value = "c077f356-43eb-416b-81da-cb36e32b3f06-test"
+        }
       }
     }
   }
