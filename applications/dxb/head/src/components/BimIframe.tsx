4a21c92e87cb016bd509fc95e33e002535d591a8
Lucas Cheung <kwok.cheung@bmigroup.com>
2021-08-31 13:59:48 +0100

    updated schema and resources to get sdp bmi description updated systemdetailspage integrate with bimdescription and title updated bmiiframe to take classname for overwriting style updated tableadblock strcuture and style to align with UI desgin added migration log for sdp bmi description added bimdescription and title to tableadblock

--- applications/dxb/head/src/components/BimIframe.tsx
+++ applications/dxb/head/src/components/BimIframe.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import classnames from "classnames";
 import { Asset } from "./types/ProductBaseTypes";
 import styles from "./styles/BimIframe.module.scss";
 
 type Props = {
   url: string;
+  className?: string;
 };
 
 export const getBimIframeUrl = (assets?: ReadonlyArray<Asset>): string | null =>
   assets?.find((asset) => asset.assetType === "BIM")?.url || null;
 
-const BimIframe = ({ url }: Props) => (
-  <iframe className={styles["bimIframe"]} src={url} />
+const BimIframe = ({ url, className, ...rest }: Props) => (
+  <iframe
+    className={classnames(styles["bimIframe"], className)}
+    src={url}
+    {...rest}
+  />
 );
 
 export default BimIframe;
