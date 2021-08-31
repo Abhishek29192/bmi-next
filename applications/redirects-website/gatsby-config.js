40b2b60ebb927332b6bb4428220a698f8cdbd98f
Mahesh Warnasooriya <mahesh.warnasooriya@bmigroup.com>
2021-08-31 17:09:55 +0100

    feat(redirects-website): configure gatsby cloud

---
+++ applications/redirects-website/gatsby-config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  siteMetadata: {
+    title: `BMI dxb redirects`,
+    description: `Website for BMI Brand redirects`,
+    author: `bmi`,
+    siteUrl: `https://bmidxbredirects.gatsbyjs.io`
+  },
+  plugins: ["gatsby-plugin-gatsby-cloud"]
+};
--- applications/redirects-website/gatsby-node.js
+++ applications/redirects-website/gatsby-node.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const fs = require("fs");
+const toml = require("toml");
+
 exports.onCreatePage = async ({ page, actions }) => {
   const { createPage, deletePage } = actions;
   // Check if the page is a localized 404
@@ -12,5 +15,26 @@ exports.onCreatePage = async ({ page, actions }) => {
     // Recreate the modified page
     deletePage(oldPage);
     createPage(page);
+  }
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.createPages = async ({ graphql, actions }) => {
+  const { createRedirect } = actions;
+  const redirectsTomlFile = `./netlify.toml`;
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  if (fs.existsSync(redirectsTomlFile)) {
+    console.log("file exists");
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
+    const redirectsToml = fs.readFileSync(redirectsTomlFile);
+
+    const redirects = toml.parse(redirectsToml);
+    redirects.redirects.forEach((redirect) =>
+      createRedirect({
+        fromPath: redirect.from,
+        toPath: redirect.to,
+        isPermanent: !redirect.status || redirect.status == "301"
+      })
+    );
   }
 };
