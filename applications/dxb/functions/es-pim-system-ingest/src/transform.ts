49c80aa73f0fe77839887ea4344854360aa9cc25
Artem Makatera <armaka@softserveinc.com>
2021-09-09 16:43:36 +0300

    refactor: transform function

--- applications/dxb/functions/es-pim-system-ingest/src/transform.ts
+++ applications/dxb/functions/es-pim-system-ingest/src/transform.ts
@@ -1,10 +1,4 @@
 import type { System } from "./pim";
-// Can't use lodash pick as it's not type-safe
-const pick = <T, K extends keyof T>(obj: T, ...keys: K[]): Pick<T, K> => {
-  const ret = {} as Pick<T, K>;
-  keys.forEach((key) => (ret[key] = obj[key]));
-  return ret;
-};
 
 export type EsSystem = {
   approvalStatus: System["approvalStatus"];
@@ -23,17 +17,15 @@ const getBrandCode = (categories: System["categories"]): string | undefined => {
 };
 
 export const transformSystem = (system: System): EsSystem => {
+  const { approvalStatus, type, images, code, name, shortDescription } = system;
   const brand = getBrandCode(system.categories);
   return {
-    ...pick(
-      system,
-      "approvalStatus",
-      "type",
-      "images",
-      "code",
-      "name",
-      "shortDescription"
-    ),
-    brand
+    approvalStatus,
+    brand,
+    code,
+    images,
+    name,
+    shortDescription,
+    type
   };
 };
