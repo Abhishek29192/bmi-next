80e8f479e99f0e046b5c23b41e46f6214e7db8d2
Dominik Widomski <dominik@digital-detox.co.uk>
2021-09-21 11:17:16 +0100

    ci(gcp-pim-message-handler): set local coverage thresholds

--- applications/dxb/functions/gcp-pim-message-handler/jest.config.js
+++ applications/dxb/functions/gcp-pim-message-handler/jest.config.js
@@ -5,5 +5,14 @@ const sharedConfig = require("../../../../jest.config");
 module.exports = {
   ...sharedConfig,
   rootDir: "../../../../",
-  roots: ["<rootDir>/applications/dxb/functions/gcp-pim-message-handler/src"]
+  roots: ["<rootDir>/applications/dxb/functions/gcp-pim-message-handler/src"],
+  coverageThreshold: {
+    global: {
+      ...sharedConfig.coverageThreshold.global,
+      statements: "80",
+      branches: "50",
+      lines: "80",
+      functions: "80"
+    }
+  }
 };
