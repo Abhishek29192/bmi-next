92458ec516ee02f22b069fe97dc210da2903a482
Lucas Cheung <kwok.cheung@bmigroup.com>
2021-09-29 14:09:24 +0100

    - add condition to systems path and updated related schema

--- applications/dxb/head/src/schema/resolvers/Systems.js
+++ applications/dxb/head/src/schema/resolvers/Systems.js
@@ -5,15 +5,13 @@ const { generateIdFromString } = require("./utils/encryption");
 module.exports = {
   path: {
     async resolve(source) {
-      const id = generateIdFromString(source.code, false);
-      console.log(
-        `s/${source.name
-          .replace(new RegExp(/([\s])/gi), "-")
-          .toLowerCase()}/${id}`
-      );
-      return `s/${source.name
-        .replace(new RegExp(/([\s_])/gi), "-")
-        .toLowerCase()}/${id}`;
+      if (source.approvalStatus === "approved") {
+        const id = generateIdFromString(source.code, false);
+        return `s/${source.name
+          .replace(new RegExp(/([\s_])/gi), "-")
+          .toLowerCase()}/${id}`;
+      }
+      return null;
     }
   }
 };
