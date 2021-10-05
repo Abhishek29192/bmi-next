438a714d9ae7f3a40098dd184b81a93c05eb0a8f
Francesco Venica <francesco.venica@gmail.com>
2021-10-05 15:03:19 +0100

    feat(intouch): update db connection to ssl

--- applications/intouch/api/services/companies/src/db.ts
+++ applications/intouch/api/services/companies/src/db.ts
@@ -1,16 +1,55 @@
 import { PoolConfig, Pool, PoolClient } from "pg";
 
 let pool;
+
+const formatCert = (cert) => {
+  return cert
+    .replace("-----BEGIN CERTIFICATE-----", "-----BEGIN CERTIFICATE-----\\n")
+    .replace("-----END CERTIFICATE-----", "\\n-----END CERTIFICATE-----");
+};
+
+const formatKey = (cert) => {
+  return cert
+    .replace(
+      "-----BEGIN RSA PRIVATE KEY-----",
+      "-----BEGIN RSA PRIVATE KEY-----\\n"
+    )
+    .replace(
+      "-----END RSA PRIVATE KEY-----",
+      "\\n-----END RSA PRIVATE KEY-----"
+    );
+};
+
 export const getDbPool = () => {
   if (!pool) {
-    const { PG_USER, PG_DATABASE, PG_PORT, PG_PASSWORD, PG_HOST } = process.env;
+    const {
+      PG_USER,
+      PG_DATABASE,
+      PG_PORT,
+      PG_PASSWORD,
+      PG_HOST,
+      PG_SSL,
+      PG_SSL_CLIENT_KEY,
+      PG_SSL_CLIENT_CERT,
+      PG_SSL_SERVER_CA,
+      PG_REJECT_UNAUTHORIZED
+    } = process.env;
 
-    const dbConfig: PoolConfig = {
+    let dbConfig: PoolConfig = {
       host: PG_HOST,
       port: parseInt(PG_PORT),
       user: PG_USER,
       database: PG_DATABASE,
-      password: PG_PASSWORD
+      password: PG_PASSWORD,
+      ssl:
+        PG_SSL === "true"
+          ? {
+              rejectUnauthorized: PG_REJECT_UNAUTHORIZED === "true",
+              ca: formatCert(PG_SSL_SERVER_CA).replace(/\\n/g, "\n"),
+              key: formatKey(PG_SSL_CLIENT_KEY).replace(/\\n/g, "\n"),
+              cert: formatCert(PG_SSL_CLIENT_CERT).replace(/\\n/g, "\n")
+            }
+          : false
     };
 
     pool = new Pool(dbConfig);
--- applications/intouch/api/services/training/src/postgraphile/postgraphile.ts
+++ applications/intouch/api/services/training/src/postgraphile/postgraphile.ts
@@ -2,16 +2,54 @@ import postgraphile from "postgraphile";
 import { PoolConfig } from "pg";
 import postGraphileOptions from "./postGraphileOpts";
 
+const formatCert = (cert) => {
+  return cert
+    .replace("-----BEGIN CERTIFICATE-----", "-----BEGIN CERTIFICATE-----\\n")
+    .replace("-----END CERTIFICATE-----", "\\n-----END CERTIFICATE-----");
+};
+
+const formatKey = (cert) => {
+  return cert
+    .replace(
+      "-----BEGIN RSA PRIVATE KEY-----",
+      "-----BEGIN RSA PRIVATE KEY-----\\n"
+    )
+    .replace(
+      "-----END RSA PRIVATE KEY-----",
+      "\\n-----END RSA PRIVATE KEY-----"
+    );
+};
+
 export default () => {
-  const { PG_SCHEMA, PG_USER, PG_DATABASE, PG_PORT, PG_PASSWORD, PG_HOST } =
-    process.env;
+  const {
+    PG_USER,
+    PG_DATABASE,
+    PG_PORT,
+    PG_PASSWORD,
+    PG_HOST,
+    PG_SSL,
+    PG_SCHEMA,
+    PG_SSL_CLIENT_KEY,
+    PG_SSL_CLIENT_CERT,
+    PG_SSL_SERVER_CA,
+    PG_REJECT_UNAUTHORIZED
+  } = process.env;
 
   const dbConfig: PoolConfig = {
     host: PG_HOST,
     port: parseInt(PG_PORT),
     user: PG_USER,
     database: PG_DATABASE,
-    password: PG_PASSWORD
+    password: PG_PASSWORD,
+    ssl:
+      PG_SSL === "true"
+        ? {
+            rejectUnauthorized: PG_REJECT_UNAUTHORIZED === "true",
+            ca: formatCert(PG_SSL_SERVER_CA).replace(/\\n/g, "\n"),
+            key: formatKey(PG_SSL_CLIENT_KEY).replace(/\\n/g, "\n"),
+            cert: formatCert(PG_SSL_CLIENT_CERT).replace(/\\n/g, "\n")
+          }
+        : false
   };
 
   return postgraphile(dbConfig, PG_SCHEMA, postGraphileOptions);
