90f8371527797c1eea6068ed19ae4bee93e8a710
Umit Karaogul <umit@digital-detox.co.uk>
2021-10-28 17:17:55 +0100

    feat(intouch-frontend): create a component to show required evidence

---
+++ applications/intouch/frontend/components/Tabs/Uploads/RequirementDialog.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useTranslation } from "next-i18next";
+import Dialog from "@bmi/dialog";
+import { Document } from "@contentful/rich-text-types";
+import { RichText } from "../../RichText";
+
+export type RequirementDialogProps = {
+  isOpen: boolean;
+  description: Document;
+  onCloseClick: () => void;
+};
+
+const RequirementDialog = ({
+  isOpen,
+  description,
+  onCloseClick
+}: RequirementDialogProps) => {
+  const { t } = useTranslation("project-page");
+
+  return (
+    <Dialog open={isOpen} onCloseClick={onCloseClick}>
+      <Dialog.Title hasUnderline>
+        {t("upload_tab.requirementModal.title")}
+      </Dialog.Title>
+      <Dialog.Content data-testid="requirement-modal-content">
+        <RichText content={description} />
+      </Dialog.Content>
+    </Dialog>
+  );
+};
+
+export default RequirementDialog;
