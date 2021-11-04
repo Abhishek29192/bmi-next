50ecd321a3d92c0ecf6da1f4728ea9806767f3a1
Igor Tykhonov <itykh@softserveinc.com>
2021-11-04 19:14:55 +0200

    feat(head): add url structure pdp,add check GATSBY_USE_LEGACY_FILTERS

--- applications/dxb/head/src/schema/resolvers/Products.js
+++ applications/dxb/head/src/schema/resolvers/Products.js
@@ -5,6 +5,7 @@ const {
   generateIdFromString,
   generateDigestFromData
 } = require("../../utils/encryption");
+const { combineVariantClassifications } = require("../../utils/filters");
 const {
   getFormatFromFileName,
   isPimLinkDocument
@@ -61,6 +62,45 @@ const resolvePathFromFamily = async (source, args, context) => {
   return resolvePath(parentFamilies[0], args, context);
 };
 
+const getSimpleUrlStructure = (source, variant, id) => {
+  const productName = variant.name || source.name;
+  const classifications = combineVariantClassifications(source, variant);
+  const featuresCodesForUrlParams = {
+    appearanceAttributes: ["colourfamily", "texturefamily"],
+    generalInformation: ["materials"]
+  };
+
+  const classificationsPath = classifications.reduce(
+    (urlFromClassifications, classification) => {
+      const featuresCodes = featuresCodesForUrlParams[classification.code];
+      if (featuresCodes) {
+        const urlParamsFromClassificationFeatures = featuresCodes.reduce(
+          (urlFromFeatures, featuresCode) => {
+            const featureByFeatureCode = classification.features.find(
+              (feature) => feature.code.endsWith(featuresCode)
+            );
+            if (featureByFeatureCode) {
+              const featureValue = featureByFeatureCode.featureValues[0].value;
+              urlFromFeatures.push(featureValue);
+            }
+            return urlFromFeatures;
+          },
+          []
+        );
+        urlFromClassifications.push(...urlParamsFromClassificationFeatures);
+      }
+      return urlFromClassifications;
+    },
+    []
+  );
+  return [productName, ...classificationsPath, id]
+    .join("-")
+    .replace(/\s+/g, "-")
+    .replace("*", "")
+    .replace('"', "")
+    .toLowerCase();
+};
+
 module.exports = {
   variantOptions: {
     async resolve(source, args, context) {
@@ -77,7 +117,13 @@ module.exports = {
               [source.name, ...getSlugAttributes(variant), id].join("/")
             )
           });
-          const path = `p/${getUrlFromPath(breadcrumbs)}`;
+
+          const path = (() => {
+            if (process.env.GATSBY_USE_SIMPLE_PDP_URL_STRUCTURE === "true") {
+              return `p/${getSimpleUrlStructure(source, variant, id)}`;
+            }
+            return `p/${getUrlFromPath(breadcrumbs)}`;
+          })();
 
           return {
             ...variant,
