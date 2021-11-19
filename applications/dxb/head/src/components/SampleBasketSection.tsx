2527fdcf8c17254b1b60ad0becc8353e28b866ab
Hitesh Khatri <hitesh.khatri@bmigroup.com>
2021-11-19 11:55:08 +0000

    feat(head): remove sample from basket

--- applications/dxb/head/src/components/SampleBasketSection.tsx
+++ applications/dxb/head/src/components/SampleBasketSection.tsx
@@ -1,42 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { graphql } from "gatsby";
 
 import Button from "@bmi/button";
 import Section from "@bmi/section";
-import { ShoppingCart } from "@material-ui/icons";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import ShoppingCart from "@material-ui/icons/ShoppingCart";
 import {
   SampleOrderElement,
   useBasketContext
 } from "../contexts/SampleBasketContext";
 import { extractFeatureValuesByClassification } from "../utils/product-url-path";
 import RichText, { RichTextData } from "./RichText";
-import SampleBasketSectionProducts, {
-  getFeatures
-} from "./SampleBasketSectionProducts";
+import SampleBasketSectionProducts from "./SampleBasketSectionProducts";
 import { useSiteContext } from "./Site";
 
 import FormSection, { Data as FormData } from "./FormSection";
 import styles from "./styles/SampleBasketSection.module.scss";
 import { ClassificationCodeEnum, FeatureCodeEnum } from "./types/pim";
+import { Data as PageInfoData } from "./PageInfo";
+import { getCTA } from "./Link";
 
 export type Data = {
   __typename: "SampleBasketSection";
-  description: RichTextData;
+  description: RichTextData | null;
   checkoutFormSection: FormData | null;
+  emptyBasketMessage: RichTextData | null;
+  browseProductsCTALabel: string | null;
+  browseProductsCTA: PageInfoData | null;
 };
 
 const SampleBasketSection = ({
-  data: { description, checkoutFormSection }
+  data: {
+    description,
+    checkoutFormSection,
+    emptyBasketMessage,
+    browseProductsCTALabel,
+    browseProductsCTA
+  }
 }: {
   data: Data;
 }) => {
   const [isCompleteFormShow, setCompleteFormShow] = useState(false);
+  const [hasSamplesInTheBasket, setHasSamplesInTheBasket] = useState(false);
 
   const handleCompleteClick = () => setCompleteFormShow(true);
 
-  const { getMicroCopy } = useSiteContext();
+  const { getMicroCopy, countryCode } = useSiteContext();
 
   const { basketState } = useBasketContext();
+
+  useEffect(() => {
+    setHasSamplesInTheBasket(basketState && basketState.products.length > 0);
+  }, [basketState]);
+
   const samples: SampleOrderElement[] = basketState.products.map((sample) => {
     const { classifications } = sample;
     const featureAttributeMapForUrl = {
@@ -59,6 +75,10 @@ const SampleBasketSection = ({
     };
   });
 
+  const basketCta =
+    browseProductsCTA &&
+    getCTA(browseProductsCTA, countryCode, browseProductsCTA.title);
+
   return (
     <>
       <Section
@@ -67,7 +87,18 @@ const SampleBasketSection = ({
       >
         <RichText document={description} />
         <SampleBasketSectionProducts />
-        {!isCompleteFormShow && (
+        {!hasSamplesInTheBasket && <RichText document={emptyBasketMessage} />}
+
+        {!hasSamplesInTheBasket && basketCta && basketCta.action && (
+          <Button
+            endIcon={<ArrowForwardIcon />}
+            variant="outlined"
+            action={basketCta.action}
+          >
+            {browseProductsCTALabel}
+          </Button>
+        )}
+        {!isCompleteFormShow && hasSamplesInTheBasket && (
           <div className={styles["complete-button"]}>
             <Button endIcon={<ShoppingCart />} onClick={handleCompleteClick}>
               {getMicroCopy("pdp.overview.completeSampleOrder")}
@@ -75,7 +106,7 @@ const SampleBasketSection = ({
           </div>
         )}
       </Section>
-      {isCompleteFormShow && (
+      {isCompleteFormShow && hasSamplesInTheBasket && (
         <Section>
           <FormSection
             data={checkoutFormSection}
@@ -98,6 +129,13 @@ export const query = graphql`
     }
     checkoutFormSection {
       ...FormSectionFragment
+    }
+    emptyBasketMessage {
+      ...RichTextFragment
+    }
+    browseProductsCTALabel
+    browseProductsCTA {
+      ...PageInfoFragment
     }
   }
 `;
---
+++ libraries/migrate/projects/dxb/migrations/sampleBasket/20211122140738-add-empty-basket-fields.js
@@ -0,0 +1,81 @@
+module.exports.description =
+  "Add empty basket fields on sample basket content type";
+
+module.exports.up = (migration) => {
+  const sampleBasket = migration.editContentType("sampleBasket");
+
+  sampleBasket
+    .createField("emptyBasketMessage")
+    .name("Empty Basket Message")
+    .type("RichText")
+    .localized(true)
+    .required(true);
+
+  sampleBasket
+    .createField("browseProductsCTALabel")
+    .name("Browse products CTA label")
+    .type("Symbol")
+    .localized(true)
+    .required(true);
+
+  sampleBasket
+    .createField("browseProductsCTA")
+    .name("Browse products CTA Link page")
+    .type("Link")
+    .required(true)
+    .validations([
+      {
+        linkContentType: [
+          "page",
+          "contactUsPage",
+          "teamPage",
+          "homePage",
+          "productListerPage",
+          "documentLibraryPage",
+          "brandLandingPage"
+        ]
+      }
+    ])
+    .linkType("Entry");
+
+  //editors and help text for fields
+  sampleBasket.changeFieldControl(
+    "emptyBasketMessage",
+    "builtin",
+    "richTextEditor",
+    {
+      helpText:
+        "This text will be visible to user when basket becomes empty. e.g. 'Your basket is empty.'"
+    }
+  );
+
+  sampleBasket.changeFieldControl(
+    "browseProductsCTALabel",
+    "builtin",
+    "singleLine",
+    {
+      helpText:
+        "Provide label for a CTA which will be shown to user when basket is empty. e.g. 'Browse our products'"
+    }
+  );
+
+  //move fields to right places
+  sampleBasket
+    .moveField("emptyBasketMessage")
+    .afterField("checkoutFormSection");
+
+  sampleBasket
+    .moveField("browseProductsCTALabel")
+    .afterField("emptyBasketMessage");
+
+  sampleBasket
+    .moveField("browseProductsCTA")
+    .afterField("browseProductsCTALabel");
+};
+
+module.exports.down = (migration) => {
+  const sampleBasket = migration.editContentType("sampleBasket");
+  sampleBasket.deleteField("emptyBasketMessage");
+  sampleBasket.deleteField("browseProductsCTALabel");
+  sampleBasket.deleteField("browseProductsCTA");
+};
