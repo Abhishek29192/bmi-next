acb74795f26bf4fa06d925444dae82cd67ce43b1
Umit Karaogul <umit@digital-detox.co.uk>
2021-11-23 15:29:27 +0000

    test(intouch-frontend): test report download action

--- applications/intouch/frontend/components/Reports/Guarantee/__tests__/index.test.tsx
+++ applications/intouch/frontend/components/Reports/Guarantee/__tests__/index.test.tsx
@@ -1,11 +1,29 @@
 import React from "react";
-import { renderWithI18NProvider } from "../../../../lib/tests/utils";
+import { renderWithI18NProvider, screen } from "../../../../lib/tests/utils";
 import GuaranteeReport from "..";
 
+const mockgetGuarantesReport = jest.fn();
+jest.mock("../../../../graphql/generated/hooks", () => ({
+  useGetGuaranteesReportLazyQuery: () => [mockgetGuarantesReport]
+}));
+
 describe("GuaranteeReport Component", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
+  });
+
   it("renders correctly", () => {
     const { container } = renderWithI18NProvider(<GuaranteeReport />);
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+  it("should export button click", () => {
+    renderWithI18NProvider(<GuaranteeReport />);
+
+    const exportButton = screen.getByTestId("export-button");
+    exportButton.click();
+
+    expect(mockgetGuarantesReport).toHaveBeenCalled();
   });
 });
--- applications/intouch/frontend/components/Reports/Team/__tests__/index.test.tsx
+++ applications/intouch/frontend/components/Reports/Team/__tests__/index.test.tsx
@@ -1,11 +1,29 @@
 import React from "react";
-import { renderWithI18NProvider } from "../../../../lib/tests/utils";
+import { renderWithI18NProvider, screen } from "../../../../lib/tests/utils";
 import TeamReport from "..";
 
+const mockGetTeamReport = jest.fn();
+jest.mock("../../../../graphql/generated/hooks", () => ({
+  useGetTeamsReportLazyQuery: () => [mockGetTeamReport]
+}));
+
 describe("TeamReport Component", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
+  });
+
   it("renders correctly", () => {
     const { container } = renderWithI18NProvider(<TeamReport />);
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+  it("should export button click", () => {
+    renderWithI18NProvider(<TeamReport />);
+
+    const exportButton = screen.getByTestId("export-button");
+    exportButton.click();
+
+    expect(mockGetTeamReport).toHaveBeenCalled();
   });
 });
