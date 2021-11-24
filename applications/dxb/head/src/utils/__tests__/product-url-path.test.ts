a16ca7e1b6db319c8692b4c1c1d4ee6192d09429
Hitesh Khatri <hitesh.khatri@bmigroup.com>
2021-11-24 12:10:01 +0000

    feat(head): replace decimals and underscores in simple urls

--- applications/dxb/head/src/utils/__tests__/product-url-path.test.ts
+++ applications/dxb/head/src/utils/__tests__/product-url-path.test.ts
@@ -10,58 +10,129 @@ import createClassification, {
 describe("product-url-path tests", () => {
   describe("generateUrl tests", () => {
     describe("When url parts has special characters", () => {
-      describe("And one single url part is provided with ONLY special characters", () => {
-        it("removes ALL special characters", () => {
-          const result = generateUrl([
-            `±!£$%^&*()_+~\`¡€#¢∞§¶•–≠«‘“{}\`∑®†¥¨^¬˚∆˙©∂≈√∫~≤≥÷≠`
-          ]);
-          expect(result).toEqual("");
+      describe("And one single url part is provided", () => {
+        describe("with ONLY special characters", () => {
+          it("removes ALL special characters", () => {
+            const result = generateUrl([
+              `±!£$%^&*()+~\`¡€#¢∞§¶•–≠«‘“{}\`∑®†¥¨^¬˚∆˙©∂≈√∫~≤≥÷≠`
+            ]);
+            expect(result).toEqual("");
+          });
+        });
+        describe("with special characters and `_`", () => {
+          it("removes ALL special characters as well as all occuranes of spaces '_' with single '-'", () => {
+            const result = generateUrl([
+              `±!£$%___^&*()+~_\`¡€#¢______∞§¶•–≠«‘“{}\`_____∑®†¥¨^¬˚∆˙©∂≈√∫~≤≥÷≠`
+            ]);
+            expect(result).toEqual("-");
+          });
+        });
+        describe("with special characters and multiple spaces", () => {
+          it("removes ALL special characters as well as all occuranes of spaces with single '-'", () => {
+            const result = generateUrl([
+              `±!£$%    ^&*()+~_\`¡€#¢    ∞§¶•–≠«‘“{}\`   ∑®†¥¨^¬˚∆˙©∂≈√∫~≤≥÷≠`
+            ]);
+            expect(result).toEqual("-");
+          });
+        });
+        describe("with special characters, '_' and multiple spaces", () => {
+          it("removes ALL special characters as well as all occuranes of '_' and spaces with single '-'", () => {
+            const result = generateUrl([
+              `±!£$%    ^&*()+~_\`______¡€#¢    ∞§¶•–≠«___‘“{}\`   ∑®†¥¨^¬˚∆˙©∂______≈√∫~≤≥÷≠`
+            ]);
+            expect(result).toEqual("-");
+          });
+        });
+        describe("with some text", () => {
+          it("removes ALL special characters", () => {
+            const result = generateUrl([
+              `Thickness\`: 1~40#mm $Wid!th:1.'5m, Th{ic?kn;"e}s\\s: 200[m]m W<i>d,th:2.5m`
+            ]);
+            expect(result).toEqual(
+              "thickness-140mm-width1-5m-thickness-200mm-wid-th2-5m"
+            );
+          });
         });
       });
-      describe("And one single url part is provided with some text", () => {
-        it("removes ALL special characters", () => {
-          const result = generateUrl([
-            `Thickness\`: 1~40#mm $Wid!th:1.'5m, Th{ic?kn;"e}s\\s: 200[m]m W<i>d,th:2.5m`
-          ]);
-          expect(result).toEqual(
-            "thickness-140mm-width15m-thickness-200mm-width25m"
-          );
-        });
-      });
+
       describe("And multiple url parts are provided", () => {
-        it("removes ALL special characters", () => {
-          const result = generateUrl([
-            `Thickness\`: 1~40#mm $Wid!th:1.'5m, Th{ic?kne}s\\s: 200[m]m W<i>d,th:2.5m`,
-            null,
-            `color\`: teg~lrød# $hEIg!ht:2.+9&0m=, we{ig?ht}\\: 300(k)g W@i>d{th:3}3m-også`
-          ]);
-          expect(result).toEqual(
-            "thickness-140mm-width15m-thickness-200mm-width25m-color-teglrød-height290m-weight-300kg-width33m-også"
-          );
+        describe("with special characters", () => {
+          it("removes ALL special characters", () => {
+            const result = generateUrl([
+              `Thickness\`: 1~40#mm $Wid!th:1.'5m, Th{ic?kne}s\\s: 200[m]m W<i>d,th:2.5m`,
+              null,
+              `color\`: teg~lrød# $hEIg!ht:2.+9&0m=, we{ig?ht}\\: 300(k)g W@i>d{th:3}3m-også`
+            ]);
+            expect(result).toEqual(
+              "thickness-140mm-width1-5m-thickness-200mm-wid-th2-5m-color-teglrød-height2-90m-weight-300kg-width33m-også"
+            );
+          });
         });
-      });
-      describe("And multiple url parts with unicode characters", () => {
-        it("removes ALL special characters", () => {
-          const result = generateUrl([
-            `Thickness\`: 1~40#mm $Widœ∑´®†¥¨^øπ“‘æ…˚∆˙©ƒ∂ßåç≈√∫~µ≤≥÷≠–ª•¶§∞¢#€¡‚·!th:1.'5m, Th{ic?kne}s\\s: 200[m]m W<i>d,th:2.5m£~$@~∑´œ∑´˙˚∆≤~çß¬´ß¬˚ƒ•¶#~¡€˚∆˙˙çß¬∂∆˚çßµ∫~∫∫ß√ç¬…¬å¨^¥∑´˙√ç`,
-            null,
-            `color\`: teg~lrød# $hEIg!ht:2.+9&0m=, we{ig?ht}\\: 300(k)g W@i>d{th:3}3m-også`
-          ]);
-          expect(result).toEqual(
-            "thickness-140mm-widœøπæƒßåçµªth15m-thickness-200mm-width25mœçßßƒçßçßµßçåç-color-teglrød-height290m-weight-300kg-width33m-også"
-          );
+
+        describe("with unicode characters", () => {
+          it("removes ALL special characters and unicode characters", () => {
+            const result = generateUrl([
+              `Thickness\`: 1~40#mm $Widœ∑´®†¥¨^øπ“‘æ…˚∆˙©ƒ∂ßåç≈√∫~µ≤≥÷≠–ª•¶§∞¢#€¡‚·!th:1.'5m, Th{ic?kne}s\\s: 200[m]m W<i>d,th:2.5m£~$@~∑´œ∑´˙˚∆≤~çß¬´ß¬˚ƒ•¶#~¡€˚∆˙˙çß¬∂∆˚çßµ∫~∫∫ß√ç¬…¬å¨^¥∑´˙√ç`,
+              null,
+              `color\`: teg~lrød# $hEIg!ht:2.+9&0m=, we{ig?ht}\\: 300(k)g W@i>d{th:3}3m-også`
+            ]);
+            expect(result).toEqual(
+              "thickness-140mm-widœøπæƒßåçµªth1-5m-thickness-200mm-wid-th2-5mœçßßƒçßçßµßçåç-color-teglrød-height2-90m-weight-300kg-width33m-også"
+            );
+          });
         });
-      });
-      describe("And DE characters with unicode characters", () => {
-        it("removes ALL special characters", () => {
-          const result = generateUrl([
-            `Thickness\`: 1~40#mm $Widœ∑´®†¥¨^øπ“‘æ…˚∆˙©ƒ∂ßåç≈√∫~µ≤≥÷≠–ª•¶§∞¢#€¡‚·!th:1.'5m, Th{ic?kne}s\\s: 200[m]m W<i>d,th:2.5m£~$@~∑´œ∑´˙˚∆≤~çLebensabschnittsgefährte rectequeß¬´ß¬˚ƒ•¶#~¡€˚∆˙˙çß¬∂∆˚çßµ∫~∫∫ß√ç¬…¬å¨^¥∑´˙√ç`,
-            null,
-            `color\`: teg~lrød# $hEIg!ht:2.+9&adhuc laboramus sadipscing per Vorsprung durch Technik in mei ullum gloriatur0m=, we{ig?ht}\\: 300(k)g W@Die Ärztei>d{th:3}3m-også`
-          ]);
-          expect(result).toEqual(
-            "thickness-140mm-widœøπæƒßåçµªth15m-thickness-200mm-width25mœçlebensabschnittsgefährte-rectequeßßƒçßçßµßçåç-color-teglrød-height29adhuc-laboramus-sadipscing-per-vorsprung-durch-technik-in-mei-ullum-gloriatur0m-weight-300kg-wdie-ärzteidth33m-også"
-          );
+        describe("with DE (German) and unicode characters", () => {
+          it("removes ALL special characters and keeps language specific characters", () => {
+            const result = generateUrl([
+              `Thickness\`: 1~40#mm $Widœ∑´®†¥¨^øπ“‘æ…˚∆˙©ƒ∂ßåç≈√∫~µ≤≥÷≠–ª•¶§∞¢#€¡‚·!th:1.'5m, Th{ic?kne}s\\s: 200[m]m W<i>d,th:2.5m£~$@~∑´œ∑´˙˚∆≤~çLebensabschnittsgefährte rectequeß¬´ß¬˚ƒ•¶#~¡€˚∆˙˙çß¬∂∆˚çßµ∫~∫∫ß√ç¬…¬å¨^¥∑´˙√ç`,
+              null,
+              `color\`: teg~lrød# $hEIg!ht:2.+9&adhuc laboramus sadipscing per Vorsprung durch Technik in mei ullum gloriatur0m=, we{ig?ht}\\: 300(k)g W@Die Ärztei>d{th:3}3m-også`
+            ]);
+            expect(result).toEqual(
+              "thickness-140mm-widœøπæƒßåçµªth1-5m-thickness-200mm-wid-th2-5mœçlebensabschnittsgefährte-rectequeßßƒçßçßµßçåç-color-teglrød-height2-9adhuc-laboramus-sadipscing-per-vorsprung-durch-technik-in-mei-ullum-gloriatur0m-weight-300kg-wdie-ärzteidth33m-også"
+            );
+          });
+        });
+        describe("with `.` and `,`", () => {
+          describe("And there are single `.` and `,` in many separate locations", () => {
+            it("removes all `.` and `,` and replaces them with single `-`", () => {
+              const result = generateUrl([
+                `Thickness: 1.40mm Width:1,5m,   Thickness: 20,000,00nm Width:2.5m`,
+                null,
+                `color: teglrød hEIght:2.90m, weight: 300(k)g Width:33333,33.33m-også, material=ubehandlet overflate`
+              ]);
+              expect(result).toEqual(
+                "thickness-1-40mm-width1-5m-thickness-20-000-00nm-width2-5m-color-teglrød-height2-90m-weight-300kg-width33333-33-33m-også-materialubehandlet-overflate"
+              );
+            });
+          });
+
+          describe("And there are repeated series of `.` and `,`", () => {
+            it("removes ALL occurances of repeated `.` and `,` and replaces them with single `-`", () => {
+              const result = generateUrl([
+                `Thickness: 1.,40mm Width:1,.5m,, Thickness: 20.,.,000,.00nm Width:2.,5m`,
+                null,
+                `color: teglrød hEIght:2..90m, weight: 300(k)g Width:33333,,33..,..,,.33m---også, material=ubehandlet overflate`
+              ]);
+              expect(result).toEqual(
+                "thickness-1-40mm-width1-5m-thickness-20-000-00nm-width2-5m-color-teglrød-height2-90m-weight-300kg-width33333-33-33m-også-materialubehandlet-overflate"
+              );
+            });
+          });
+
+          describe("And there are repeated series of `.` , '_' and `,`", () => {
+            it("removes ALL occurances of repeated `.` , '_' and `,` and replaces them with single `-`", () => {
+              const result = generateUrl([
+                "zanda_arktis___normalstein",
+                `Thickness: 1.,__40mm Width:1,.5m,, Thick__ness: 20.,.,000,.00nm Width:2.__,5m`,
+                null,
+                `color: teglrød hE_Ight:2..90m, weight: 300(k)g Width:33333,,33..,..,,.33m---også, material=ubehandlet overflate`
+              ]);
+              expect(result).toEqual(
+                "zanda-arktis-normalstein-thickness-1-40mm-width1-5m-thick-ness-20-000-00nm-width2-5m-color-teglrød-he-ight2-90m-weight-300kg-width33333-33-33m-også-materialubehandlet-overflate"
+              );
+            });
+          });
         });
       });
     });
