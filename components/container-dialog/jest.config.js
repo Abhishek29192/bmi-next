c692989309f265db264746a0acf8134b5689c210
Vladyslav Aleksieiev <vladyslav.aleksieiev@gmail.com>
2021-11-25 13:37:02 +0200

    test(container-dialog): increase test coverage to 100%

--- components/container-dialog/jest.config.js
+++ components/container-dialog/jest.config.js
@@ -8,13 +8,5 @@ module.exports = {
   roots: ["<rootDir>/components/container-dialog/src"],
   collectCoverageFrom: [
     "<rootDir>/components/container-dialog/src/**/*.{ts,tsx,js}"
-  ],
-  coverageThreshold: {
-    global: {
-      ...sharedConfig.coverageThreshold.global,
-      statements: "95",
-      branches: "70",
-      lines: "95"
-    }
-  }
+  ]
 };
