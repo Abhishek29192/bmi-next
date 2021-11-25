293f43167dc8e58148b4c7a5a4ef75830422901f
Oksana Bilokin <obelo@softserveinc.com>
2021-11-25 15:49:46 +0200

    test(autocomplete): increase test coverage to 100%

--- components/autocomplete/src/__tests__/Autocomplete.test.tsx
+++ components/autocomplete/src/__tests__/Autocomplete.test.tsx
@@ -20,4 +20,44 @@ describe("Autocomplete component", () => {
     );
     expect(container.firstChild).toMatchSnapshot();
   });
+
+  it("renders correctly with start icon", () => {
+    const { container } = render(
+      <Autocomplete
+        id="autocomplete-test"
+        options={[
+          "Icopal",
+          "Monier",
+          "Monarplan",
+          "Monarflex",
+          "Arrow",
+          "Zanda",
+          "AeroDek"
+        ]}
+        startAdornmentIcon="HardHatHead"
+      />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("renders correctly with all data option component", () => {
+    const { container } = render(
+      <Autocomplete.Option
+        text="text"
+        secondaryText="secondaryText"
+        matches={[
+          { length: 1, offset: 1 },
+          { length: 2, offset: 2 }
+        ]}
+      />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("renders correctly with partial data option component", () => {
+    const { container } = render(
+      <Autocomplete.Option text="text" secondaryText="secondaryText" />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
