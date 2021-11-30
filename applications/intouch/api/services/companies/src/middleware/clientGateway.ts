46013354bc964ff98785317d5ded99ccdfed1fdc
Francesco Venica <francesco.venica@gmail.com>
2021-11-30 14:36:39 +0000

    fix(intouch): send activation email in the right language

--- applications/intouch/api/services/companies/src/middleware/clientGateway.ts
+++ applications/intouch/api/services/companies/src/middleware/clientGateway.ts
@@ -4,6 +4,22 @@ import { getGCPToken } from "../utils";
 
 export default (req, res, next: NextFunction) => {
   const logger = req.logger("gateway:client");
+
+  /* 
+    In Auth0 we can't use a dynamic redirect_to url that contains the
+    user market, so we are redirecting the use to a default market (`no`).
+    The first time the user login will be in the defulat market insted of his
+    real market, he will not have a `req?.user?.market?.domain` becuase it is not
+    in our db yet, in thic case we overwrite the `x-request-market-domain` to match
+    the user market in order fetch everything from the right market (email included).
+
+    Super Admin doesn't have a market so for him we skip this overwrite
+   */
+  if (req.user.role !== "SUPER_ADMIN" && !req?.user?.market?.domain) {
+    req.headers["x-request-market-domain"] =
+      req?.user[`${process.env.AUTH0_NAMESPACE}/intouch_market_code`];
+  }
+
   req.clientGateway = async (query: string, variables: Object) => {
     const { GATEWAY_URL } = process.env;
     const bearer = await getGCPToken(GATEWAY_URL);
