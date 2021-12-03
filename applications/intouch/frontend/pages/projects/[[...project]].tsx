7d7c7c97bab8830fbffcb28ec079cda0d82c1598
Dominik Widomski <dominik@digital-detox.co.uk>
2021-12-03 11:48:36 +0000

    fix(intouch-frontend): move deactivated company header logic

--- applications/intouch/frontend/pages/projects/[[...project]].tsx
+++ applications/intouch/frontend/pages/projects/[[...project]].tsx
@@ -67,14 +67,7 @@ const Projects = ({
   };
 
   return (
-    <Layout
-      title={t("common:Projects")}
-      attentionHeading={
-        activeProject?.company?.status === "DEACTIVATED" &&
-        t("project-page:deactivatedCompany")
-      }
-      pageData={globalPageData}
-    >
+    <Layout title={t("common:Projects")} pageData={globalPageData}>
       <div className={layoutStyles.sidePanelWrapper}>
         <ProjectSidePanel
           projects={sortedProjects}
