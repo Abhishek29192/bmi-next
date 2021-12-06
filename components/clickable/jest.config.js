f0c6dbfb8cb040dc8f6a55e44fafe7ffeb12a9e5
Vladyslav Aleksieiev <vladyslav.aleksieiev@gmail.com>
2021-12-06 10:42:08 +0200

    test(clickable): increase test coverage to 100%

--- components/clickable/jest.config.js
+++ components/clickable/jest.config.js
@@ -6,13 +6,5 @@ module.exports = {
   ...sharedConfig,
   rootDir: "../../",
   roots: ["<rootDir>/components/clickable/src"],
-  collectCoverageFrom: ["<rootDir>/components/clickable/src/**/*.{ts,tsx,js}"],
-  coverageThreshold: {
-    global: {
-      ...sharedConfig.coverageThreshold.global,
-      statements: "92",
-      branches: "85",
-      lines: "92"
-    }
-  }
+  collectCoverageFrom: ["<rootDir>/components/clickable/src/**/*.{ts,tsx,js}"]
 };
