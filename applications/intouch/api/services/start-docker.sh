c1da1aed65a56d755038675cf11d90cdc447e913
Francesco Venica <francesco.venica@gmail.com>
2021-12-06 12:08:10 +0000

    feat(intouch): update docker compose for local development

--- applications/intouch/api/services/start-docker.sh
+++ applications/intouch/api/services/start-docker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 docker-compose down;
-docker volume rm services_intouch_company_db;
-docker volume rm services_intouch_training_db;
+docker volume rm services_intouch_db;
 docker-compose up -d;
-docker logs postgres-company-db;
-docker logs postgres-training-db;
+docker logs intouch-db;
