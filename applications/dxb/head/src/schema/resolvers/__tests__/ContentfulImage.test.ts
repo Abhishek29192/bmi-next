9f77b94c9198733e1b44db216a1d27e8994125e2
Vladyslav Aleksieiev <vladyslav.aleksieiev@gmail.com>
2021-12-10 15:48:40 +0200

    test(head): writing tests contentful-image resolver

---
+++ applications/dxb/head/src/schema/resolvers/__tests__/ContentfulImage.test.ts
@@ -0,0 +1,71 @@
+import ContentfulImage from "../ContentfulImage";
+import { Context, Node } from "../types";
+
+const context: Context = {
+  nodeModel: {
+    getAllNodes: jest.fn(),
+    getNodeById: jest.fn(),
+    getNodesByIds: jest.fn(),
+    runQuery: jest.fn()
+  }
+};
+
+const source: Node = {
+  id: "source",
+  children: null,
+  parent: null,
+  internal: null,
+  image___NODE: "image",
+  focalPoint___NODE: "focalPoint"
+};
+
+describe("ContentfulImage resolver", () => {
+  it("should contain specific type", () => {
+    expect(ContentfulImage.focalPoint.type).toBe("FocalPoint");
+  });
+  it("should return null if source without image___NODE and focalPoint___NODE", async () => {
+    expect(
+      await ContentfulImage.focalPoint.resolve(
+        { ...source, image___NODE: null, focalPoint___NODE: null },
+        null,
+        context
+      )
+    ).toBeNull();
+  });
+  it("should return null if no focalPoint found", async () => {
+    context.nodeModel.getNodeById = jest.fn().mockResolvedValue({
+      file: {
+        details: {
+          image: {
+            width: 100,
+            height: 100
+          }
+        }
+      }
+    });
+
+    expect(
+      await ContentfulImage.focalPoint.resolve(source, null, context)
+    ).toBeNull();
+  });
+  it("should resolve focalPoint", async () => {
+    context.nodeModel.getNodeById = jest.fn().mockResolvedValue({
+      file: {
+        details: {
+          image: {
+            width: 120,
+            height: 120
+          }
+        }
+      },
+      focalPoint: {
+        x: 20,
+        y: 80
+      }
+    });
+
+    expect(
+      await ContentfulImage.focalPoint.resolve(source, null, context)
+    ).toEqual({ x: 17, y: 67 });
+  });
+});
