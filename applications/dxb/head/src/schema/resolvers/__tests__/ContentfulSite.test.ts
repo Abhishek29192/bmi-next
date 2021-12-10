79ac680324dd12e2e8e632fef43457b25466c0db
Vladyslav Aleksieiev <vladyslav.aleksieiev@gmail.com>
2021-12-10 15:48:40 +0200

    test(head): writing tests contentful-site resolver

---
+++ applications/dxb/head/src/schema/resolvers/__tests__/ContentfulSite.test.ts
@@ -0,0 +1,27 @@
+import ContentfulSite from "../ContentfulSite";
+import { Context } from "../types";
+
+const context: Context = {
+  nodeModel: {
+    getAllNodes: jest.fn().mockResolvedValue([{ type: "ContentfulSite" }]),
+    getNodeById: jest.fn(),
+    getNodesByIds: jest.fn(),
+    runQuery: jest.fn()
+  }
+};
+
+describe("ContentfulSite resolver", () => {
+  it("should contain specific type", () => {
+    expect(ContentfulSite.regions.type).toEqual(["RegionJson"]);
+  });
+  it("should resolve regions", async () => {
+    expect(await ContentfulSite.regions.resolve(null, null, context)).toEqual([
+      { type: "ContentfulSite" }
+    ]);
+
+    expect(context.nodeModel.getAllNodes).toHaveBeenCalledWith(
+      { type: "RegionJson" },
+      { connectionType: "RegionJson" }
+    );
+  });
+});
