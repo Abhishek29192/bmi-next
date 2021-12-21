e73c4a12d8012d5397e2aee01bf2235a66d7181b
Yevgen Stepanenko <ystepa@softserveinc.com>
2021-12-21 18:49:53 +0200

    test(filters): increase coverage to 100%
    Update modules-sha

--- components/filters/jest.config.js
+++ components/filters/jest.config.js
@@ -6,11 +6,5 @@ module.exports = {
   ...sharedConfig,
   rootDir: "../../",
   roots: ["<rootDir>/components/filters/src"],
-  collectCoverageFrom: ["<rootDir>/components/filters/src/**/*.{ts,tsx,js}"],
-  coverageThreshold: {
-    global: {
-      ...sharedConfig.coverageThreshold.global,
-      branches: "87"
-    }
-  }
+  collectCoverageFrom: ["<rootDir>/components/filters/src/**/*.{ts,tsx,js}"]
 };
--- components/filters/src/index.ts
+++ components/filters/src/index.ts
@@ -1,3 +1,4 @@
+// istanbul ignore file: doesn't hold any logic
 import Filters from "./Filters";
 
 export { Props as FilterProps, Filter } from "./Filters";
