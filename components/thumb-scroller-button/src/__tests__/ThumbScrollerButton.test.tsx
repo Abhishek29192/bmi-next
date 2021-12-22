6da1f3b5fea7303ff43255506a59bc63bf6e3634
“Dilaram <dilaram.gaire@bmigroup.com>
2021-12-22 08:50:56 +0000

    test(thumb-scroller-button): increase coverage to 100%

--- components/thumb-scroller-button/src/__tests__/ThumbScrollerButton.test.tsx
+++ components/thumb-scroller-button/src/__tests__/ThumbScrollerButton.test.tsx
@@ -12,4 +12,24 @@ describe("ThumbScrollerButton component", () => {
     );
     expect(container.firstChild).toMatchSnapshot();
   });
+  it("renders correctly when direction is left", () => {
+    const { container } = render(
+      <ThumbScrollerButton
+        direction="left"
+        onClick={() => console.log("Left clicked!")}
+      />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it("renders correctly when className is provided", () => {
+    const { container } = render(
+      <ThumbScrollerButton
+        className="testClass"
+        direction="left"
+        onClick={() => console.log("Left clicked!")}
+      />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.querySelectorAll(".testClass").length).toBe(1);
+  });
 });
