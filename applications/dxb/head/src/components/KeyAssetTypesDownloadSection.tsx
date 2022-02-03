3176d028ce71be873a0aca3c95809ce9a2addbc9
Dmitriy Shubin <dshub@softserveinc.com>
2022-02-03 11:16:39 +0200

    fix(head): resolve esLint issue

--- applications/dxb/head/src/components/KeyAssetTypesDownloadSection.tsx
+++ applications/dxb/head/src/components/KeyAssetTypesDownloadSection.tsx
@@ -31,11 +31,12 @@ type CommonData = {
   assetType: string;
 };
 
-const GTMButton = withGTM<
-  ButtonProps & {
-    action?: ClickableAction;
-  }
->(Button);
+const GTMButton =
+  withGTM<
+    ButtonProps & {
+      action?: ClickableAction;
+    }
+  >(Button);
 
 export const handleDownloadClick = async (
   list: CommonData[],
