c6aa9b20ae960a59c9c9e8fe63875cf306333ff7
“Dilaram <dilaram.gaire@bmigroup.com>
2022-02-09 12:23:24 +0000

    fix(head): change name optional

--- applications/dxb/head/src/components/TitleWithContent.tsx
+++ applications/dxb/head/src/components/TitleWithContent.tsx
@@ -4,7 +4,7 @@ import { RichTextData } from "../components/RichText";
 
 export type Data = {
   __typename: "ContentfulTitleWithContent";
-  name: string;
+  name?: string;
   title: string | null;
   content: RichTextData | null;
 };
