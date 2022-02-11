acdabb2fe5e2c1c3f70b81daf97b0febcc9fd315
Nazar Antoniuk <nanto@softserveinc.com>
2022-02-11 13:47:48 +0200

    IRP-1103: add additional test cases

--- applications/intouch/frontend/__tests__/pages/project.test.tsx
+++ applications/intouch/frontend/__tests__/pages/project.test.tsx
@@ -1,6 +1,18 @@
-import { getServerSideProps } from "../../pages/projects/[[...project]]";
+import React from "react";
+import Projects, {
+  getServerSideProps
+} from "../../pages/projects/[[...project]]";
 import { generateProject } from "../../lib/tests/factories/project";
 import { ROLES } from "../../lib/constants";
+import { generateGlobalPageData } from "../../lib/tests/factories/globalPageData";
+import { GetGlobalDataQuery } from "../../graphql/generated/operations";
+import { renderWithUserProvider } from "../../lib/tests/utils";
+import AccountContextWrapper from "../../lib/tests/fixtures/account";
+import ApolloProvider from "../../lib/tests/fixtures/apollo";
+import { generateMarketContext } from "../../lib/tests/factories/market";
+import { generateAccount } from "../../lib/tests/factories/account";
+import MarketContextWrapper from "../../lib/tests/fixtures/market";
+import { ErrorStatusCode } from "../../lib/error";
 
 jest.mock("../../lib/middleware/withPage", () => ({
   withPage: (getServerSideProps: any) => {
@@ -21,6 +33,7 @@ jest.mock("next-i18next/serverSideTranslations", () => ({
 }));
 
 describe("Projects Page", () => {
+  const globalPageData: GetGlobalDataQuery = generateGlobalPageData();
   const defaultContext = {
     apolloClient: {
       query: jest.fn()
@@ -28,6 +41,7 @@ describe("Projects Page", () => {
     res: {}
   };
 
+  const projects = [generateProject()];
   it("should view all projects if super admin", async () => {
     const context = {
       ...defaultContext,
@@ -37,12 +51,11 @@ describe("Projects Page", () => {
       params: {},
       market: { id: 1 }
     };
-    const project = generateProject();
     mockGetServerPageGetProjects.mockImplementation(() => {
       return {
         props: {
           data: {
-            projectsByMarket: { nodes: [project] }
+            projectsByMarket: { nodes: projects }
           }
         }
       };
@@ -103,5 +116,86 @@ describe("Projects Page", () => {
         globalPageData: undefined
       }
     });
+  });
+  it("return props for forbidden or missing project", async () => {
+    const context = {
+      ...defaultContext,
+      account: {
+        role: ROLES.SUPER_ADMIN
+      },
+      params: {
+        project: [{ name: "not existing project", id: 9999 }]
+      },
+      market: { id: 1 }
+    };
+    mockGetServerPageGetProjects.mockImplementation(() => {
+      return {
+        props: {
+          data: {
+            projectsByMarket: { nodes: projects }
+          }
+        }
+      };
+    });
+
+    const result = await getServerSideProps(context);
+    expect(result).toEqual({
+      props: {
+        _pageError: {
+          statusCode: ErrorStatusCode.NOT_FOUND,
+          title: "Not found"
+        },
+        globalPageData: undefined
+      }
+    });
+  });
+  it("check default props", async () => {
+    const context = {
+      ...defaultContext,
+      account: {
+        role: ROLES.SUPER_ADMIN
+      },
+      params: {},
+      market: { id: 1 }
+    };
+    mockGetServerPageGetProjects.mockImplementation(() => {
+      return {
+        props: {
+          data: {
+            projectsByMarket: { nodes: [] }
+          }
+        }
+      };
+    });
+
+    const result = await getServerSideProps(context);
+    expect(result).toEqual({
+      props: {
+        isPowerfulUser: true,
+        projects: {
+          nodes: []
+        }
+      }
+    });
+  });
+  it("render correctly", async () => {
+    const acc = generateAccount({ role: "SUPER_ADMIN" });
+    const { container } = renderWithUserProvider(
+      <ApolloProvider>
+        <MarketContextWrapper>
+          <AccountContextWrapper account={acc}>
+            <Projects
+              projects={{ nodes: projects }}
+              isPowerfulUser
+              globalPageData={globalPageData}
+              market={generateMarketContext()}
+              account={acc}
+              _pageError={{ statusCode: 401 }}
+            />
+          </AccountContextWrapper>
+        </MarketContextWrapper>
+      </ApolloProvider>
+    );
+    expect(container).toMatchSnapshot();
   });
 });
