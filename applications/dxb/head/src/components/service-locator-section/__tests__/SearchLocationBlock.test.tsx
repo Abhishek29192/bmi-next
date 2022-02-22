3f7324c03798b526f5e1ff75557877190c55c7bb
Dmitriy Shubin <dshub@softserveinc.com>
2022-02-22 16:42:40 +0200

    feat(service-locator-section): add analytics

--- applications/dxb/head/src/components/service-locator-section/__tests__/SearchLocationBlock.test.tsx
+++ applications/dxb/head/src/components/service-locator-section/__tests__/SearchLocationBlock.test.tsx
@@ -2,7 +2,6 @@ import { act, fireEvent, render } from "@testing-library/react";
 import { GoogleApi } from "@bmi/components";
 import React from "react";
 import { SearchLocationBlock } from "../components";
-import { EntryTypeEnum } from "../../Service";
 import "@testing-library/jest-dom";
 import { googleMock } from "../__mocks__/google";
 
@@ -12,7 +11,7 @@ describe("SearchLocationBlock component", () => {
   it("should renders correctly with CircularProgress component if google === undefined", () => {
     const { getByRole } = render(
       <SearchLocationBlock
-        sectionType={EntryTypeEnum.ROOFER_TYPE}
+        autocompleteLabel={"MC: findARoofer.companyFieldLabel"}
         handleAutocompleteOnChange={jest.fn}
         handlePlaceChange={jest.fn}
         options={[]}
@@ -36,7 +35,7 @@ describe("SearchLocationBlock component", () => {
     const testValue = "test2";
     const { getByRole } = render(
       <SearchLocationBlock
-        sectionType={EntryTypeEnum.ROOFER_TYPE}
+        autocompleteLabel={"MC: findARoofer.companyFieldLabel"}
         handleAutocompleteOnChange={jest.fn}
         handlePlaceChange={jest.fn}
         options={["test1", "test2", "test3"]}
@@ -57,7 +56,7 @@ describe("SearchLocationBlock component", () => {
     const { getByRole } = render(
       <GoogleApi.Provider value={googleMock}>
         <SearchLocationBlock
-          sectionType={EntryTypeEnum.ROOFER_TYPE}
+          autocompleteLabel={"MC: findARoofer.companyFieldLabel"}
           handleAutocompleteOnChange={jest.fn}
           handlePlaceChange={jest.fn}
           options={[]}
@@ -91,7 +90,7 @@ describe("SearchLocationBlock component", () => {
     const { getByRole } = render(
       <GoogleApi.Provider value={googleMock}>
         <SearchLocationBlock
-          sectionType={EntryTypeEnum.ROOFER_TYPE}
+          autocompleteLabel={"MC: findARoofer.companyFieldLabel"}
           handleAutocompleteOnChange={jest.fn}
           handlePlaceChange={jest.fn}
           options={[]}
@@ -108,10 +107,9 @@ describe("SearchLocationBlock component", () => {
     expect(getPosition).toHaveBeenCalled();
   });
   it("should render autocomplete input with label merchantNameSearchLabel if sectionType === EntryTypeEnum.MERCHANT_TYPE", () => {
-    const sectionType = EntryTypeEnum.MERCHANT_TYPE;
     const { getByRole } = render(
       <SearchLocationBlock
-        sectionType={sectionType}
+        autocompleteLabel={"MC: findARoofer.merchantNameSearchLabel"}
         handleAutocompleteOnChange={jest.fn}
         handlePlaceChange={jest.fn}
         options={["test1", "test2", "test3"]}
--- applications/dxb/head/src/components/service-locator-section/components/ServiceLocatorChips.tsx
+++ applications/dxb/head/src/components/service-locator-section/components/ServiceLocatorChips.tsx
@@ -1,11 +1,12 @@
 import { Grid } from "@bmi/components";
 import { Typography } from "@bmi/components";
-import { Chip } from "@bmi/components";
+import { Chip, ChipProps } from "@bmi/components";
 import camelCase from "lodash-es/camelCase";
 import React from "react";
 import styles from "../styles/ServiceLocatorSection.module.scss";
 import { useSiteContext } from "../../Site";
 import { ServiceType, ServiceTypesPrefixesEnum } from "../../Service";
+import withGTM from "../../../utils/google-tag-manager";
 
 interface ServiceLocatorChipsProps {
   microCopyPrefix: ServiceTypesPrefixesEnum;
@@ -21,6 +22,8 @@ export const ServiceLocatorChips = ({
   activeFilters
 }: ServiceLocatorChipsProps) => {
   const { getMicroCopy } = useSiteContext();
+  const GTMChip = withGTM<ChipProps>(Chip);
+
   return (
     <Grid item xs={12} md={6} lg={8}>
       <div className={styles["filters"]}>
@@ -28,19 +31,27 @@ export const ServiceLocatorChips = ({
           <Typography variant="h6" className={styles["chips-label"]}>
             {getMicroCopy(`${microCopyPrefix}.filtersLabel`)}
           </Typography>
-          {uniqueRoofTypeByData.map((serviceType, index) => (
-            <Chip
-              key={index}
-              type="selectable"
-              onClick={() => onChipClick(serviceType)}
-              // eslint-disable-next-line security/detect-object-injection
-              isSelected={activeFilters[serviceType]}
-            >
-              {getMicroCopy(
-                `${microCopyPrefix}.filters.${camelCase(serviceType)}`
-              )}
-            </Chip>
-          ))}
+          {uniqueRoofTypeByData.map((serviceType, index) => {
+            const chipMicroCopy = getMicroCopy(
+              `${microCopyPrefix}.filters.${camelCase(serviceType)}`
+            );
+            return (
+              <GTMChip
+                key={index}
+                type="selectable"
+                onClick={() => onChipClick(serviceType)}
+                // eslint-disable-next-line security/detect-object-injection
+                isSelected={activeFilters[serviceType]}
+                gtm={{
+                  id: "selector-cards1",
+                  label: chipMicroCopy,
+                  action: "Selector - Cards Filter"
+                }}
+              >
+                {chipMicroCopy}
+              </GTMChip>
+            );
+          })}
         </div>
       </div>
     </Grid>
