2d6fe054a38316c52ab3adf5105fdd49455aa87e
valeksi <valeksi@softserveinc.com>
2022-03-07 10:22:48 +0200

    fix(gcp-form-submit): add email validation to cloud function

--- libraries/migrate/projects/dxb/migrations/scripts/20220216104417-change-recipients-validation-pattern.js
+++ libraries/migrate/projects/dxb/migrations/scripts/20220216104417-change-recipients-validation-pattern.js
@@ -5,7 +5,8 @@ module.exports.up = (migration) => {
   form.editField("recipients").validations([
     {
       regexp: {
-        pattern: "^(?:\\b\\w[\\w.-]*@([\\w-]+\\.)+[\\w-]+\\b[\\s,]*){1,4}$"
+        pattern:
+          "^((\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+)\\s*,?\\s*){1,4}$"
       },
       message: "You should provide up to 4 valid emails"
     }
