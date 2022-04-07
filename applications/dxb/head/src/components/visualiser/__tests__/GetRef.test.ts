2726a379cae24146dc37807b7d83e8d23883bdad
Igor Tykhonov <itykh@softserveinc.com>
2022-04-07 12:29:54 +0200

    test(head): add tests

--- applications/dxb/head/src/components/visualiser/__tests__/GetRef.test.ts
+++ applications/dxb/head/src/components/visualiser/__tests__/GetRef.test.ts
@@ -11,13 +11,13 @@ describe("getRef", () => {
     const result = getRef(ref, null);
     expect(result).toEqual(undefined);
   });
-  it("should return url for http urls", () => {
+  it("should return url for http protocol urls", () => {
     const ref = "http://test-url.com";
     const result = getRef(ref, null);
     expect(result).toEqual("//test-url.com");
   });
 
-  it("should return url for https urls", () => {
+  it("should return url for https protocol urls", () => {
     const ref = "https://test-url.com";
     const result = getRef(ref, null);
     expect(result).toEqual("//test-url.com");
@@ -27,54 +27,54 @@ describe("getRef", () => {
     const result = getRef(ref, null);
     expect(result).toEqual("//test-url.com");
   });
-  it("should return url for url urls", () => {
-    const ref = "fa://test-url.com";
+  it("should return url for url protocol urls", () => {
+    const ref = "url://test-url.com";
     const result = getRef(ref, null);
     expect(result).toEqual("//test-url.com");
   });
-  it("should return url for fad urls", () => {
+  it("should return url for fad protocol urls", () => {
     const ref = "fad://test-url.com";
     const result = getRef(ref, null);
     expect(result).toEqual("//test-url.com");
   });
-  it("should return url for fas urls", () => {
+  it("should return url for fas protocol urls", () => {
     const ref = "fas://test-url.com";
     const result = getRef(ref, null);
     expect(result).toEqual("//test-url.com");
   });
-  it("should return url for far urls", () => {
+  it("should return url for far protocol urls", () => {
     const ref = "far://test-url.com";
     const result = getRef(ref, null);
     expect(result).toEqual("//test-url.com");
   });
-  it("should return url for fal urls", () => {
+  it("should return url for fal protocol urls", () => {
     const ref = "fal://test-url.com";
     const result = getRef(ref, null);
     expect(result).toEqual("//test-url.com");
   });
-  it("should return url for fab urls", () => {
+  it("should return url for fab protocol urls", () => {
     const ref = "fab://test-url.com";
     const result = getRef(ref, null);
     expect(result).toEqual("//test-url.com");
   });
-  it("should return url for fr urls", () => {
+  it("should return url for fr protocol urls", () => {
     const ref = "fr://test-url.com";
     const result = getRef(ref, null);
     expect(result).toEqual("//test-url.com");
   });
-  it("should return url for emoji urls", () => {
+  it("should return url for emoji protocol urls", () => {
     const ref = "emoji://test-url.com";
     const result = getRef(ref, null);
     expect(result).toEqual("//test-url.com");
   });
-  it("should return url for pablic urls without options", () => {
+  it("should return url for pablic protocol urls without options", () => {
     const ref = "public://part/part-2/file-part.file-type";
     const result = getRef(ref, null);
     expect(result).toEqual(
       "/content///part/part-2/file-part-original.file-type"
     );
   });
-  it("should return url for pablic urls with options", () => {
+  it("should return url for pablic protocol urls with options", () => {
     const ref = "public:part.sub-part/part-2/file-part.file-type";
     const result = getRef(ref, {
       size: "default-size",
@@ -86,7 +86,7 @@ describe("getRef", () => {
       "test-api-host:////part.sub-part/content/part-2/file-directory/file-part-default-size.file-type"
     );
   });
-  it("should return url for pablic urls with options and hadServer = false", () => {
+  it("should return url for pablic protocol urls with options and hadServer = false", () => {
     const ref = "public:part/part-2/file-part.file-type";
     const result = getRef(ref, {
       size: "default-size",
@@ -98,7 +98,7 @@ describe("getRef", () => {
       "test-api-host://source-test//content/part/part-2/file-directory/file-part-default-size.file-type"
     );
   });
-  it("should return url for private urls with options and hadServer = false", () => {
+  it("should return url for private protocol urls with options and hadServer = false", () => {
     const ref = "private:part/part-2/file-part.file-type";
     const result = getRef(ref, {
       size: "default-size",
@@ -110,7 +110,7 @@ describe("getRef", () => {
       "test-api-host://source-test//content-private/part/part-2/file-directory/file-part-default-size.file-type"
     );
   });
-  it("should return url for private urls with mp4 options and hadServer = false", () => {
+  it("should return url for private protocol urls with mp4 options and hadServer = false", () => {
     const ref = "private:file-part.mp4";
     const result = getRef(ref, {
       size: "default-size",
