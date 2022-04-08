8b948fbb69c9d6fe115df99cd878cd8de15a4920
Benjamin <benjamin@benjaminsproule.com>
2022-04-08 11:52:49 +0100

    fix(head): allow pim data to be disabled

--- applications/dxb/head/gatsby-config.js
+++ applications/dxb/head/gatsby-config.js
@@ -373,45 +373,52 @@ module.exports = {
         }
       })
     ),
-    {
-      resolve: "@bmi/gatsby-plugin-firestore",
-      options: {
-        credential: {
-          type: "service_account",
-          project_id: process.env.GCP_PROJECT_ID,
-          private_key_id: process.env.FIRESTORE_PRIVATE_KEY_ID,
-          private_key: process.env.FIRESTORE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
-          client_email: process.env.FIRESTORE_CLIENT_EMAIL,
-          client_id: process.env.FIRESTORE_CLIENT_ID,
-          auth_uri: process.env.FIRESTORE_AUTH_URI,
-          token_uri: process.env.FIRESTORE_TOKEN_URI,
-          auth_provider_x509_cert_url:
-            process.env.FIRESTORE_AUTH_PROVIDER_X509_CERT_URL,
-          client_x509_cert_url: process.env.FIRESTORE_CLIENT_X509_CERT_URL
-        },
-        // TODO: Can we type these better?
-        types: [
+    ...(process.env.DISABLE_PIM_DATA === "true"
+      ? []
+      : [
           {
-            type: "Products",
-            collection: `${process.env.FIRESTORE_ROOT_COLLECTION}/root/products`,
-            map: (doc) => ({
-              // TODO: More explicit data mapping? Any data mapping at all? This is a big complex document.
-              // Certain things (the arrays) should be split into collections?
-              ...doc
-            })
-          },
-          {
-            type: "Systems",
-            collection: `${process.env.FIRESTORE_ROOT_COLLECTION}/root/systems`,
-            map: (doc) => {
-              return {
-                ...doc
-              };
+            resolve: "@bmi/gatsby-plugin-firestore",
+            options: {
+              credential: {
+                type: "service_account",
+                project_id: process.env.GCP_PROJECT_ID,
+                private_key_id: process.env.FIRESTORE_PRIVATE_KEY_ID,
+                private_key: (process.env.FIRESTORE_PRIVATE_KEY || "").replace(
+                  /\\n/gm,
+                  "\n"
+                ),
+                client_email: process.env.FIRESTORE_CLIENT_EMAIL,
+                client_id: process.env.FIRESTORE_CLIENT_ID,
+                auth_uri: process.env.FIRESTORE_AUTH_URI,
+                token_uri: process.env.FIRESTORE_TOKEN_URI,
+                auth_provider_x509_cert_url:
+                  process.env.FIRESTORE_AUTH_PROVIDER_X509_CERT_URL,
+                client_x509_cert_url: process.env.FIRESTORE_CLIENT_X509_CERT_URL
+              },
+              // TODO: Can we type these better?
+              types: [
+                {
+                  type: "Products",
+                  collection: `${process.env.FIRESTORE_ROOT_COLLECTION}/root/products`,
+                  map: (doc) => ({
+                    // TODO: More explicit data mapping? Any data mapping at all? This is a big complex document.
+                    // Certain things (the arrays) should be split into collections?
+                    ...doc
+                  })
+                },
+                {
+                  type: "Systems",
+                  collection: `${process.env.FIRESTORE_ROOT_COLLECTION}/root/systems`,
+                  map: (doc) => {
+                    return {
+                      ...doc
+                    };
+                  }
+                }
+              ]
             }
           }
-        ]
-      }
-    },
+        ]),
     ...elasticSearchPlugin,
     {
       resolve: `gatsby-plugin-sass`,
