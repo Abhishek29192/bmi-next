66704671bd2d47c332c9d0cde1aab18d57585c53
Mahesh Warnasooriya <mahesh.warnasooriya@bmigroup.com>
2022-06-07 14:27:32 +0100

    feat(contentful-tag-utility): export sleep

--- applications/dxb/libraries/contentful-tag-utility/src/index.ts
+++ applications/dxb/libraries/contentful-tag-utility/src/index.ts
@@ -1,4 +1,9 @@
 // istanbul ignore file: doesn't hold any logic
-export { fillDefaultValues, publishAll, tagAndUpdate } from "./coordinate";
+export {
+  fillDefaultValues,
+  publishAll,
+  sleep,
+  tagAndUpdate
+} from "./coordinate";
 export { copyDefaultValues, findIrrelevantLocales } from "./locale";
 export { createTag, tagEntity } from "./tag";
