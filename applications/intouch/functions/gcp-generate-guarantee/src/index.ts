82127bde08445c0b4ec5a08dd27cea462a9eb5b2
Lucas Cheung <kwok.cheung@bmigroup.com>
2022-06-24 17:19:40 +0100

    fix(intouch): fix guarantee company name not showing

--- applications/intouch/functions/gcp-generate-guarantee/src/index.ts
+++ applications/intouch/functions/gcp-generate-guarantee/src/index.ts
@@ -1,29 +1,33 @@
+import { writeFileSync, mkdir } from "fs";
+import { dirname } from "path";
 import { MailService } from "@sendgrid/mail";
 import { AttachmentData } from "@sendgrid/helpers/classes/attachment";
 import { Guarantee } from "@bmi/intouch-api-types";
 import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
 import logger from "@bmi-digital/functions-logger";
+import { mockSolutionGuarantee } from "../mocks/guarantee";
 import { replaceData } from "./util/mail";
 import GuaranteePdfGenerator from "./GuaranteePdf";
 import StorageClient from "./storage-client";
 import GatewayClient from "./GatewayClient";
 
-const { GCP_SECRET_PROJECT, MAIL_FROM, GCP_PRIVATE_BUCKET_NAME } = process.env;
-
-const getSendGridClient = async () => {
-  const secretManagerClient = new SecretManagerServiceClient();
-  const [value] = await secretManagerClient.accessSecretVersion({
-    name: `projects/${GCP_SECRET_PROJECT}/secrets/SENDGRID_API_KEY/versions/latest`
-  });
-  const sendGridClient = new MailService();
-  sendGridClient.setApiKey(value.payload.data.toString());
-  return sendGridClient;
-};
 export const sendGuaranteePdf = async (postEvent: any) => {
+  const { GCP_SECRET_PROJECT, MAIL_FROM, GCP_PRIVATE_BUCKET_NAME, NODE_ENV } =
+    process.env;
+  const isLocal = NODE_ENV === "local";
+  const getSendGridClient = async () => {
+    const secretManagerClient = new SecretManagerServiceClient();
+    const [value] = await secretManagerClient.accessSecretVersion({
+      name: `projects/${GCP_SECRET_PROJECT}/secrets/SENDGRID_API_KEY/versions/latest`
+    });
+    const sendGridClient = new MailService();
+    sendGridClient.setApiKey(value.payload.data.toString());
+    return sendGridClient;
+  };
   try {
-    const payload: Guarantee = JSON.parse(
-      Buffer.from(postEvent.data, "base64").toString()
-    );
+    const payload: Guarantee = isLocal
+      ? mockSolutionGuarantee
+      : JSON.parse(Buffer.from(postEvent.data, "base64").toString());
 
     logger.info({
       message: `successfully fetched guarantee with ID: ${payload.id}`
@@ -46,6 +50,13 @@ export const sendGuaranteePdf = async (postEvent: any) => {
     const guaranteePdf = new GuaranteePdfGenerator(payload);
 
     const file = await guaranteePdf.create();
+    if (isLocal) {
+      const path = "./pdf/blank.pdf";
+      mkdir(dirname(path), function (err) {
+        if (err && err.code !== "EEXIST") return logger.error(err);
+        writeFileSync(path, file.data, { flag: "w" });
+      });
+    }
 
     // Write file to google cloud
     const fileName = `guarantee/${id}/${file.name}`;
@@ -56,28 +67,34 @@ export const sendGuaranteePdf = async (postEvent: any) => {
     );
 
     // Update guarantee
-    await gatewayClient.updateGuaranteeFileStorage(id, fileName);
+    const response = await gatewayClient.updateGuaranteeFileStorage(
+      id,
+      fileName
+    );
+    if (response.ok) {
+      const attachment: AttachmentData = {
+        content: Buffer.from(file.data).toString("base64"),
+        filename: file.name,
+        type: "application/pdf",
+        disposition: "attachment"
+      };
 
-    const attachment: AttachmentData = {
-      content: Buffer.from(file.data).toString("base64"),
-      filename: file.name,
-      type: "application/pdf",
-      disposition: "attachment"
-    };
-
-    const sendgridClient = await getSendGridClient();
-    const replyTo = "no-reply@intouch.bmigroup.com";
-    await sendgridClient.send({
-      replyTo,
-      to: project.buildingOwnerMail,
-      from: MAIL_FROM,
-      subject: template.mailSubject,
-      text: replaceData(template.mailBody, {
-        product,
-        system
-      }),
-      attachments: [attachment]
-    });
+      const sendgridClient = await getSendGridClient();
+      const replyTo = "no-reply@intouch.bmigroup.com";
+      await sendgridClient.send({
+        replyTo,
+        to: project.buildingOwnerMail,
+        from: MAIL_FROM,
+        subject: template.mailSubject,
+        text: replaceData(template.mailBody, {
+          product,
+          system
+        }),
+        attachments: [attachment]
+      });
+    } else {
+      logger.error({ message: `${response.status} ${response.statusText}` });
+    }
   } catch (error) {
     logger.error(error as any);
   }
