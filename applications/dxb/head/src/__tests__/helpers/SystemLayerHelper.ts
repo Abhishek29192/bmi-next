3984ec1458323be7ae882eb4f93acbe6f1b426de
Hitesh Khatri <hitesh.khatri@bmigroup.com>
2022-06-30 15:02:53 +0100

    fix(*): compile head

--- applications/dxb/head/src/__tests__/helpers/SystemLayerHelper.ts
+++ applications/dxb/head/src/__tests__/helpers/SystemLayerHelper.ts
@@ -4,7 +4,7 @@ import createProduct from "./ProductHelper";
 const createSystemLayer = (systemLayer?: Partial<SystemLayer>): SystemLayer => {
   return {
     name: "system-layer-1",
-    layerNumber: 1,
+    layerNumber: "1",
     type: "layer-type-1",
     relatedProducts: [
       createProduct({ code: "related-product-1", name: "related-product-1" })
