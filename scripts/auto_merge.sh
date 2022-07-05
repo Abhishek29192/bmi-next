033d64e0f2e84f431eddc99eef27f13a35848816
Nikita Makarov <mmakaro@softserveinc.com>
2022-07-05 19:44:13 +0300

    ci: add auto-merge

---
+++ scripts/auto_merge.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+MINOR_VERSION=`echo "$CI_COMMIT_TAG" | awk -F'[v.-]' '{print $4}'`
+which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
+eval $(ssh-agent -s)
+ssh-add <(echo "$SSH_PRIV_KEY_ENCODED" | base64 -d)
+mkdir -p ~/.ssh
+ssh-keyscan gitlab.com >> ~/.ssh/known_hosts
+
+merge_preprod () {
+  git checkout master
+  git merge pre-production
+  if [ $? -ne 0 ];
+    then
+    echo "Will open merge request for pre-production in the next job..."
+    echo "PREPROD_MR=true" >> $CI_PROJECT_DIR/post_deploy.env
+    exit 0
+  else
+    echo "Auto-merge of pre-production was successful"
+  fi
+  git push origin master
+}
+
+merge_prod () {
+  git checkout pre-production
+  git merge production
+  if [ $? -ne 0 ];
+    then
+    echo "Will open two merge requests for production & pre-production in the next job..."
+    echo "PROD_MR=true" >> $CI_PROJECT_DIR/post_deploy.env
+    exit 0
+  else
+    echo "Auto-merge of production was successful"
+  fi
+  git push origin pre-production
+  merge_preprod
+}
+
+git clone git@gitlab.com:bmi-digital/dxb.git && cd dxb
+git config user.email "gitlab-runner@not-existing.com"
+git config user.name "Gitlab Runner"
+git checkout pre-production
+git checkout production
+
+if [ "$MINOR_VERSION" == "1" ];
+  then
+  merge_preprod
+elif [ "$MINOR_VERSION" == "0" ];
+  then
+  merge_prod
+else
+  echo "Something went wrong: the minor version of the release is unknown"
+  exit 1
+fi
