773035f1d32a8bdc91898d1acd8787f526207493
Nazar Antoniuk <nanto@softserveinc.com>
2022-07-14 10:23:12 +0300

    feat (project) Add data atributes for QA needs

--- applications/intouch/frontend/components/Tabs/Uploads/__tests__/AddEvidenceDialog.test.tsx
+++ applications/intouch/frontend/components/Tabs/Uploads/__tests__/AddEvidenceDialog.test.tsx
@@ -20,8 +20,26 @@ describe("AddEvidenceDialog Components", () => {
     expect(screen.queryByTestId("add-evidence")).toBeNull();
   });
 
+  it("should close modal window", () => {
+    const onCloseClick = jest.fn();
+    renderWithI18NProvider(
+      <AddEvidenceDialog
+        isOpen={true}
+        onCloseClick={onCloseClick}
+        onConfirmClick={jest.fn()}
+        loading={false}
+      />
+    );
+
+    const closeButton = screen.getByText(
+      "upload_tab.add_evidence_modal.cancel_label"
+    );
+    closeButton.click();
+    expect(onCloseClick).toBeCalledTimes(1);
+  });
+
   it("should upload files on confirmClick event triggered", () => {
-    const onConfirmClick = jest.fn((files) => {});
+    const onConfirmClick = jest.fn(() => {});
     renderWithI18NProvider(
       <AddEvidenceDialog
         isOpen={true}
@@ -52,5 +70,100 @@ describe("AddEvidenceDialog Components", () => {
     );
     confirmButton.click();
     expect(onConfirmClick).toHaveBeenCalledWith("MISCELLANEOUS", null, files);
+  });
+
+  it("cover different category types", () => {
+    const onConfirmClick = jest.fn(() => {});
+    renderWithI18NProvider(
+      <AddEvidenceDialog
+        isOpen={true}
+        onCloseClick={jest.fn()}
+        onConfirmClick={onConfirmClick}
+        loading={false}
+        defaultEvidenceCategory="FLAT_BASE"
+      />
+    );
+    const addEvidence = screen.queryByTestId("add-evidence");
+    expect(addEvidence).toBeTruthy();
+
+    const files = [
+      new File([""], "name1", { type: "pdf" }),
+      new File([""], "name2", { type: "pdf" })
+    ];
+
+    const evidenceCategoryType: EvidenceCategoryType = "MISCELLANEOUS";
+    const customEvidenceCategoryKey: string = null;
+    fireEvent.change(addEvidence, {
+      target: {
+        evidenceCategoryType,
+        customEvidenceCategoryKey,
+        files
+      }
+    });
+
+    const confirmButton = screen.getByText(
+      "upload_tab.add_evidence_modal.confirm_label"
+    );
+    confirmButton.click();
+    expect(onConfirmClick).toHaveBeenCalledWith("CUSTOM", "FLAT_BASE", files);
+  });
+
+  it("validate big files size", () => {
+    const onConfirmClick = jest.fn(() => {});
+    renderWithI18NProvider(
+      <AddEvidenceDialog
+        isOpen={true}
+        onCloseClick={jest.fn()}
+        onConfirmClick={onConfirmClick}
+        loading={false}
+      />
+    );
+    const addEvidence = screen.queryByTestId("add-evidence");
+    expect(addEvidence).toBeTruthy();
+    const MAX_FILE_SIZE = 40;
+    const files = [
+      new File([""], "name1", { type: "pdf" }),
+      new File([""], "name2", { type: "pdf" })
+    ];
+    Object.defineProperty(files[0], "size", {
+      value: MAX_FILE_SIZE * 1024 * 1024 * 2
+    });
+    Object.defineProperty(files[1], "size", {
+      value: MAX_FILE_SIZE * 1024 * 1024 * 2
+    });
+    const evidenceCategoryType: EvidenceCategoryType = "MISCELLANEOUS";
+    const customEvidenceCategoryKey: string = null;
+    fireEvent.change(addEvidence, {
+      target: {
+        evidenceCategoryType,
+        customEvidenceCategoryKey,
+        files
+      }
+    });
+    const confirmButton = screen.getByText(
+      "upload_tab.add_evidence_modal.confirm_label"
+    );
+    confirmButton.click();
+    expect(onConfirmClick).toBeCalledTimes(0);
+  });
+
+  it("should behave properly on category change", () => {
+    const onConfirmClick = jest.fn(() => {});
+    renderWithI18NProvider(
+      <AddEvidenceDialog
+        isOpen={true}
+        onCloseClick={jest.fn()}
+        onConfirmClick={onConfirmClick}
+        loading={false}
+        evidenceCategories={[{ name: "Waterproofing", referenceCode: "TEST1" }]}
+      />
+    );
+
+    const selectList = screen
+      .getByTestId("evidence-categories-list")
+      .querySelector("input");
+    fireEvent.change(selectList, {
+      target: { value: "TEST1" }
+    });
   });
 });
