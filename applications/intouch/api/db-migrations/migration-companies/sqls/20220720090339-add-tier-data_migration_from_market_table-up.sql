65c722c53029a2beda9d2ef5111db3707120091c
Prithvi Tomer <prithvi.tomer@bmigroup.com>
2022-07-20 16:07:45 +0100

    #1511 update tier to include T5 TO T7

--- applications/intouch/api/db-migrations/migration-companies/sqls/20220720090339-add-tier-data_migration_from_market_table-up.sql
+++ applications/intouch/api/db-migrations/migration-companies/sqls/20220720090339-add-tier-data_migration_from_market_table-up.sql
@@ -21,6 +21,8 @@ grant select on docebo_tier to company_admin;
 grant select on docebo_tier to installer;
 grant select on docebo_tier to auditor;
 
+ALTER TABLE docebo_tier ENABLE ROW LEVEL SECURITY;
+
 DROP POLICY IF EXISTS policy_super_admin ON docebo_tier;
 CREATE POLICY policy_super_admin ON docebo_tier FOR ALL TO super_admin USING (true);
 
@@ -36,10 +38,13 @@ CREATE POLICY policy_installer ON docebo_tier FOR SELECT TO installer USING (cur
 DROP POLICY IF EXISTS policy_auditor ON docebo_tier;
 CREATE POLICY policy_auditor ON docebo_tier FOR SELECT TO auditor USING (current_market() = market_id);
 
+alter type public."tier" ADD VALUE 'T5';
+alter type public."tier" ADD VALUE 'T6';
+alter type public."tier" ADD VALUE 'T7';
 
 INSERT INTO docebo_tier (market_id,tier_code,docebo_catalogue_id)
 SELECT id as market_id,
-       UNNEST(ARRAY ['T1', 'T2', 'T3','T4'])::TIER AS tier_code,
-       UNNEST(ARRAY [T1, T2, T3,T4]) AS docebo_catalogue_id
-FROM (SELECT id, docebo_catalogue_id as T1, docebo_catalogue_id_t2  as T2, docebo_catalogue_id_t3 as T3,docebo_catalogue_id_t4 as T4
-from market) X 
+       UNNEST(ARRAY ['T1', 'T2', 'T3','T4','T5','T6','T7'])::TIER AS tier_code,
+       UNNEST(ARRAY [T1, T2, T3, T4, T5, T6, T7]) AS docebo_catalogue_id
+FROM (SELECT id, docebo_catalogue_id as T1, docebo_catalogue_id_t2  as T2, docebo_catalogue_id_t3 as T3,docebo_catalogue_id_t4 as T4, docebo_catalogue_id_t4 AS T5, docebo_catalogue_id_t4 AS T6, docebo_catalogue_id_t4 AS T7
+from market) X
