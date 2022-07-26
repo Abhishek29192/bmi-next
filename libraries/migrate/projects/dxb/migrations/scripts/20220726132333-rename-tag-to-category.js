f05ee40940bd758dbb1dd0aa41678aaf33217e0a
Maksym Kudenko <maxkudenko@gmail.com>
2022-07-26 16:46:36 +0300

    feat(migrations): rename contentful ContentType Tag to Category

--- libraries/migrate/projects/dxb/migrations/scripts/20220726132333-rename-tag-to-category.js
+++ libraries/migrate/projects/dxb/migrations/scripts/20220726132333-rename-tag-to-category.js
@@ -1,9 +1,9 @@
-module.exports.description = "<Rename Tag content type to Tag Type>";
+module.exports.description = "Rename Tag content type to Category";
 
 module.exports.up = (migration) => {
   const tagType = migration.editContentType("tag");
 
-  tagType.name("Tag Type");
+  tagType.name("Category");
 };
 
 module.exports.down = (migration) => {
