9d11accefd8ad52d708c8407eab04fb42f4445d7
“Dilaram <dilaram.gaire@bmigroup.com>
2022-07-27 12:27:40 +0100

    feat(head): configure homepage to handle brands for spa

--- components/src/brand-intro-card/BrandIntroCard.tsx
+++ components/src/brand-intro-card/BrandIntroCard.tsx
@@ -1,18 +1,18 @@
-import React from "react";
 import { SVGImport } from "@bmi-digital/svg-import";
 import { ArrowForward } from "@material-ui/icons";
 import classnames from "classnames";
-import Card from "../card/Card";
-import Typography from "../typography/Typography";
+import React from "react";
 import DefaultButton from "../button/Button";
+import Card from "../card/Card";
 import { ClickableAction } from "../clickable/Clickable";
+import Typography from "../typography/Typography";
 import styles from "./BrandIntroCard.module.scss";
 
 type Props = {
   logoIcon: SVGImport;
   buttonComponent?: React.ComponentType<any>; // TODO
-  description: React.ReactNode;
-  buttonLabel: React.ReactNode;
+  description?: React.ReactNode;
+  buttonLabel?: React.ReactNode;
   action?: ClickableAction;
   whiteBox?: boolean;
 };
@@ -30,7 +30,10 @@ const BrandIntroCard = ({
   return (
     <Card className={styles["BrandIntroCard"]}>
       <Button
-        className={styles["brandLogoButton"]}
+        className={classnames(
+          styles["brandLogoButton"],
+          !action && styles["brandLogoButton-no-pointer"]
+        )}
         action={action}
         variant="text"
         disableRipple
@@ -42,12 +45,17 @@ const BrandIntroCard = ({
           })}
         />
       </Button>
-      <Typography className={styles["description"]} variant="subtitle1">
-        {description}
-      </Typography>
-      <Button action={action} variant="text" endIcon={<ArrowForward />}>
-        {buttonLabel}
-      </Button>
+      {description && (
+        <Typography className={styles["description"]} variant="subtitle1">
+          {description}
+        </Typography>
+      )}
+
+      {buttonLabel && (
+        <Button action={action} variant="text" endIcon={<ArrowForward />}>
+          {buttonLabel}
+        </Button>
+      )}
     </Card>
   );
 };
