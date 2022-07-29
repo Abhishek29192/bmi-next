36668786beaab59df16c8f3303e5545883cc1dc7
Benjamin <benjamin@benjaminsproule.com>
2022-07-29 17:18:18 +0100

    fix(head): fix brand colours appearing in header

--- applications/dxb/head/gatsby-browser.tsx
+++ applications/dxb/head/gatsby-browser.tsx
@@ -1,32 +1,21 @@
-import "./src/styles/global.css";
-import React from "react";
 import { HubspotProvider } from "@aaronhayes/react-use-hubspot-form";
 import { ThemeProvider } from "@bmi/components";
 import type { GatsbyBrowser } from "gatsby";
+import React from "react";
 import { ConfigProvider } from "./src/contexts/ConfigProvider";
 import "./src/styles/global.css";
-
-import ComposeProviders from "./src/components/ComposeProviders";
 
 export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = ({
   element
 }) => {
   return (
     /*providers with static data*/
-    <ComposeProviders
-      components={[
-        (child) => <ConfigProvider>{child}</ConfigProvider>,
-        (child) => (
-          <ThemeProvider includeCssBaseline={false}> {child} </ThemeProvider>
-        ),
-        (child) => (
-          <HubspotProvider async={false} addToHead={true}>
-            {child}
-          </HubspotProvider>
-        )
-      ]}
-    >
-      {element}
-    </ComposeProviders>
+    <ConfigProvider>
+      <ThemeProvider includeCssBaseline={false}>
+        <HubspotProvider async={false} addToHead={true}>
+          {element}
+        </HubspotProvider>
+      </ThemeProvider>
+    </ConfigProvider>
   );
 };
