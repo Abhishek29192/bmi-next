aec85806a792cf0f4f77177df5f4d93f3eba7fd0
Nazar Antoniuk <nanto@softserveinc.com>
2022-08-05 16:32:35 +0300

    fix (project) Introduce 'ANNUAL_INSPECTION1' message template. Inspect req and user

--- applications/intouch/api/services/companies/src/middleware/__tests__/clientGateway.test.ts
+++ applications/intouch/api/services/companies/src/middleware/__tests__/clientGateway.test.ts
@@ -1,8 +1,13 @@
 import { overwriteMarketHeader } from "../clientGateway";
 
 describe("Client gateway", () => {
+  const logger = () => ({
+    info: () => {},
+    error: () => {}
+  });
   it("should overwrite the market header if first login", () => {
     const req = {
+      logger,
       user: {
         [`${process.env.AUTH0_NAMESPACE}/intouch_market_code`]: "no"
       },
@@ -15,6 +20,7 @@ describe("Client gateway", () => {
   });
   it("should not overwrite the market header if no user", () => {
     const req = {
+      logger,
       user: null,
       headers: {
         "x-request-market-domain": "en"
@@ -26,6 +32,7 @@ describe("Client gateway", () => {
   // let the app do the necessary redirect
   it("should not overwrite the market header if user with market", () => {
     const req = {
+      logger,
       user: {
         market: {
           domain: "no"
