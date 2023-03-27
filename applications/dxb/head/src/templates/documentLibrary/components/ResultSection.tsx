8e88abf0b80abc1e5774d1e7cb0eaba01488e115
Taras Dyda <taras.dyda@bmigroup.com>
2023-03-27 18:14:27 +0300

    fix(dxb): there is no space between table and download button

--- applications/dxb/head/src/templates/documentLibrary/components/ResultSection.tsx
+++ applications/dxb/head/src/templates/documentLibrary/components/ResultSection.tsx
@@ -3,7 +3,6 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import React from "react";
 import DocumentResultsFooter from "../../../components/DocumentResultsFooter";
 import { useSiteContext } from "../../../components/Site";
-import filterStyles from "../../../components/styles/Filters.module.scss";
 import { AssetType } from "../types";
 import DocumentResults, { DocumentResultData, Format } from "./DocumentResults";
 
@@ -37,10 +36,7 @@ const ResultSection = ({
             assetTypes={assetTypes}
             format={format}
           />
-          <div
-            className={filterStyles["results"]}
-            data-testid="document-results-footer-wrapper"
-          >
+          <div data-testid="document-results-footer-wrapper">
             <DocumentResultsFooter
               page={page + 1}
               count={pageCount}
