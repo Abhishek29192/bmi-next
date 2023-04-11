1a62703ecce3ca636017fc4ad62868c8e2e735d6
Taras Dyda <taras.dyda@bmigroup.com>
2023-04-11 16:28:51 +0300

    feat(dxb): update gcp-build-status-logger to receive events from gatsby

---
+++ applications/dxb/functions/gcp-build-status-logger/src/types.ts
@@ -0,0 +1,21 @@
+export enum BuildStatusType {
+  BUILD_SUCCEEDED = "BUILD_SUCCEEDED",
+  BUILD_FAILED = "BUILD_FAILED",
+  BUILD_TIMED_OUT = "BUILD_TIMED_OUT",
+  PREVIEW_SUCCEEDED = "PREVIEW_SUCCEEDED",
+  PREVIEW_FAILED = "PREVIEW_FAILED",
+  PREVIEW_TIMED_OUT = "PREVIEW_TIMED_OUT"
+}
+
+export type GatsbyLog = {
+  body: string;
+  buildId: string;
+  workspaceName: string;
+  siteName: string;
+  deployPreviewUrl: string;
+  logsUrl: string;
+  duration: string;
+  resourceId: string;
+  resourceType: string;
+  event: BuildStatusType;
+};
