599332a547fd2cecf2a118689a340d4587f92742
Hitesh Khatri <hitesh.khatri@bmigroup.com>
2023-08-17 21:09:37 +0100

    fix(head): login working

--- applications/dxb/head/src/components/LoginButton.tsx
+++ applications/dxb/head/src/components/LoginButton.tsx
@@ -1,21 +1,3 @@
-// import React from "react";
-// import { useAuth0 } from "@auth0/auth0-react";
-// import { Button } from "@bmi-digital/components";
-
-// function LoginButton() {
-//   const { isAuthenticated, loginWithRedirect } = useAuth0();
-
-//   return (
-//     !isAuthenticated && (
-//       <Button onClick={loginWithRedirect} style={{ margin: "36px" }}>
-//         Log in
-//       </Button>
-//     )
-//   );
-// }
-
-// export default LoginButton;
-
 import React from "react";
 import { AuthService, useAuth } from "gatsby-theme-auth0";
 import { Button } from "@bmi-digital/components";
