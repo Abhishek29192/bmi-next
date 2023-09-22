419f393a67a4daaa32159a575e42cd54a726047e
Nandhini Chidambaram <nandhini04@infosys.com>
2023-09-22 15:40:36 +0530

    fix(dxb): fixed multiline wrap in syndicate description

--- applications/dxb/head/src/components/styles/SyndicateSection.styles.ts
+++ applications/dxb/head/src/components/styles/SyndicateSection.styles.ts
@@ -1,5 +1,5 @@
 import { styled } from "@mui/material/styles";
-import { Grid } from "@bmi-digital/components";
+import { Grid, Typography } from "@bmi-digital/components";
 
 export const DescriptionGrid = styled(Grid)(({ theme }) => ({
   margin: "0 0 48px",
@@ -7,3 +7,7 @@ export const DescriptionGrid = styled(Grid)(({ theme }) => ({
     marginBottom: "60px"
   }
 }));
+
+export const DescriptionTypoMultiLine = styled(Typography)({
+  whiteSpace: "pre-line"
+});
