1bc5aa71f346585075af3d5cf8468665804142f3
Benjamin Spoule <benjamin.sproule@bmigroup.com>
2023-10-16 23:28:01 +0100

    ci: upgrade to latest eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,18 @@ module.exports = {
     "**/public/**",
     ".yarn/**",
     ".yarn_cache",
-    // Needed only until we can use upstream fixes or we take this into our own setup properly
+    // Needed only until we can use upstream fixes or we take this into our
+    // own setup properly
     "applications/dxb/libraries/gatsby-plugin-sitemap"
   ],
   rules: {
-    "import/extensions": [0, "never"]
+    complexity: "warn",
+    "max-depth": "warn",
+    "max-len": "warn",
+    "max-lines": "warn",
+    "max-lines-per-function": "warn",
+    "max-nested-callbacks": "warn",
+    "max-params": "warn",
+    "max-statements": ["warn", { max: 25 }]
   }
 };
