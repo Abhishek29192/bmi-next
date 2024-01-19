4b3939fc3982ad1931d130632320bde0ba47efd7
Claudio Manenti <claudio.manenti@bmigroup.com>
2024-01-19 11:31:46 +0000

    feat(head): belgium redirects url typo

--- applications/redirects-website-be-universalpro/gatsby-config.js
+++ applications/redirects-website-be-universalpro/gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: `BMI dxb BE redirects - Belgium-universalpro`,
     description: `Website for BE redirects - Belgium Universalpro`,
     author: `bmi`,
-    siteUrl: `https://bmidxbredirectsbelgiumuniversalpro.gatsbyjs.io`
+    siteUrl: `https://bmidxbredirectsbelgiumunivpro.gatsbyjs.io/`
   },
   plugins: [
     {
