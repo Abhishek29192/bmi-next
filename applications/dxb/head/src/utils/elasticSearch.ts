d355edd1f1fd082c072a7024aa3dfc31bd4b618a
tdyda <taras.dyda@bmigroup.com>
2024-03-13 17:05:20 +0200

    fix(dxb): replace / with \/ in ES search queries

--- applications/dxb/head/src/utils/elasticSearch.ts
+++ applications/dxb/head/src/utils/elasticSearch.ts
@@ -259,7 +259,7 @@ export const getDocumentQueryObject = (
   const queryElements = [
     {
       query_string: {
-        query: `*${queryString}*`,
+        query: `*${sanitiseQueryString(queryString)}*`,
         type: "cross_fields",
         fields: ["title"]
       }
@@ -401,4 +401,4 @@ export const queryElasticSearch = async (query = {}, indexName: string) => {
 };
 
 export const sanitiseQueryString = (queryString: string) =>
-  queryString.replace(/[^.,\s\p{L}\p{Nd}-]/gu, " ");
+  queryString.replaceAll("/", "\\/").replace(/[^.,\s\p{L}\p{Nd}/\\-]/gu, " ");
