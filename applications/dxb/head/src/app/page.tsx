4df6164523c23f7cdf22a632a4e2fec75bada265
Benjamin Spoule <benjamin.sproule@bmigroup.com>
2024-04-08 18:55:27 +0100

    feat: add draft mode

--- applications/dxb/head/src/app/page.tsx
+++ applications/dxb/head/src/app/page.tsx
@@ -1,10 +1,13 @@
+import { draftMode } from "next/headers";
 import React from "react";
 
 async function getData() {
-  const res = await fetch(
-    "https://run.mocky.io/v3/97233b65-a44c-4347-9ca5-7fadc4578aaa",
-    { next: { revalidate: 3600 } }
-  );
+  const { isEnabled } = draftMode();
+
+  const url = isEnabled
+    ? "https://run.mocky.io/v3/911bca8f-ba43-4fff-af66-14f5b735125a"
+    : "https://run.mocky.io/v3/5cc0155a-fac5-4692-bc95-6821ef9a43d8";
+  const res = await fetch(url, { next: { tags: ["home-page"] } });
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
