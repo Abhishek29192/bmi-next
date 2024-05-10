b9067c8ec44fd3a48375dbfc85744d6f6e3b1f27
Mahesh Warnasooriya <mahesh.warnasooriya@bmigroup.com>
2024-05-10 15:50:44 +0100

    feat(head): use env prefix in shared variables

--- applications/dxb/head/src/utils/httpHeadersHelper.mjs
+++ applications/dxb/head/src/utils/httpHeadersHelper.mjs
@@ -3,22 +3,29 @@ export const contentSecurityPolicy = (marketCode) => {
   if (marketCode === groupSiteCode || !process.env.IS_NETLIFY) {
     return process.env.CONTENT_SECURITY_POLICY;
   }
-  return process.env[`DXB_CONTENT_SECURITY_POLICY_${envVarSuffix(marketCode)}`];
+  const environment = process.env["DXB_ENVIRONMENT"];
+  return process.env[
+    `${environment}_DXB_CONTENT_SECURITY_POLICY_${envVarSuffix(marketCode)}`
+  ];
 };
 
 export const xRobotTags = (marketCode) => {
   if (marketCode === groupSiteCode || !process.env.IS_NETLIFY) {
     return process.env.X_ROBOTS_TAG;
   }
-  return process.env[`DXB_X_ROBOTS_TAG_${envVarSuffix(marketCode)}`];
+  const environment = process.env["DXB_ENVIRONMENT"];
+  return process.env[
+    `${environment}_DXB_X_ROBOTS_TAG_${envVarSuffix(marketCode)}`
+  ];
 };
 
 export const accessControlAllowOrigin = (marketCode) => {
   if (marketCode === groupSiteCode || !process.env.IS_NETLIFY) {
     return process.env.ACCESS_CONTROL_ALLOW_ORIGIN;
   }
+  const environment = process.env["DXB_ENVIRONMENT"];
   return process.env[
-    `DXB_ACCESS_CONTROL_ALLOW_ORIGIN_${envVarSuffix(marketCode)}`
+    `${environment}_DXB_ACCESS_CONTROL_ALLOW_ORIGIN_${envVarSuffix(marketCode)}`
   ];
 };
 
