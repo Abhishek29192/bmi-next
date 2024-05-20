6634cd9fbf69e0515e55ae2eb2eb653b256123a9
Mahesh Warnasooriya <mahesh.warnasooriya@bmigroup.com>
2024-05-20 12:15:18 +0100

    feat(head): split CSP

--- applications/dxb/head/src/utils/httpHeadersHelper.mjs
+++ applications/dxb/head/src/utils/httpHeadersHelper.mjs
@@ -1,36 +1,81 @@
+import parseContentSecurityPolicy from "content-security-policy-parser";
+
 const groupSiteCode = "grp";
 export const contentSecurityPolicy = (marketCode) => {
-  if (marketCode === groupSiteCode || !process.env.IS_NETLIFY) {
+  if (!process.env.IS_NETLIFY) {
     return process.env.CONTENT_SECURITY_POLICY;
   }
-  const environment = process.env["DXB_ENVIRONMENT"];
-  return process.env[
-    `${environment}_DXB_CONTENT_SECURITY_POLICY_${envVarSuffix(marketCode)}`
-  ];
+
+  const csp = buildCsp(marketCode);
+  if (process.env["DXB_DEBUG"]) {
+    console.log(`${marketCode} Content Security Policy: ${csp}`);
+  }
+  return csp;
 };
 
 export const xRobotTags = (marketCode) => {
-  if (marketCode === groupSiteCode || !process.env.IS_NETLIFY) {
+  if (!process.env.IS_NETLIFY) {
     return process.env.X_ROBOTS_TAG;
   }
   const environment = process.env["DXB_ENVIRONMENT"];
-  return process.env[
-    `${environment}_DXB_X_ROBOTS_TAG_${envVarSuffix(marketCode)}`
-  ];
+  const marketOverride =
+    process.env[`${environment}_DXB_X_ROBOTS_TAG_${envVarSuffix(marketCode)}`];
+  if (marketOverride) {
+    return marketOverride;
+  }
+
+  return process.env[`${environment}_DXB_X_ROBOTS_TAG`];
 };
 
 export const accessControlAllowOrigin = (marketCode) => {
-  if (marketCode === groupSiteCode || !process.env.IS_NETLIFY) {
+  if (!process.env.IS_NETLIFY) {
     return process.env.ACCESS_CONTROL_ALLOW_ORIGIN;
   }
   const environment = process.env["DXB_ENVIRONMENT"];
-  return process.env[
-    `${environment}_DXB_ACCESS_CONTROL_ALLOW_ORIGIN_${envVarSuffix(marketCode)}`
-  ];
+  const marketOverride =
+    process.env[
+      `${environment}_DXB_ACCESS_CONTROL_ALLOW_ORIGIN_${envVarSuffix(
+        marketCode
+      )}`
+    ];
+  if (marketOverride) {
+    return marketOverride;
+  }
+  return process.env[`${environment}_DXB_ACCESS_CONTROL_ALLOW_ORIGIN`];
 };
 
 // Builds the market environment variable suffix in the form NO
 // or FR_BE (for bilingual markets).
 const envVarSuffix = (marketCode) => {
   return `${marketCode.split("/").pop().toUpperCase().replace("-", "_")}`;
+};
+
+const buildCsp = (marketCode) => {
+  const environment = process.env["DXB_ENVIRONMENT"].toLocaleUpperCase();
+  const commonCsp = process.env[`${environment}_DXB_CONTENT_SECURITY_POLICY`];
+  const marketcsp =
+    process.env[
+      `${environment}_DXB_CONTENT_SECURITY_POLICY_${envVarSuffix(marketCode)}`
+    ];
+  if (!marketcsp) {
+    return commonCsp;
+  }
+
+  const parsedCommonCsp = parseContentSecurityPolicy(commonCsp);
+  const parsedMarketCsp = parseContentSecurityPolicy(marketcsp);
+
+  parsedMarketCsp.forEach((v, k) => {
+    parsedCommonCsp.set(
+      k,
+      parsedCommonCsp.get(k) ? parsedCommonCsp.get(k).concat(v) : v
+    );
+  });
+
+  return stringifyCsp(parsedCommonCsp);
+};
+
+const stringifyCsp = (csp) => {
+  return Array.from(csp, ([key, value]) => `${key} ${value.join(" ")}`).join(
+    "; "
+  );
 };
