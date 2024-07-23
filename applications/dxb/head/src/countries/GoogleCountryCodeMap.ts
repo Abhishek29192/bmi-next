4530858b46b050efb1d30476db6495057266b7c8
Benjamin Spoule <benjamin.sproule@bmigroup.com>
2024-07-23 10:27:11 +0100

    fix(head): provide Google Maps country for QA

--- applications/dxb/head/src/countries/GoogleCountryCodeMap.ts
+++ applications/dxb/head/src/countries/GoogleCountryCodeMap.ts
@@ -1,3 +1,5 @@
 // add country codes here for e.g {"uk": ["uk", "ir", "no"], "at": ["at", "de"]}
-const AutoCompleteCountryMap: { [countryCode: string]: string[] } = {};
+const AutoCompleteCountryMap: { [countryCode: string]: string[] } = {
+  dxb: ["no"]
+};
 export default AutoCompleteCountryMap;
